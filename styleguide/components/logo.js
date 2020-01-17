758efe40e0c48777f78274317eca2e53d075b0df
Panos Voudouris <panosvoudouris@gmail.com>
2020-01-17 13:14:21 +0000

    Doc updates

--- styleguide/components/logo.js
+++ styleguide/components/logo.js
@@ -6,7 +6,7 @@ const logo = () => {
         '<a class="cads-logo" href="javascript:;" title="Citizens Advice Logo"></a>';
 
     const usage =
-        'You can use the cads-logo class on anything to display the logo.';
+        'You can use the <code>cads-logo</code> class on anything to display the logo.';
 
     return wrapper('Logo', component, usage);
 };
--- styleguide/tokens/animations.md
+++ styleguide/tokens/animations.md
@@ -1,3 +1,5 @@
 # Animations
 
-All animations are 0.3s long. Use the `$cads-animation-speed` variable.
+All animations are 0.3s long. Use the `$cads-animation-speed` variable. Hover over the logo below to see an example:
+
+<div class="cads-logo"></div>
