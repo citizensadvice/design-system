bdc313ddb1d2b0ab11a21fb41dc18d6047d6c339
Panos Voudouris <panosvoudouris@gmail.com>
2020-01-28 17:09:28 +0000

    Callout box scss

--- styleguide/components/callout-box.js
+++ styleguide/components/callout-box.js
@@ -1,11 +1,18 @@
-import { text } from '@storybook/addon-knobs'; // eslint-disable-line
+import { text, select } from '@storybook/addon-knobs'; // eslint-disable-line
 import wrapper from './component-wrapper';
 
 const callout = () => {
     const heading = text('Heading', 'Callout title');
     const content = text('Content', 'The callout box content');
-    const component = `<div class="cads-callout">
-  <h1>${heading}</h1>
+    const options = {
+        Info: 'info',
+        Quote: 'quote',
+        Important: 'important'
+    };
+    const boxType = select('Callout type', options, 'quote');
+
+    const component = `<div class="cads-callout cads-callout-${boxType}">
+  <h1><span class="cads-callout-label"></span>${heading}</h1>
   ${content}
 </div>`;
     return wrapper('Callout', component);
