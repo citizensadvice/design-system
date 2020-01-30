9fdaa9f6f7b99a5b918f82d1f0809cee8a75c002
Panos Voudouris <panosvoudouris@gmail.com>
2020-01-30 16:18:39 +0000

    Callout update

--- styleguide/components/callout-box.js
+++ styleguide/components/callout-box.js
@@ -5,11 +5,11 @@ const callout = () => {
     const heading = text('Heading', 'Callout title');
     const content = text('Content', 'The callout box content');
     const options = {
+        Example: 'example',
         Info: 'info',
-        Notice: 'notice',
         Important: 'important'
     };
-    const boxType = select('Callout type', options, 'notice');
+    const boxType = select('Callout type', options, 'info');
 
     const component = `<div class="cads-callout cads-callout-${boxType}">
   <h3><span class="cads-callout-label"></span>${heading}</h3>
