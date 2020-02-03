42232503b70a70e1e9899a2ece80862c23e6e311
Panos Voudouris <panosvoudouris@gmail.com>
2020-02-03 16:30:45 +0000

    Long label tests

--- styleguide/components/breadcrumbs.js
+++ styleguide/components/breadcrumbs.js
@@ -12,6 +12,11 @@ const breadcrumbs = () => {
     const component = `<div class="cads-breadcrumbs">
     <ul class="cads-list-unordered">
         ${items}
+        <li>
+            <a href="javascript:;" title="The truncated text is provided in the a tag title">
+            This is a very long label that should be trunacted</a>
+        </li>
+        <li title="The accessible title is in the li title">This is a very long label that should be trunacted</li>
     </ul>
 </div>`;
     return wrapper('Breadcrumb', component);
