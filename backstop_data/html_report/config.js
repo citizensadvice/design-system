35a5db593105f64c54305f1219c8b21a6d1e9ecf
Marcelo Nicolosi <marcelonicolosi@ca-admins-mbp-3.internal.citizensadvice.org.uk>
2020-02-28 10:05:28 +0000

    Backstop running screenshot tests on a small, medium and large screen on Component Button page

---
+++ backstop_data/html_report/config.js
@@ -0,0 +1,78 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Design_System_Buttons_0_document_0_Small.png",
+        "test": "../bitmaps_test/20200228-100031/backstop_default_Design_System_Buttons_0_document_0_Small.png",
+        "selector": "document",
+        "fileName": "backstop_default_Design_System_Buttons_0_document_0_Small.png",
+        "label": "Design System Buttons",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:6006/?path=/story/3-markup-components--buttons",
+        "referenceUrl": "https://citizensadvice.github.io/design-system/?path=/story/3-markup-components--buttons",
+        "expect": 0,
+        "viewportLabel": "Small",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Design_System_Buttons_0_document_1_Small.png",
+        "test": "../bitmaps_test/20200228-100031/backstop_default_Design_System_Buttons_0_document_1_Small.png",
+        "selector": "document",
+        "fileName": "backstop_default_Design_System_Buttons_0_document_1_Small.png",
+        "label": "Design System Buttons",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:6006/?path=/story/3-markup-components--buttons",
+        "referenceUrl": "https://citizensadvice.github.io/design-system/?path=/story/3-markup-components--buttons",
+        "expect": 0,
+        "viewportLabel": "Small",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Design_System_Buttons_0_document_2_Large.png",
+        "test": "../bitmaps_test/20200228-100031/backstop_default_Design_System_Buttons_0_document_2_Large.png",
+        "selector": "document",
+        "fileName": "backstop_default_Design_System_Buttons_0_document_2_Large.png",
+        "label": "Design System Buttons",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:6006/?path=/story/3-markup-components--buttons",
+        "referenceUrl": "https://citizensadvice.github.io/design-system/?path=/story/3-markup-components--buttons",
+        "expect": 0,
+        "viewportLabel": "Large",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    }
+  ],
+  "id": "backstop_default"
+});
