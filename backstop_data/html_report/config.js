3f22b91d81963cf5716129630559a5528dc40fdc
Marcelo Nicolosi <marcelonicolosi@ca-admins-mbp-3.internal.citizensadvice.org.uk>
2020-03-03 14:26:36 +0000

    All '3 Markup Components' pages

--- backstop_data/html_report/config.js
+++ backstop_data/html_report/config.js
@@ -3,11 +3,107 @@ report({
   "tests": [
     {
       "pair": {
-        "reference": "../bitmaps_reference/backstop_default_Design_System_Buttons_0_document_0_Small.png",
-        "test": "../bitmaps_test/20200303-113816/backstop_default_Design_System_Buttons_0_document_0_Small.png",
+        "reference": "../bitmaps_reference/backstop_default_3_Markup_Components_-_Breadcrumbs_0_document_0_Small.png",
+        "test": "../bitmaps_test/20200303-142419/backstop_default_3_Markup_Components_-_Breadcrumbs_0_document_0_Small.png",
         "selector": "document",
-        "fileName": "backstop_default_Design_System_Buttons_0_document_0_Small.png",
-        "label": "Design System Buttons",
+        "fileName": "backstop_default_3_Markup_Components_-_Breadcrumbs_0_document_0_Small.png",
+        "label": "3 Markup Components - Breadcrumbs",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:6006/?path=/story/3-markup-components--breadcrumbs",
+        "referenceUrl": "https://citizensadvice.github.io/design-system/?path=/story/3-markup-components--breadcrumbs",
+        "expect": 0,
+        "viewportLabel": "Small",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_3_Markup_Components_-_Breadcrumbs_0_document_1_Medium.png",
+        "test": "../bitmaps_test/20200303-142419/backstop_default_3_Markup_Components_-_Breadcrumbs_0_document_1_Medium.png",
+        "selector": "document",
+        "fileName": "backstop_default_3_Markup_Components_-_Breadcrumbs_0_document_1_Medium.png",
+        "label": "3 Markup Components - Breadcrumbs",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:6006/?path=/story/3-markup-components--breadcrumbs",
+        "referenceUrl": "https://citizensadvice.github.io/design-system/?path=/story/3-markup-components--breadcrumbs",
+        "expect": 0,
+        "viewportLabel": "Medium",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_3_Markup_Components_-_Breadcrumbs_0_document_2_Large.png",
+        "test": "../bitmaps_test/20200303-142419/backstop_default_3_Markup_Components_-_Breadcrumbs_0_document_2_Large.png",
+        "selector": "document",
+        "fileName": "backstop_default_3_Markup_Components_-_Breadcrumbs_0_document_2_Large.png",
+        "label": "3 Markup Components - Breadcrumbs",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:6006/?path=/story/3-markup-components--breadcrumbs",
+        "referenceUrl": "https://citizensadvice.github.io/design-system/?path=/story/3-markup-components--breadcrumbs",
+        "expect": 0,
+        "viewportLabel": "Large",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_3_Markup_Components_-_Breadcrumbs_xsmall_0_document_0_phone.png",
+        "test": "../bitmaps_test/20200303-142419/backstop_default_3_Markup_Components_-_Breadcrumbs_xsmall_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "backstop_default_3_Markup_Components_-_Breadcrumbs_xsmall_0_document_0_phone.png",
+        "label": "3 Markup Components - Breadcrumbs (xsmall)",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:6006/?path=/story/3-markup-components--breadcrumbs",
+        "referenceUrl": "https://citizensadvice.github.io/design-system/?path=/story/3-markup-components--breadcrumbs",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_3_Markup_Components_-_Buttons_0_document_0_Small.png",
+        "test": "../bitmaps_test/20200303-142419/backstop_default_3_Markup_Components_-_Buttons_0_document_0_Small.png",
+        "selector": "document",
+        "fileName": "backstop_default_3_Markup_Components_-_Buttons_0_document_0_Small.png",
+        "label": "3 Markup Components - Buttons",
         "requireSameDimensions": true,
         "misMatchThreshold": 0.1,
         "url": "http://localhost:6006/?path=/story/3-markup-components--buttons",
@@ -27,11 +123,11 @@ report({
     },
     {
       "pair": {
-        "reference": "../bitmaps_reference/backstop_default_Design_System_Buttons_0_document_1_Medium.png",
-        "test": "../bitmaps_test/20200303-113816/backstop_default_Design_System_Buttons_0_document_1_Medium.png",
+        "reference": "../bitmaps_reference/backstop_default_3_Markup_Components_-_Buttons_0_document_1_Medium.png",
+        "test": "../bitmaps_test/20200303-142419/backstop_default_3_Markup_Components_-_Buttons_0_document_1_Medium.png",
         "selector": "document",
-        "fileName": "backstop_default_Design_System_Buttons_0_document_1_Medium.png",
-        "label": "Design System Buttons",
+        "fileName": "backstop_default_3_Markup_Components_-_Buttons_0_document_1_Medium.png",
+        "label": "3 Markup Components - Buttons",
         "requireSameDimensions": true,
         "misMatchThreshold": 0.1,
         "url": "http://localhost:6006/?path=/story/3-markup-components--buttons",
@@ -52,11 +148,11 @@ report({
     },
     {
       "pair": {
-        "reference": "../bitmaps_reference/backstop_default_Design_System_Buttons_0_document_2_Large.png",
-        "test": "../bitmaps_test/20200303-113816/backstop_default_Design_System_Buttons_0_document_2_Large.png",
+        "reference": "../bitmaps_reference/backstop_default_3_Markup_Components_-_Buttons_0_document_2_Large.png",
+        "test": "../bitmaps_test/20200303-142419/backstop_default_3_Markup_Components_-_Buttons_0_document_2_Large.png",
         "selector": "document",
-        "fileName": "backstop_default_Design_System_Buttons_0_document_2_Large.png",
-        "label": "Design System Buttons",
+        "fileName": "backstop_default_3_Markup_Components_-_Buttons_0_document_2_Large.png",
+        "label": "3 Markup Components - Buttons",
         "requireSameDimensions": true,
         "misMatchThreshold": 0.1,
         "url": "http://localhost:6006/?path=/story/3-markup-components--buttons",
@@ -76,15 +172,207 @@ report({
     },
     {
       "pair": {
-        "reference": "../bitmaps_reference/backstop_default_Design_System_Buttons_-_phone_0_document_0_phone.png",
-        "test": "../bitmaps_test/20200303-113816/backstop_default_Design_System_Buttons_-_phone_0_document_0_phone.png",
+        "reference": "../bitmaps_reference/backstop_default_3_Markup_Components_-_Buttons_xsmall_0_document_0_phone.png",
+        "test": "../bitmaps_test/20200303-142419/backstop_default_3_Markup_Components_-_Buttons_xsmall_0_document_0_phone.png",
         "selector": "document",
-        "fileName": "backstop_default_Design_System_Buttons_-_phone_0_document_0_phone.png",
-        "label": "Design System Buttons - phone",
+        "fileName": "backstop_default_3_Markup_Components_-_Buttons_xsmall_0_document_0_phone.png",
+        "label": "3 Markup Components - Buttons (xsmall)",
         "requireSameDimensions": true,
         "misMatchThreshold": 0.1,
         "url": "http://localhost:6006/?path=/story/3-markup-components--buttons",
         "referenceUrl": "https://citizensadvice.github.io/design-system/?path=/story/3-markup-components--buttons",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_3_Markup_Components_-_Callout_0_document_0_Small.png",
+        "test": "../bitmaps_test/20200303-142419/backstop_default_3_Markup_Components_-_Callout_0_document_0_Small.png",
+        "selector": "document",
+        "fileName": "backstop_default_3_Markup_Components_-_Callout_0_document_0_Small.png",
+        "label": "3 Markup Components - Callout",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:6006/?path=/story/3-markup-components--callout",
+        "referenceUrl": "https://citizensadvice.github.io/design-system/?path=/story/3-markup-components--callout",
+        "expect": 0,
+        "viewportLabel": "Small",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_3_Markup_Components_-_Callout_0_document_1_Medium.png",
+        "test": "../bitmaps_test/20200303-142419/backstop_default_3_Markup_Components_-_Callout_0_document_1_Medium.png",
+        "selector": "document",
+        "fileName": "backstop_default_3_Markup_Components_-_Callout_0_document_1_Medium.png",
+        "label": "3 Markup Components - Callout",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:6006/?path=/story/3-markup-components--callout",
+        "referenceUrl": "https://citizensadvice.github.io/design-system/?path=/story/3-markup-components--callout",
+        "expect": 0,
+        "viewportLabel": "Medium",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_3_Markup_Components_-_Callout_0_document_2_Large.png",
+        "test": "../bitmaps_test/20200303-142419/backstop_default_3_Markup_Components_-_Callout_0_document_2_Large.png",
+        "selector": "document",
+        "fileName": "backstop_default_3_Markup_Components_-_Callout_0_document_2_Large.png",
+        "label": "3 Markup Components - Callout",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:6006/?path=/story/3-markup-components--callout",
+        "referenceUrl": "https://citizensadvice.github.io/design-system/?path=/story/3-markup-components--callout",
+        "expect": 0,
+        "viewportLabel": "Large",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_3_Markup_Components_-_Callout_xsmall_0_document_0_phone.png",
+        "test": "../bitmaps_test/20200303-142419/backstop_default_3_Markup_Components_-_Callout_xsmall_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "backstop_default_3_Markup_Components_-_Callout_xsmall_0_document_0_phone.png",
+        "label": "3 Markup Components - Callout (xsmall)",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:6006/?path=/story/3-markup-components--callout",
+        "referenceUrl": "https://citizensadvice.github.io/design-system/?path=/story/3-markup-components--callout",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_3_Markup_Components_-_Navigation_0_document_0_Small.png",
+        "test": "../bitmaps_test/20200303-142419/backstop_default_3_Markup_Components_-_Navigation_0_document_0_Small.png",
+        "selector": "document",
+        "fileName": "backstop_default_3_Markup_Components_-_Navigation_0_document_0_Small.png",
+        "label": "3 Markup Components - Navigation",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:6006/?path=/story/3-markup-components--navigation",
+        "referenceUrl": "https://citizensadvice.github.io/design-system/?path=/story/3-markup-components--navigation",
+        "expect": 0,
+        "viewportLabel": "Small",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_3_Markup_Components_-_Navigation_0_document_1_Medium.png",
+        "test": "../bitmaps_test/20200303-142419/backstop_default_3_Markup_Components_-_Navigation_0_document_1_Medium.png",
+        "selector": "document",
+        "fileName": "backstop_default_3_Markup_Components_-_Navigation_0_document_1_Medium.png",
+        "label": "3 Markup Components - Navigation",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:6006/?path=/story/3-markup-components--navigation",
+        "referenceUrl": "https://citizensadvice.github.io/design-system/?path=/story/3-markup-components--navigation",
+        "expect": 0,
+        "viewportLabel": "Medium",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_3_Markup_Components_-_Navigation_0_document_2_Large.png",
+        "test": "../bitmaps_test/20200303-142419/backstop_default_3_Markup_Components_-_Navigation_0_document_2_Large.png",
+        "selector": "document",
+        "fileName": "backstop_default_3_Markup_Components_-_Navigation_0_document_2_Large.png",
+        "label": "3 Markup Components - Navigation",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:6006/?path=/story/3-markup-components--navigation",
+        "referenceUrl": "https://citizensadvice.github.io/design-system/?path=/story/3-markup-components--navigation",
+        "expect": 0,
+        "viewportLabel": "Large",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_3_Markup_Components_-_Navigation_xsmall_0_document_0_phone.png",
+        "test": "../bitmaps_test/20200303-142419/backstop_default_3_Markup_Components_-_Navigation_xsmall_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "backstop_default_3_Markup_Components_-_Navigation_xsmall_0_document_0_phone.png",
+        "label": "3 Markup Components - Navigation (xsmall)",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:6006/?path=/story/3-markup-components--navigation",
+        "referenceUrl": "https://citizensadvice.github.io/design-system/?path=/story/3-markup-components--navigation",
         "expect": 0,
         "viewportLabel": "phone",
         "diff": {
