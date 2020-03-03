a87cec07d06e866ab0cca46c7148c20334476934
Marcelo Nicolosi <marcelonicolosi@ca-admins-mbp-3.internal.citizensadvice.org.uk>
2020-03-03 11:40:20 +0000

    Tests running on Button page as a POC, working fine in 4 different sizes of screens

--- backstop_data/html_report/config.js
+++ backstop_data/html_report/config.js
@@ -4,7 +4,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/backstop_default_Design_System_Buttons_0_document_0_Small.png",
-        "test": "../bitmaps_test/20200228-164353/backstop_default_Design_System_Buttons_0_document_0_Small.png",
+        "test": "../bitmaps_test/20200303-113816/backstop_default_Design_System_Buttons_0_document_0_Small.png",
         "selector": "document",
         "fileName": "backstop_default_Design_System_Buttons_0_document_0_Small.png",
         "label": "Design System Buttons",
@@ -27,17 +27,42 @@ report({
     },
     {
       "pair": {
-        "reference": "../bitmaps_reference/backstop_default_Design_System_Buttons_0_document_1_Small.png",
-        "test": "../bitmaps_test/20200228-164353/backstop_default_Design_System_Buttons_0_document_1_Small.png",
+        "reference": "../bitmaps_reference/backstop_default_Design_System_Buttons_0_document_1_Medium.png",
+        "test": "../bitmaps_test/20200303-113816/backstop_default_Design_System_Buttons_0_document_1_Medium.png",
         "selector": "document",
-        "fileName": "backstop_default_Design_System_Buttons_0_document_1_Small.png",
+        "fileName": "backstop_default_Design_System_Buttons_0_document_1_Medium.png",
         "label": "Design System Buttons",
         "requireSameDimensions": true,
         "misMatchThreshold": 0.1,
         "url": "http://localhost:6006/?path=/story/3-markup-components--buttons",
         "referenceUrl": "https://citizensadvice.github.io/design-system/?path=/story/3-markup-components--buttons",
         "expect": 0,
-        "viewportLabel": "Small",
+        "viewportLabel": "Medium",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 16
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Design_System_Buttons_0_document_2_Large.png",
+        "test": "../bitmaps_test/20200303-113816/backstop_default_Design_System_Buttons_0_document_2_Large.png",
+        "selector": "document",
+        "fileName": "backstop_default_Design_System_Buttons_0_document_2_Large.png",
+        "label": "Design System Buttons",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:6006/?path=/story/3-markup-components--buttons",
+        "referenceUrl": "https://citizensadvice.github.io/design-system/?path=/story/3-markup-components--buttons",
+        "expect": 0,
+        "viewportLabel": "Large",
         "diff": {
           "isSameDimensions": true,
           "dimensionDifference": {
@@ -51,17 +76,17 @@ report({
     },
     {
       "pair": {
-        "reference": "../bitmaps_reference/backstop_default_Design_System_Buttons_0_document_2_Large.png",
-        "test": "../bitmaps_test/20200228-164353/backstop_default_Design_System_Buttons_0_document_2_Large.png",
+        "reference": "../bitmaps_reference/backstop_default_Design_System_Buttons_-_phone_0_document_0_phone.png",
+        "test": "../bitmaps_test/20200303-113816/backstop_default_Design_System_Buttons_-_phone_0_document_0_phone.png",
         "selector": "document",
-        "fileName": "backstop_default_Design_System_Buttons_0_document_2_Large.png",
-        "label": "Design System Buttons",
+        "fileName": "backstop_default_Design_System_Buttons_-_phone_0_document_0_phone.png",
+        "label": "Design System Buttons - phone",
         "requireSameDimensions": true,
         "misMatchThreshold": 0.1,
         "url": "http://localhost:6006/?path=/story/3-markup-components--buttons",
         "referenceUrl": "https://citizensadvice.github.io/design-system/?path=/story/3-markup-components--buttons",
         "expect": 0,
-        "viewportLabel": "Large",
+        "viewportLabel": "phone",
         "diff": {
           "isSameDimensions": true,
           "dimensionDifference": {
