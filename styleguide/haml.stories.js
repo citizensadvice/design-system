de4ab0802d6f012bb9c4dab0319316a9147c8949
Panos Voudouris <panosvoudouris@gmail.com>
2020-03-09 09:59:01 +0000

    Merge radio button stories

--- styleguide/haml.stories.js
+++ styleguide/haml.stories.js
@@ -22,7 +22,6 @@ import tSearch from '../haml/_search.html.haml';
 import tNoticeBanner from '../haml/_notice_banner.html.haml';
 import tBreadcrumb from '../haml/_breadcrumb.story.html.haml';
 import tRadio from '../haml/_radio_group.story.html.haml';
-import tRadioSmall from '../haml/_radio_group_small.story.html.haml';
 import tInput from '../haml/_input.html.haml';
 
 // ...then queue in memory partials that are used by other partials
@@ -74,8 +73,6 @@ export const header = () => renderHamlTemplate('Header', tHeader, 'header');
 export const input = () => renderHamlTemplate('Input', tInput, 'input');
 export const radioGroup = () =>
     renderHamlTemplate('Radio Group', tRadio, 'radio_group');
-export const radioGroupSmall = () =>
-    renderHamlTemplate('Radio Group Small', tRadioSmall, 'radio_group_small');
 export const breadcrumb = () =>
     renderHamlTemplate('Breadcrumbs', tBreadcrumb, 'breadcrumb');
 export const logo = () =>
