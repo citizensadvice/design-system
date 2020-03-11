54f18bb91273358631ce0e8afa253ed752c3b5fb
Dan Cseko <dan@baseonmars.co.uk>
2020-03-11 14:36:53 +0000

    wip: shell script using pipes

--- scripts/haml.rb
+++ scripts/haml.rb
@@ -6,6 +6,8 @@ HAML_DIR = File.join(Dir.pwd, 'haml')
 HTML_EXTENSION = '.html.haml'
 STORY_DIR = File.join(Dir.pwd, 'styleguide', 'components', 'haml')
 
+##
+# Emulates the rails render helper so that nested partials can be rendered
 def render(partial, opts = {})
   case partial
   when Hash
@@ -23,25 +25,45 @@ def render(partial, opts = {})
   end
 end
 
+##
+# Renders a comment in place of a react component.
 def react_component(component_class_name, props={}, html_options={}) 
   "<!-- there should be a #{component_class_name} react_component here -->"
 end
 
-Dir.glob('*.haml', base: HAML_DIR) do |file_name|
-  template_path = File.join(HAML_DIR, file_name)
-  
-  basename = File.basename(template_path, HTML_EXTENSION)
-  locals_path = File.join(STORY_DIR, basename + '.locals.rb')
+# loads all the locals. Downside - getting templates
+# to regenerate when this changes.
+require('./styleguide/components/haml/locals.rb')
+engine = Haml::Engine.new(ARGF.read)
+STDOUT.write(engine.render(Object.new, @locals))
 
-  require(locals_path) # load locals
 
-  File.open(template_path, 'r') do |file|
-    puts "Generating haml for #{file_name}"
-    puts '===================='
-    engine = Haml::Engine.new(file.read)
-    puts engine.render(Object.new, @locals || {})
-    puts '===================='
-  end
-end
+
+# This code takes a haml filename and writes out html to a temp folder
+# but I don't think we're gonna do it this way - piping seems better
+
+# TEMP_DIR = File.join(Dir.pwd, 'tmp')
+
+# def renderPartial(file_name)
+#   template_path = File.join(HAML_DIR, file_name)
   
+#   basename = File.basename(template_path, HTML_EXTENSION)
+#   locals_path = File.join(STORY_DIR, basename + '.locals.rb')
+
+#   require(locals_path) # load locals
+
+#   File.open(template_path, 'r') do |file|
+#     engine = Haml::Engine.new(file.read)
+#     rendered = engine.render(Object.new, @locals || {})
+#     File.open(File.join(TEMP_DIR, "#{basename}.html"), 'w') do |file|
+#       file.write(rendered)
+#     end    
+#   end
+# end
+
+# Dir.glob('*.haml', base: HAML_DIR) do |file_name|
+#   renderPartial(file_name)
+# end
+  
+
     
