7185e530be1e57feca7674134287fcbf56cb86d2
Dan Cseko <dan@baseonmars.co.uk>
2020-04-06 11:02:51 +0100

    testing: Move BackstopJS testing to separate package, some usability updates. (#76)
    * chore: move backstop config and data
    
    * chore: set up new package that includes only backstop
    
    * chore: rename visual_regression_testing to testing
    
    * test: update backstop data
    
    * chore: update references using docker
    
    * build: add script for running visual regression testing
    
    * chore: updated script and instructions
    
    * chore: remove backstopjs from package.json

---
+++ scripts/vr-test.js
@@ -0,0 +1,95 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const childProcess = require('child_process');
+const chalk = require('chalk');
+
+const { log, error } = console;
+const ci = process.argv.some(arg => arg === '--ci');
+
+const baseCommand = 'npx backstop test --docker';
+const command = ci ? `${baseCommand} --config=backstop-ci.json` : baseCommand;
+
+const boldLog = [/report \| \d+/];
+const errorLog = [
+    /compare \| ERROR/,
+    /report \| \*{3} Mismatch errors found \*{3}/
+];
+const positiveLog = [/report \| Test completed.../];
+const neutralLog = [/SCENARIO/, /compare \| OK/, /report \|/];
+
+/*
+ * Prints out white listed log lines with highlighting.
+ *
+ * New lines are already appended to the end of log lines. `chunk`
+ * usually a single line but is sometimes multiline.
+ */
+const printWhitelistedLogLines = chunk => {
+    /* eslint-disable no-restricted-syntax */
+    for (const regexp of boldLog) {
+        if (regexp.test(chunk)) {
+            process.stdout.write(chalk.bold(chunk));
+            return;
+        }
+    }
+    for (const regexp of errorLog) {
+        if (regexp.test(chunk)) {
+            process.stderr.write(chalk.red(chunk));
+            return;
+        }
+    }
+
+    for (const regexp of positiveLog) {
+        if (regexp.test(chunk)) {
+            process.stdout.write(chalk.green(chunk));
+            return;
+        }
+    }
+
+    for (const regexp of neutralLog) {
+        if (regexp.test(chunk)) {
+            // output already has new line at end
+            process.stdout.write(chunk);
+            return;
+        }
+    }
+    /* eslint-enable no-restricted-syntax  */
+};
+
+let started = false;
+
+const storyBook = childProcess.exec(
+    'npm run styleguide -- --ci',
+    [],
+    (err, _, stderr) => {
+        if (err) {
+            error(chalk.red(err));
+        }
+
+        if (stderr) {
+            error(chalk.red(stderr));
+        }
+    }
+);
+
+log(chalk.black('Starting Storybook, this may take a little while...'));
+
+storyBook.stdout.on('data', data => {
+    if (!started && /^webpack built/.test(data)) {
+        started = true;
+        log(chalk.green('Storybook started'));
+        log(chalk.bold('Running BackstopJS visual regression tests'));
+
+        const backstop = childProcess.exec(`cd testing; ${command}`);
+
+        backstop.stdout.on('data', printWhitelistedLogLines);
+
+        backstop.stderr.on('data', backstopError => {
+            error(chalk.red(backstopError));
+        });
+
+        backstop.on('exit', code => {
+            log(chalk.green(`BackstopJS completed with exit code ${code}`));
+            storyBook.kill('SIGINT');
+            process.exit(code);
+        });
+    }
+});
