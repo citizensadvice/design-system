b52258c92c1d7ba28496d16607f428dc0401b0e8
Panos Voudouris <panosvoudouris@gmail.com>
2020-04-20 15:54:44 +0100

    Add close button

--- js/advice-feedback.js
+++ js/advice-feedback.js
@@ -2,7 +2,6 @@ const initAdviceFeedback = () => {
     try {
         const d = document;
         const form = d.getElementById('cads-advice-feedback');
-        form.classList.add('step1');
         const rg = d.getElementsByName('radiogroup-advice-feedback');
 
         rg[0].addEventListener('change', () => {
@@ -18,29 +17,41 @@ const initAdviceFeedback = () => {
             // No
             form.classList.remove('step1');
             form.classList.add('step2');
-            form.addEventListener('submit', e => {
-                e.preventDefault();
-                const moreInfo = d.getElementById(
-                    'advice-feedback-optional-input'
-                ).value;
-                let reason = 'No reason given';
-                const rg2 = d.getElementsByName(
-                    'radiogroup-advice-feedback-step2'
-                );
-                for (let j = 0; j < rg2.length; j++) {
-                    if (rg2[j].checked) {
-                        reason = rg2[j].value;
-                        break;
-                    }
-                }
-
-                form.classList.remove('step2');
-                form.classList.add('step3');
-
-                // TODO Send response to API
-                console.log(reason, moreInfo);
-            });
         });
+
+        form.addEventListener('submit', e => {
+            e.preventDefault();
+            const moreInfo = d.getElementById('advice-feedback-optional-input')
+                .value;
+            let reason = 'No reason given';
+            const rg2 = d.getElementsByName('radiogroup-advice-feedback-step2');
+            for (let j = 0; j < rg2.length; j++) {
+                if (rg2[j].checked) {
+                    reason = rg2[j].value;
+                    break;
+                }
+            }
+
+            form.classList.remove('step2');
+            form.classList.add('step3');
+
+            // TODO Send response to API
+            console.log(reason, moreInfo);
+        });
+
+        const closeButton = form.getElementsByClassName(
+            'cads-advice-feedback__close'
+        )[0];
+        closeButton.addEventListener('click', e => {
+            e.preventDefault();
+            rg[0].checked = false;
+            rg[1].checked = false;
+            form.classList.add('step1');
+            form.classList.remove('step2');
+            form.classList.remove('step3');
+        });
+
+        form.classList.add('step1');
     } catch (e) {
         console.warn(`Could not initialise advice feedback component. ${e}`);
     }
