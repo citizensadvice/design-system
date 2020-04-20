ef694cec8e399a9e03d08ef7164d005d1fd6c3c2
Shahzain Badruddin <6561371+shahzainb@users.noreply.github.com>
2020-04-20 10:33:22 +0100

    Fix failing publishing backstopjs report when tests fail (#91)

--- scripts/vr-test.js
+++ scripts/vr-test.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const childProcess = require('child_process');
 const chalk = require('chalk');
+const http = require('http');
 
 const { log, error } = console;
 const ci = process.argv.some(arg => arg === '--ci');
@@ -54,42 +55,83 @@ const printWhitelistedLogLines = chunk => {
     /* eslint-enable no-restricted-syntax  */
 };
 
+const sleep = ms => {
+    return new Promise(resolve => {
+        setTimeout(resolve, ms);
+    });
+};
+
 let started = false;
 
-const storyBook = childProcess.exec(
-    'npm run styleguide -- --ci',
-    { maxBuffer: 1024 * 5000 },
-    (err, _, stderr) => {
-        if (err) {
-            error(chalk.red(err));
-        }
+const isDockerStorybookRunning = () => {
+    return new Promise(resolve => {
+        http.get('http://ca-styleguide.test:6006', res => {
+            if (res.statusCode === 200) {
+                resolve(true);
+            } else {
+                resolve(false);
+            }
+        }).on('error', e => {
+            resolve(false);
+        });
+    });
+};
 
-        if (stderr) {
-            error(chalk.red(stderr));
-        }
-    }
-);
+const runBackstopTests = () => {
+    log(chalk.green('Running BackstopJS visual regression tests'));
 
-log(chalk.black('Starting Storybook, this may take a little while...'));
+    const backstop = childProcess.exec(`cd testing; ${command}`);
 
-storyBook.stdout.on('data', data => {
-    if (!started && /^webpack built/.test(data)) {
-        started = true;
+    backstop.stdout.on('data', printWhitelistedLogLines);
+
+    backstop.stderr.on('data', backstopError => {
+        error(chalk.red(backstopError));
+    });
+
+    backstop.on('exit', code => {
+        log(chalk.green(`BackstopJS completed with exit code ${code}`));
+        if (!ci) storyBook.kill('SIGINT');
+        process.exit(code);
+    });
+};
+
+const executeCI = async () => {
+    // check if docker storybook is already running
+    if (await isDockerStorybookRunning()) {
+        log(chalk.green('Storybook already started'));
+        runBackstopTests();
+    } else {
+        childProcess.exec('./bin/docker/start');
+        await sleep(120000); // 2 minutes
         log(chalk.green('Storybook started'));
-        log(chalk.green('Running BackstopJS visual regression tests'));
-
-        const backstop = childProcess.exec(`cd testing; ${command}`);
-
-        backstop.stdout.on('data', printWhitelistedLogLines);
-
-        backstop.stderr.on('data', backstopError => {
-            error(chalk.red(backstopError));
-        });
-
-        backstop.on('exit', code => {
-            log(chalk.green(`BackstopJS completed with exit code ${code}`));
-            storyBook.kill('SIGINT');
-            process.exit(code);
-        });
+        runBackstopTests();
     }
-});
+};
+
+log(chalk.green('Starting Storybook, this may take a little while...'));
+
+if (ci) {
+    executeCI();
+} else {
+    const storyBook = childProcess.exec(
+        'npm run styleguide -- --ci',
+        { maxBuffer: 1024 * 5000 },
+        (err, _, stderr) => {
+            if (err) {
+                error(chalk.red(err));
+            }
+
+            if (stderr) {
+                error(chalk.red(stderr));
+            }
+        }
+    );
+
+    storyBook.stdout.on('data', data => {
+        if (!started && /^webpack built/.test(data)) {
+            started = true;
+            log(chalk.green('Storybook started'));
+            runBackstopTests();
+        }
+    });
+}
