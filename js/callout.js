afd327d5295490dd12dafa245d11d0f80b2e0dc7
Panos Voudouris <panos.voudouris@citizensadvice.org.uk>
2020-04-23 15:19:27 +0100

    NP-238 Callout Headings (#102)
    Tested and approved!

---
+++ js/callout.js
@@ -0,0 +1,21 @@
+const initCallouts = () => {
+    try {
+        const h2s = document.getElementsByTagName('h2');
+        if (!h2s || !h2s.length) {
+            // Only do this if we don't have an H2 in the page
+            const callouts = document.getElementsByClassName('cads-callout');
+            if (callouts && callouts.length > 0) {
+                const callout = callouts[0];
+                const h3ToReplace = callout.getElementsByTagName('h3')[0];
+                const heading = h3ToReplace.innerHTML;
+                const newHeading = document.createElement('h2');
+                newHeading.innerHTML = heading;
+                h3ToReplace.parentNode.replaceChild(newHeading, h3ToReplace);
+            }
+        }
+    } catch (e) {
+        console.log(`Could not initialise callouts ${e}`);
+    }
+};
+
+export default initCallouts;
