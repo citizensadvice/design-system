e5cd4b9f0a33314eb8d9ff686024d5dc9eaba5a1
Panos Voudouris <panos.voudouris@citizensadvice.org.uk>
2020-05-12 18:05:53 +0100

    NP-274 print styles (#123)
    * Basic print styles
    
    * Print styles
    
    * Print margins
    
    * Remove callout border
    
    * Fix footer print style
    
    * Fix logo printing

--- js/targeted-content.js
+++ js/targeted-content.js
@@ -3,6 +3,7 @@ const initTargetedContent = () => {
         const content = document.getElementsByClassName(
             'cads-targeted-content'
         );
+
         for (let i = 0; i < content.length; i++) {
             const item = content[i];
             const summary = item.getElementsByClassName(
@@ -12,20 +13,33 @@ const initTargetedContent = () => {
             summary.addEventListener('click', () => {
                 if (item.classList.contains('is-open')) {
                     item.classList.remove('is-open');
-                    summary.ariaExpanded = false;
+                    summary.setAttribute('aria-expanded', false);
                 } else {
                     item.classList.add('is-open');
-                    summary.ariaExpanded = true;
+                    summary.setAttribute('aria-expanded', true);
                 }
             });
             item.getElementsByClassName(
                 'cads-targeted-content__close-button'
             )[0].addEventListener('click', () => {
                 item.classList.remove('is-open');
-                summary.ariaExpanded = false;
+                summary.setAttribute('aria-expanded', false);
                 item.open = false;
             });
         }
+
+        // eslint-disable-next-line
+        window.addEventListener('beforeprint', () => {
+            for (let i = 0; i < content.length; i++) {
+                const item = content[i];
+                const summary = item.getElementsByClassName(
+                    'cads-targeted-content__summary'
+                )[0];
+                if (!item.classList.contains('is-open')) {
+                    summary.click();
+                }
+            }
+        });
     } catch (e) {
         console.warn(`Could not initialise targeted content ${e}`);
     }
