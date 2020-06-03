3e3fe11896e69ca34a2c6154d8ee3ad01c81f4f9
Panos Voudouris <panosvoudouris@gmail.com>
2020-06-03 11:36:21 +0100

    NP-332 Remove title from callout

--- styleguide/haml_locals.rb
+++ styleguide/haml_locals.rb
@@ -20,8 +20,7 @@
 
   'notice' => {
     'type' => 'urgent',
-    'title' => 'Callout title: Urgent',
-    'body' => '<p>The important callout should be used for any important snippet of text that has serious and/or legal implications if the client does not follow the advice.</p>'
+    'body' => '<h3>This is the callout title</h3><p>The important callout should be used for any important snippet of text that has serious and/or legal implications if the client does not follow the advice.</p>'
   },
 
   'root_path' => 'root_path',
