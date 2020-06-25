783c5394841a4b58d12c9c17b6216f208c3b4f8c
Panos Voudouris <panosvoudouris@gmail.com>
2020-06-25 10:59:32 +0100

    Tertiary button and expand all functionality

--- src/js/targeted-content.js
+++ src/js/targeted-content.js
@@ -1,9 +1,13 @@
 const initTargetedContent = () => {
     try {
-        const content = document.getElementsByClassName(
+        const pageContent = document.getElementsByClassName(
+            'cads-main-content'
+        )[0];
+        const content = pageContent.getElementsByClassName(
             'cads-targeted-content'
         );
 
+        // Expand/collapse handlers for individual items
         for (let i = 0; i < content.length; i++) {
             const item = content[i];
             const summary = item.getElementsByClassName(
@@ -28,6 +32,7 @@ const initTargetedContent = () => {
             });
         }
 
+        // Print setup
         // eslint-disable-next-line
         window.addEventListener('beforeprint', () => {
             for (let i = 0; i < content.length; i++) {
@@ -40,6 +45,25 @@ const initTargetedContent = () => {
                 }
             }
         });
+
+        // Expand all for adviser
+        const h1 = pageContent.getElementsByTagName('h1')[0];
+        const b = document.createElement('buton');
+        b.innerHTML = 'Expand all<span class="cads-icon-plus"></span>';
+        b.setAttribute('type', 'button');
+        b.classList.add('cads-targeted-content__expand-all');
+        b.addEventListener('click', () => {
+            for (let i = 0; i < content.length; i++) {
+                const item = content[i];
+                const summary = item.getElementsByClassName(
+                    'cads-targeted-content__summary'
+                )[0];
+                if (!item.classList.contains('is-open')) {
+                    summary.click();
+                }
+            }
+        });
+        h1.parentNode.insertBefore(b, h1.nextSibling);
     } catch (e) {
         console.warn(`Could not initialise targeted content ${e}`);
     }
