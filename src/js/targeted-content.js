e37e41f85f04eedd14c5081b2b579fe4dde4ba9f
Panos Voudouris <panos.voudouris@citizensadvice.org.uk>
2020-06-30 10:13:36 +0100

    feature: Adviser callout and Expand All [NP-470][NP-477] (#156)
    * Adviser callout styles
    
    * Tertiary button and expand all functionality
    
    * VRT updates
    
    * Expand all adviser  button
    
    * Add adviser callout in normal callout example
    
    * Fix background colour mixing
    
    * Add top margin to nested callout
    
    * Only show expand button if targeted content present
    
    * Expand/collapse all
    
    * PR updates
    
    * Fix expand/collapse handler

--- src/js/targeted-content.js
+++ src/js/targeted-content.js
@@ -1,9 +1,14 @@
 const initTargetedContent = () => {
     try {
-        const content = document.getElementsByClassName(
+        const pageContents = document.getElementsByClassName(
+            'cads-main-content'
+        );
+        const pageContent = pageContents?.[0] ?? document;
+        const content = pageContent.getElementsByClassName(
             'cads-targeted-content'
         );
 
+        // Expand/collapse handlers for individual items
         for (let i = 0; i < content.length; i++) {
             const item = content[i];
             const summary = item.getElementsByClassName(
@@ -28,6 +33,7 @@ const initTargetedContent = () => {
             });
         }
 
+        // Print setup
         // eslint-disable-next-line
         window.addEventListener('beforeprint', () => {
             for (let i = 0; i < content.length; i++) {
@@ -40,6 +46,56 @@ const initTargetedContent = () => {
                 }
             }
         });
+
+        // Expand all button for adviser if content present
+        if (content.length > 0) {
+            const h1 = pageContent.getElementsByTagName('h1')[0];
+            const b = document.createElement('buton');
+            b.innerHTML = 'Expand all<span class="cads-icon-plus"></span>';
+            b.setAttribute('type', 'button');
+            b.classList.add('cads-targeted-content__expand-all');
+            b.addEventListener('click', e => {
+                let expandAll = false;
+                const { target } = e;
+                let t = target;
+
+                if (
+                    target.innerText &&
+                    target.innerText.indexOf('Expand all') !== -1
+                ) {
+                    expandAll = true;
+                }
+                if (target.className.indexOf('cads-icon-plus') !== -1) {
+                    expandAll = true;
+                    t = target.parentNode;
+                }
+                if (target.className.indexOf('cads-icon-minus') !== -1) {
+                    t = target.parentNode;
+                }
+
+                if (expandAll) {
+                    t.innerHTML =
+                        'Collapse all<span class="cads-icon-minus"></span>';
+                } else {
+                    t.innerHTML =
+                        'Expand all<span class="cads-icon-plus"></span>';
+                }
+
+                for (let i = 0; i < content.length; i++) {
+                    const item = content[i];
+                    const summary = item.getElementsByClassName(
+                        'cads-targeted-content__summary'
+                    )[0];
+                    if (
+                        (expandAll && !item.classList.contains('is-open')) ||
+                        (!expandAll && item.classList.contains('is-open'))
+                    ) {
+                        summary.click();
+                    }
+                }
+            });
+            h1.parentNode.insertBefore(b, h1.nextSibling);
+        }
     } catch (e) {
         console.warn(`Could not initialise targeted content ${e}`);
     }
--- styleguide/components/buttons.js
+++ styleguide/components/buttons.js
@@ -2,10 +2,11 @@ import wrapper from '../component-wrapper';
 
 const buttons = () => {
     const content = 'This is a button';
-    const component = `<h4>Primary button</h4><button type="button">${content}</button>
-<h4>Primary button</h4><button class="cads-button" type="button">${content}</button>
+    const component = `<h4>Default button style</h4><button type="button">${content}</button>
+<h4>With .cads-bbutton class</h4><button class="cads-button" type="button">${content}</button>
 <h4>Primary button</h4><button class="cads-button cads-button__primary" type="button">${content}</button>
-<h4>Secondary button</h4><button class="cads-button cads-button__secondary" type="button">${content}</button>`;
+<h4>Secondary button</h4><button class="cads-button cads-button__secondary" type="button">${content}</button>
+<h4>Tertiary button</h4><button class="cads-button cads-button__tertiary" type="button">${content}</button>`;
     return wrapper('Buttons', component);
 };
 
