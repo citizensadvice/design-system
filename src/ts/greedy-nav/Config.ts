78ec53739261db59d1e259affe7e2bfe2f3865c6
Dan Cseko <dan@baseonmars.co.uk>
2020-07-02 12:05:30 +0100

    chore: add ts configuration and placeholder project

---
+++ src/ts/greedy-nav/Config.ts
@@ -0,0 +1,10 @@
+// this is just a placeholder file
+interface Config {
+    navSelector: string;
+}
+
+const defaultConfig: Config = {
+    navSelector: 'greedy-nav'
+};
+
+export { defaultConfig, Config };
--- src/ts/greedy-nav/index.ts
+++ src/ts/greedy-nav/index.ts
@@ -1,7 +1,12 @@
+// this is just a placeholder file
+
 import { Config, defaultConfig } from './Config';
 
 class GreedyNav {
     constructor(config: Config) {
         Object.assign(defaultConfig, config);
+        console.log('greedy-nav loaded');
     }
 }
+
+export default GreedyNav;
