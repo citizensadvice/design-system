e2f62fbbd5db122e2869b39fa25b2d216936a270
Panos Voudouris <panosvoudouris@gmail.com>
2020-07-03 12:02:54 +0100

    Table examples

--- styleguide/haml_locals.rb
+++ styleguide/haml_locals.rb
@@ -4,6 +4,31 @@
 
 @locals = {
   'table_data' => {
+    'caption' => 'Post box collection times (Monday to Friday)',
+    'header' => [
+      'Your location',
+      'Post box collection times'
+    ],
+    'rows' => [
+      [
+        'City or town',
+        '9am to 6.30pm'
+      ],
+      [
+        'Areas with lots of businesses - known as commercial',
+        '9am to 7.30pm'
+      ],
+      [
+        'Very rural areas - for example, where there aren\'t many people',
+        '9am to 4pm'
+      ],
+      [
+        'Rest of the UK',
+        '9am to 5.30pm'
+      ]
+    ]
+  },
+  'table_data_long' => {
     'caption' => 'Client under pension age',
     'header' => [
       'Rate',
