57401dd91d65fa27b28b4009542b8d15221c2c9e
Panos Voudouris <panosvoudouris@gmail.com>
2020-07-07 12:36:41 +0100

    Remove border mixin

--- styleguide/haml_locals.rb
+++ styleguide/haml_locals.rb
@@ -181,7 +181,7 @@
   'targetContent' => {
     'id' => 'target-content-123',
     'title' => 'If you are a citizen of a country outside the EU, EEA or Switzerland',
-    'body' => '<p>You should apply to the EU Settlement Scheme if both =></p>
+    'body' => '<p>You should apply to the EU Settlement Scheme if both:</p>
   <ul>
   <li>you’re in the UK by 31 December 2020</li>
   <li>you have family in the UK from the EU, EEA or Switzerland</li>
