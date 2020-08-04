017cb668500fa9484ababfcf1b1b891d3d963d15
Dan Cseko <dan@baseonmars.co.uk>
2020-08-04 08:26:14 +0100

    chore: remove helpers file

--- src/ts/greedy-nav/GreedyNav.ts
+++ src/ts/greedy-nav/GreedyNav.ts
@@ -1,9 +1,24 @@
-import { blurEventName } from './helpers';
 import { Config, defaultConfig } from './Config';
 
 const root = window;
 
 const supports = !!document.querySelector && !!root.addEventListener; // Feature test
+
+/**
+ * Provides backwards compatibility with IE 11.
+ *
+ * If we want to access the relatedTarget of an event we should use
+ * a focusout event as per the spec, but in IE 11 we need to use blur.
+ *
+ * This constant should be used in place of focusout/blur when assigning
+ * event handlers.
+ */
+const blurEventName = Object.prototype.hasOwnProperty.call(
+    MouseEvent,
+    'relatedTarget'
+)
+    ? 'focusout'
+    : 'blur';
 
 /**
  * Get the closest matching element up the DOM tree
---
+++ src/ts/greedy-nav/index.ts
@@ -0,0 +1,5 @@
+import GreedyNav, { GreedyNavMenu } from './GreedyNav';
+
+export { defaultConfig, Config } from './Config';
+export default GreedyNav;
+export { GreedyNavMenu };
