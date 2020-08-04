11a222042c377c3bb44f99405ea31e3152162cf5
Dan Cseko <dan@baseonmars.co.uk>
2020-08-04 10:44:33 +0100

    chore: fix type exports

--- src/ts/greedy-nav/index.ts
+++ src/ts/greedy-nav/index.ts
@@ -1,5 +1,6 @@
 import GreedyNav, { GreedyNavMenu } from './GreedyNav';
 
-export { defaultConfig, Config } from './Config';
+export { defaultConfig } from './Config';
+
 export default GreedyNav;
 export { GreedyNavMenu };
