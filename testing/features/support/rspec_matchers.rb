ff60fb28067eeee67b7c74b843b431456e38dba3
Luke Hill <lukehill_uk@hotmail.com>
2020-08-24 10:18:01 +0100

    Add in refactor to assertiosn

--- testing/features/support/rspec_matchers.rb
+++ testing/features/support/rspec_matchers.rb
@@ -1,30 +1,41 @@
 # frozen_string_literal: true
 
 RSpec::Matchers.define :have_all_correct_navigation_titles do
-  match do |actual|
-    actual ==
-      [
-        "Benefits",
-        "Work",
-        "Debt and money",
-        "Consumer",
-        "Housing",
-        "Family",
-        "Law and courts",
-        "Immigration",
-        "Health"
-      ]
+  match { |actual| actual == correct_titles }
+
+  failure_message do |actual|
+    "Expected:\n#{correct_titles}\nBut Selenium found:\n#{actual}"
+  end
+
+  def correct_titles
+    [
+      "Benefits",
+      "Work",
+      "Debt and money",
+      "Consumer",
+      "Housing",
+      "Family",
+      "Law and courts",
+      "Immigration",
+      "Health",
+      "More from us"
+    ]
   end
 end
 
 RSpec::Matchers.define :have_all_correct_footer_titles do
-  match do |actual|
-    actual ==
-      [
-        "Advice",
-        "Resources and tools",
-        "More from us",
-        "About Citizens Advice"
-      ]
+  match { |actual| actual == correct_titles }
+
+  failure_message do |actual|
+    "Expected:\n#{correct_titles}\nBut Selenium found:\n#{actual}"
+  end
+
+  def correct_titles
+    [
+      "Advice",
+      "Resources and tools",
+      "More from us",
+      "About Citizens Advice"
+    ]
   end
 end
