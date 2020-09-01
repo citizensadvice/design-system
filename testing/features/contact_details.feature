36c0b24b09d442a5a8a9a00cc6f2ec8b89799254
Luke Hill <lukehill_uk@hotmail.com>
2020-09-01 10:08:06 +0100

    Remove unrequired assertion

--- testing/features/contact_details.feature
+++ testing/features/contact_details.feature
@@ -7,8 +7,7 @@ Feature: Contact Details component
     Given a Contact Details component is on the page
 
   Scenario: Validate initial state
-    Then the header says "Contact Details"
-    And a contact details block of text is present
+    Then the header says "Contact details"
 
   Scenario: Ensure component is of correct size/format
     Then there is at least 1 paragraph
--- testing/features/step_definitions/contact_details_steps.rb
+++ testing/features/step_definitions/contact_details_steps.rb
@@ -5,14 +5,10 @@ Given("a Contact Details component is on the page") do
   @component.load
 end
 
-Then("a contact details block of text is present") do
-  expect(@component.initial_form).to have_heading
-end
-
 Then("there is at least 1 paragraph") do
   expect(@component.initial_form).to have_paragraphs
 end
 
 Then("the top paragraph is bold") do
-  expect(@component.initial_form.paragraphs.first).to be_bold
+  expect(@component.initial_form).to have_first_bold_paragraph
 end
