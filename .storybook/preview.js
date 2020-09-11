ac2e252143f94b08cd7275fc64dc0eb0b49f75da
David Rapson <david.rapson@citizensadvice.org.uk>
2020-09-11 12:36:57 +0100

    Upgrade Storybook to v6
    From the docs:
    
    > presets.js has been renamed to main.js. main.js is the main point of configuration for storybook.
    > config.js has been renamed to preview.js. preview.js configures the "preview" iframe that renders your components.
    > addons.js has been renamed to manager.js. manager.js configures Storybook's "manager" UI that wraps the preview, and also configures addons panel.
    
    In addition to structure changes the following config changes were
    needed
    
    - v6 support zero-config Typescript so storybook/preset-typescript was
      removed
    - withA11y is no longer required, this code is injected by default with
      the addon

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,4 @@
-import { configure, addDecorator, addParameters } from '@storybook/html';
-import theme from './theme';
+import { addDecorator, addParameters } from '@storybook/html';
 
 addDecorator(
     storyFn => `<div class="cads-styleguide__wrapper">
@@ -15,10 +14,6 @@ addParameters({
         isToolshown: false,
         showPanel: false,
         panelPosition: 'bottom',
-        storySort: (a, b) => a[1].id.localeCompare(b[1].id),
-        theme
+        storySort: (a, b) => a[1].id.localeCompare(b[1].id)
     }
 });
-
-// automatically import all files ending in *.stories.js
-configure(require.context('../styleguide', true, /\.stories\.js$/), module);
--- .storybook/theme.js
+++
@@ -1,7 +0,0 @@
-import { create } from '@storybook/theming/create';
-import data from '../package.json';
-
-export default create({
-    base: 'light',
-    brandTitle: `Citizens Advice Design System v${data.version}`
-});
