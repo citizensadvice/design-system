b69b80f9767a1d14adab95816502814e247bf642
David Rapson <david.rapson@citizensadvice.org.uk>
2020-09-15 12:44:23 +0100

    Initial button animation

--- styleguide/components/buttons.js
+++ styleguide/components/buttons.js
@@ -1,11 +1,10 @@
 import wrapper from '../component-wrapper';
 
-const buttons = () => {
-  const content = 'This is a button';
-  const component = `<h4>Primary button style (also default and with just .cads-button class)</h4><button type="button">${content}</button>
-<h4>Secondary button</h4><button class="cads-button cads-button__secondary" type="button">${content}</button>
-<h4>Tertiary button</h4><button class="cads-button cads-button__tertiary" type="button">${content}</button>`;
-  return wrapper('Buttons', component);
-};
-
-export default buttons;
+export default function buttons() {
+  return wrapper(
+    'Buttons',
+    `<p><button type="button">Primary button</button></p>
+<p><button class="cads-button cads-button__secondary" type="button">Secondary button</button></p>
+<p><button class="cads-button cads-button__tertiary" type="button">Tertiary button</button></p>`
+  );
+}
