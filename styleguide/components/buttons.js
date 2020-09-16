658e93e35ceebc70f00284f5e5f91fbdcf0bb79b
David Rapson <david.rapson@citizensadvice.org.uk>
2020-09-16 16:34:40 +0100

    Redesigned buttons
    New button style based w/affordance based on NHS design system buttons

--- styleguide/components/buttons.js
+++ styleguide/components/buttons.js
@@ -3,7 +3,7 @@ import wrapper from '../component-wrapper';
 export default function buttons() {
   return wrapper(
     'Buttons',
-    `<p><button type="button">Primary button</button></p>
+    `<p><button type="button" class="cads-button">Primary button</button></p>
 <p><button class="cads-button cads-button__secondary" type="button">Secondary button</button></p>
 <p><button class="cads-button cads-button__tertiary" type="button">Tertiary button</button></p>`
   );
