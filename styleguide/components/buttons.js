e102e395c8ff2f23b3eb37c3b7ea6675f268024b
David Rapson <david.rapson@citizensadvice.org.uk>
2020-09-17 17:18:21 +0100

    Decouple linkbutton and pagination from sharing _all_ button styles

--- styleguide/components/buttons.js
+++ styleguide/components/buttons.js
@@ -4,7 +4,7 @@ export default function buttons() {
   return wrapper(
     'Buttons',
     `<p><button type="button" class="cads-button cads-button__primary">Primary button</button></p>
-<p><button class="cads-button cads-button__secondary" type="button">Secondary button</button></p>
-<p><button class="cads-button cads-button__tertiary" type="button">Tertiary button</button></p>`
+<p><button type="button"class="cads-button cads-button__secondary">Secondary button</button></p>
+<p><button type="button" class="cads-button cads-button__tertiary">Tertiary button</button></p>`
   );
 }
