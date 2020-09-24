b64c133b200d9d0667cc26cdc7ef7dc7405ef9e5
David Rapson <david.rapson@citizensadvice.org.uk>
2020-09-24 17:29:07 +0100

    Add initial OISC component with docs

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
-    stories: ['../**/*.stories.js'],
-    addons: ['@storybook/addon-a11y']
+  stories: ['../**/*.stories.@(js|mdx)'],
+  addons: ['@storybook/addon-a11y', '@storybook/addon-docs'],
 };
