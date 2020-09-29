7430b6a67afa21912342040740418c5d3cae3a72
Luke Hill <lukehill_uk@hotmail.com>
2020-09-29 11:28:20 +0100

    Initial feature/scenario and supporting code

---
+++ testing/features/step_definitions/breadcrumbs_steps.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+Given("a Breadcrumbs component is on the page") do
+  @component = DesignSystem::Breadcrumbs.new
+  @component.load
+end
+
+Then("a series of breadcrumbs are present") do
+  expect(@component.initial_form).to have_breadcrumbs
+end
+
+Then("the initial breadcrumbs are all links") do
+  expect(@component.initial_form.all_but_last_breadcrumb).to have_link
+end
+
+Then("the final breadcrumb isn't a link") do
+  expect(@component.initial_form.breadcrumbs.last).not_to have_link
+end
