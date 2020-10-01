d6e2c601302bfeaafff3fb259403d6ee04db0fe7
David Rapson <david@davidrapson.co.uk>
2020-10-01 09:02:33 +0100

    Backstop config cleanup (#252)
    * Remove unused chromy scripts
    * Move engine_scripts up, allow linting and formatting
    * Emulate prefers-reduced-motion instead of overriding CSS
    * Remove unused engine_scripts
    * Remove redundant cookies engine script
    * Remove unnecessary defaults
    * Flatten engine_scripts

--- testing/visual-regression/backstop_data/engine_scripts/puppet/onReady.js
+++
@@ -1,7 +0,0 @@
-module.exports = async (page, scenario, vp) => {
-    console.log('SCENARIO > ' + scenario.label);
-    await require('./overrideCSS')(page, scenario);
-    await require('./clickAndHoverHelper')(page, scenario);
-
-    // add more ready handlers here...
-};
