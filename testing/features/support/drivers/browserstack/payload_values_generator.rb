90329c3df34a5628fcbf462421a4b12c1a8ed236
Luke Hill <lukehill_uk@hotmail.com>
2020-10-06 15:49:40 +0100

    As build iteration isn't readily available, just use SHA ref.
    This then fixes the session name for master/branches

--- testing/features/support/drivers/browserstack/payload_values_generator.rb
+++ testing/features/support/drivers/browserstack/payload_values_generator.rb
@@ -9,9 +9,9 @@ module Drivers
 
         def build_name
           if master?
-            "Design System - #{pr_sha_reference} - URL: #{base_url}"
+            "Design System - #{pr_with_sha} - URL: #{base_url}"
           elsif branch?
-            "Design System - #{pr_without_build_iteration} - URL: #{base_url}"
+            "Design System - #{pr_without_sha} - URL: #{base_url}"
           else
             "Local Machine run - Month #{Time.now.month} - Ignore results!"
           end
@@ -19,7 +19,7 @@ module Drivers
 
         def session_name
           if branch? || master?
-            "Build: #{build_iteration} - CALLING_PROJECT: design-system"
+            "SHA: #{sha} - CALLING_PROJECT: design-system"
           else
             "Local run: Ran at #{timestamp}"
           end
@@ -36,17 +36,18 @@ module Drivers
         end
 
         # Example: PR-284
-        def pr_without_build_iteration
-          pr_sha_reference.split("_").first
+        def pr_without_sha
+          pr_with_sha.split("_").first
         end
 
-        def build_iteration
-          pr_sha_reference.split("_").last
+        # Example: ad4b223
+        def sha
+          pr_with_sha.split("_").last
         end
 
         # Example: master_ad4b223
         # Example: PR-284_dbbb8b6
-        def pr_sha_reference
+        def pr_with_sha
           @pr_sha_reference ||= docker_tag.split("design-system/").last
         end
       end
