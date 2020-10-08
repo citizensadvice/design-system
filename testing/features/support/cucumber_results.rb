ee328553b824209606cb2349080a6dd7c88c2003
Simon W. Gill <simon.gill@citizensadvice.org.uk>
2020-10-08 14:00:49 +0100

    Stylistic Tweaks

--- testing/features/support/cucumber_results.rb
+++ testing/features/support/cucumber_results.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require "retriable"
-
 class CucumberResults
   include Singleton
   include Helpers::EnvVariables
@@ -10,22 +8,31 @@ class CucumberResults
   attr_writer :status
 
   def set_session_id
-    log_exception = proc do |exception, try, elapsed_time, next_interval|
-      AutomationLogger.error(
-        "#{exception.class}: '#{exception.message}' - #{try} tries in #{elapsed_time} seconds and #{next_interval} seconds until the next "\
-        "try.\n#{exception.full_message}}"
-      )
-    end
-
-    Retriable.retriable tries: 3, on_retry: log_exception do
+    Retriable.retriable(
+      on: Selenium::WebDriver::Error::WebDriverError,
+      tries: 5,
+      base_interval: 1, # in seconds
+      on_retry: method(:log_exception)
+    ) do
       # The call to Capybara.page.driver.browser can fail with the exception "no sessionId in returned payload"
-      # when the remote end of the Selenium driver fails to provide a session_id. This should be retried as it is
-      # likely to be an intermittent failure
+      # This is caused by the response to create_session in Selenium's remote WebDriver not returning a payload
+      # when being served by the Browserstack Hub infrastructure.
+
+      # This should be retried as it's an intermittent failure from Browserstack
       @session_id = Capybara.page.driver.browser.session_id if browserstack?
     end
   end
 
   def status
     @status ||= "passed"
+  end
+
+  private
+
+  def log_exception(exception, try, elapsed_time, next_interval)
+    AutomationLogger.error(
+      "#{exception.class}: '#{exception.message}' - #{try} tries in #{elapsed_time} seconds and #{next_interval} seconds until the next "\
+      "try.\n#{exception.full_message}}"
+    )
   end
 end
