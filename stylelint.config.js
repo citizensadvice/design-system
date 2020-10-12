5ff856928153d80a029f6662c093d7b463d59c38
David Rapson <david.rapson@citizensadvice.org.uk>
2020-10-12 12:51:52 +0100

    Streamline and document stylelint rules
    - Add comments for all stylelint rules
    - Require that @mixin and @function declarations start with cads-
    - Add tests for any regexps in config
    - Document BEM lint check, this previously never worked
    - Include warning check for styling IDs
    - Include warning check for @extend without a placeholder

--- stylelint.config.js
+++ stylelint.config.js
@@ -3,35 +3,48 @@ module.exports = {
   plugins: ['stylelint-scss', 'stylelint-selector-bem-pattern'],
   ignoreFiles: ['**/_normalize.scss', '**/_cads-iconfont.scss'],
   rules: {
-    'at-rule-disallowed-list': ['debug'],
-    'at-rule-no-unknown': null,
-    'at-rule-no-vendor-prefix': true,
-    'block-closing-brace-newline-after': null,
     'color-named': 'never',
-    'declaration-colon-newline-after': null,
-    'property-disallowed-list': ['transition'],
-    'scss/at-if-closing-brace-newline-after': 'always-last-in-chain',
-    'scss/at-function-pattern': '^[a-z]+([a-z0-9-]+[a-z0-9]+)?$',
-    'scss/at-import-no-partial-leading-underscore': true,
-    'scss/at-import-partial-extension-blacklist': ['scss'],
-    'scss/at-mixin-pattern': '^[a-z]+([a-z0-9-]+[a-z0-9]+)?$',
-    'scss/dollar-variable-colon-space-after': 'always',
-    'scss/dollar-variable-colon-space-before': 'never',
-    'scss/selector-no-redundant-nesting-selector': true,
-    'selector-max-compound-selectors': 6,
+    'selector-max-compound-selectors': 4,
     'selector-no-vendor-prefix': true,
     'value-no-vendor-prefix': true,
-    'plugin/selector-bem-pattern': {
-      preset: 'bem',
-      componentName: 'ds-(([a-z0-9]+(?!-$)-?)+)',
-      componentSelectors: {
-        initial:
-          "\\.{componentName}(((__|--)(([a-z0-9\\[\\]'=]+(?!-$)-?)+))+)?$",
-      },
-    },
+
+    // We provide a cads-transition-animation mixin for handling
+    // transitions along with a prefers-reduced-motion fallback,
+    // so disallow this property when used without this mixin.
+    'property-disallowed-list': ['transition'],
+
     'font-family-no-missing-generic-family-keyword': [
       true,
+      // Allow cads icon font
       { ignoreFontFamilies: ['/cads/'] },
     ],
+
+    // Default at-rule conflicts with scss features,
+    // replace with stylelint-scss version
+    'at-rule-no-unknown': null,
+    'at-rule-disallowed-list': ['debug'],
+    'scss/at-rule-no-unknown': true,
+
+    // Require that @function and @mixin rules are prefixed with cads-
+    'scss/at-function-pattern': '^cads-+([a-z0-9-]+[a-z0-9]+)?$',
+    'scss/at-mixin-pattern': '^cads-+([a-z0-9-]+[a-z0-9]+)?$',
+
+    // Additional stylelint-scss rules
+    'scss/selector-no-redundant-nesting-selector': true,
+    'scss/at-import-no-partial-leading-underscore': true,
+    'scss/at-import-partial-extension-blacklist': ['scss'],
+
+    // Warning-only rules
+    // These should be enabled as errors once current warnings have been fixed.
+    'selector-max-id': [0, { severity: 'warning' }],
+    'scss/at-extend-no-missing-placeholder': [true, { severity: 'warning' }],
+
+    // Configure BEM pattern
+    // Note: requires components to have an explicit @define comment
+    // e.g. cads-breadcrumb would use /** @define breadcrumb */
+    'plugin/selector-bem-pattern': {
+      preset: 'bem',
+      presetOptions: { namespace: 'cads' },
+    },
   },
 };
