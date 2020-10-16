360b33acc26cb778c558cfd5120a330346083c91
David Rapson <david.rapson@citizensadvice.org.uk>
2020-10-16 12:26:46 +0100

    Add test for header behaviour

---
+++ src/js/header.test.js
@@ -0,0 +1,46 @@
+/* eslint-env jest */
+import { screen } from '@testing-library/dom';
+import '@testing-library/jest-dom/extend-expect';
+
+import initHeader from './header';
+
+const searchFormHtml = `<form action='/the/search-action-url' class='cads-search' role='search'>
+  <input aria-label='Search through site content' name='q' type='search'>
+  <button class='cads-button cads-search__button' title='Submit search query'></button>
+</form>`;
+
+const minimalHeaderHtml = `<header class='cads-header' data-testid="header">
+  <div class='cads-grid-container'>
+    <div class='cads-grid-row'>
+      <div class='cads-grid-col-md-5 cads-header-logo-row'>
+        <a class='cads-logo' href='root_path'
+          title='Citizens Advice homepage'></a>
+        <button aria-expanded='false'
+          class='cads-search-reveal cads-icon_search'
+          title='Open search'></button>
+      </div>
+      <div class='cads-grid-col-md-7 cads-align-right cads-header-search-row'>
+        ${searchFormHtml}
+      </div>
+    </div>
+  </div>
+</header>`;
+
+test('allow toggling search', () => {
+  document.body.innerHTML = minimalHeaderHtml;
+  initHeader();
+
+  const headerEl = screen.getByTestId('header');
+  const controlButtonEl = screen.getByTitle('Open search');
+
+  expect(headerEl).not.toHaveClass('cads-header-show-search');
+  expect(controlButtonEl).toHaveAttribute('aria-expanded', 'false');
+  expect(controlButtonEl).toHaveClass('cads-icon_search');
+
+  controlButtonEl.click();
+
+  expect(headerEl).toHaveClass('cads-header-show-search');
+  expect(controlButtonEl.title).toBe('Close search');
+  expect(controlButtonEl).toHaveAttribute('aria-expanded', 'true');
+  expect(controlButtonEl).toHaveClass('cads-icon_close');
+});
