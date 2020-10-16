f26f2f8ebffcc2f4e978f184dc2349384f342cb3
Luke Hill <lukehill_uk@hotmail.com>
2020-10-16 15:03:51 +0100

    Final tidy-ups

--- testing/features/support/pages/oisc_warning/sticky.rb
+++ testing/features/support/pages/oisc_warning/sticky.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require_relative "title_description"
-
 module OISCWarning
   class Sticky < TitleDescription
     set_url "/iframe.html?id=components-oisc-warning--sticky"
@@ -9,9 +7,5 @@ module OISCWarning
     element :heading, "h2.cads-oisc-warning__title"
     element :description, ".cads-oisc-warning__body > p"
     element :close_sticky, ".cads-icon_close"
-
-    def vertical_position_of(element)
-      element.native.rect[:y]
-    end
   end
 end
--- testing/features/support/pages/oisc_warning/title_only.rb
+++ testing/features/support/pages/oisc_warning/title_only.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require_relative "title_description"
-
 module OISCWarning
   class TitleOnly < TitleDescription
     set_url "/iframe.html?id=components-oisc-warning--title-only"
