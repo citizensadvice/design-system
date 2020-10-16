d349eeace38be75c5aec01289c6ece9847035c66
David Rapson <david.rapson@citizensadvice.org.uk>
2020-10-16 12:05:46 +0100

    Fixup lint and style errors in callout.js

--- src/js/callout.js
+++ src/js/callout.js
@@ -1,20 +1,18 @@
 const initCallouts = () => {
-  try {
-    const h2s = document.getElementsByTagName('h2');
-    if (!h2s || !h2s.length) {
-      // Only do this if we don't have an H2 in the page
-      const callouts = document.getElementsByClassName('cads-callout');
-      if (callouts && callouts.length > 0) {
-        const callout = callouts[0];
-        const h3ToReplace = callout.getElementsByTagName('h3')[0];
-        const heading = h3ToReplace.innerHTML;
-        const newHeading = document.createElement('h2');
-        newHeading.innerHTML = heading;
-        h3ToReplace.parentNode.replaceChild(newHeading, h3ToReplace);
-      }
-    }
-  } catch (e) {
-    console.log(`Could not initialise callouts ${e}`);
+  const h2s = document.querySelectorAll('h2');
+  const firstCallout = document.querySelector('.cads-callout');
+
+  /**
+   * Only run if we:
+   * - Have a callout on the page
+   * - Don't already have a h2
+   */
+  if (firstCallout && h2s.length === 0) {
+    const h3ToReplace = firstCallout.querySelector('h3');
+    const heading = h3ToReplace.innerHTML;
+    const newHeading = document.createElement('h2');
+    newHeading.innerHTML = heading;
+    h3ToReplace.parentNode.replaceChild(newHeading, h3ToReplace);
   }
 };
 
