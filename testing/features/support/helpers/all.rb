3b826ed766b68cce8e49b687cdb765707737e805
Luke Hill <lukehill_uk@hotmail.com>
2020-10-16 15:29:12 +0100

    Bugfix: Require paths

--- testing/features/support/helpers/all.rb
+++ testing/features/support/helpers/all.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-require_relative "helpers/env_variables"
-require_relative "helpers/drivers"
-require_relative "helpers/faraday"
-require_relative "helpers/methods"
-require_relative "helpers/page"
-require_relative "helpers/regex"
+require_relative "env_variables"
+require_relative "drivers"
+require_relative "faraday"
+require_relative "methods"
+require_relative "page"
+require_relative "regex"
