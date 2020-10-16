f38cf15ebde692d39cd007426962e712241e9cc7
David Rapson <david.rapson@citizensadvice.org.uk>
2020-10-16 12:29:49 +0100

    Refactor header.js, fix lint error

--- src/js/header.js
+++ src/js/header.js
@@ -1,27 +1,29 @@
 const initHeader = () => {
-  try {
-    const header = document.getElementsByClassName('cads-header')[0];
-    const controlButton = document.getElementsByClassName(
-      'cads-search-reveal'
-    )[0];
+  const CLASS_NAMES = {
+    showSearch: 'cads-header-show-search',
+    iconSearch: 'cads-icon_search',
+    iconClose: 'cads-icon_close',
+  };
 
+  const header = document.querySelector('.cads-header');
+  const controlButton = header.querySelector('.cads-search-reveal');
+
+  if (header && controlButton) {
     controlButton.addEventListener('click', () => {
-      if (header.classList.contains('cads-header-show-search')) {
-        header.classList.remove('cads-header-show-search');
-        controlButton.classList.remove('cads-icon_close');
-        controlButton.classList.add('cads-icon_search');
+      if (header.classList.contains(CLASS_NAMES.showSearch)) {
+        header.classList.remove(CLASS_NAMES.showSearch);
+        controlButton.classList.remove(CLASS_NAMES.iconClose);
+        controlButton.classList.add(CLASS_NAMES.iconSearch);
         controlButton.setAttribute('aria-expanded', 'false');
         controlButton.title = 'Open search';
       } else {
-        header.classList.add('cads-header-show-search');
-        controlButton.classList.remove('cads-icon_search');
-        controlButton.classList.add('cads-icon_close');
+        header.classList.add(CLASS_NAMES.showSearch);
+        controlButton.classList.remove(CLASS_NAMES.iconSearch);
+        controlButton.classList.add(CLASS_NAMES.iconClose);
         controlButton.setAttribute('aria-expanded', 'true');
         controlButton.title = 'Close search';
       }
     });
-  } catch (e) {
-    console.warn(`Could not initialise header ${e}`);
   }
 };
 
