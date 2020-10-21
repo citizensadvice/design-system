acbfe5af2265d46def67bd4d854cb34d08a35b6c
Luke Hill <lukehill_uk@hotmail.com>
2020-10-21 10:55:13 +0100

    Adviser warning looks slightly different, so we must validate this

--- testing/features/support/components/callout/adviser.rb
+++ testing/features/support/components/callout/adviser.rb
@@ -3,5 +3,13 @@
 module Callout
   class Adviser < Standard
     set_url "/iframe.html?id=components-callouts--adviser-callout&viewMode=story"
+
+    def warning
+      evaluate_script(
+        "window.getComputedStyle(
+          document.querySelector('.cads-callout__adviser-label'), '::before'
+        ).getPropertyValue('content')"
+      ).delete('\\"')
+    end
   end
 end
