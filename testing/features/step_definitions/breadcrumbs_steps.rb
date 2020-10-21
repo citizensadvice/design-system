4c6c5d94943d26d5b673e3adb51ab61ec30eb621
Luke Hill <lukehill_uk@hotmail.com>
2020-10-21 11:09:49 +0100

    Further DRY ups Breadcrumbs no longer needs the initial_form wrapping section Asset Hyperlink can use the script helper

--- testing/features/step_definitions/breadcrumbs_steps.rb
+++ testing/features/step_definitions/breadcrumbs_steps.rb
@@ -11,13 +11,13 @@ Given("a No Collapse Breadcrumbs component is on the page") do
 end
 
 Then("a series of breadcrumbs are present") do
-  expect(@component.initial_form).to have_breadcrumbs
+  expect(@component).to have_breadcrumbs(minimum: 3)
 end
 
 Then("the initial breadcrumbs are all links") do
-  expect(@component.initial_form.all_but_last_breadcrumb).to all have_link
+  expect(@component.all_but_last_breadcrumb).to all have_link
 end
 
 Then("the final breadcrumb isn't a link") do
-  expect(@component.initial_form.breadcrumbs.last).not_to have_link
+  expect(@component.breadcrumbs.last).not_to have_link
 end
--- testing/features/support/components/breadcrumbs/default.rb
+++ testing/features/support/components/breadcrumbs/default.rb
@@ -4,12 +4,14 @@ module Breadcrumbs
   class Default < ::Base
     set_url "/iframe.html?id=components-breadcrumbs--breadcrumbs-default&viewMode=story"
 
-    section :initial_form, ".cads-breadcrumbs" do
-      elements :breadcrumbs, ".cads-breadcrumbs li"
+    elements :breadcrumbs, ".cads-breadcrumbs li"
 
-      def all_but_last_breadcrumb
-        breadcrumbs[0..-2]
-      end
+    def all_but_last_breadcrumb
+      breadcrumbs[0..-2]
+    end
+
+    def validate_initial_state!
+      has_breadcrumbs?(wait: 5)
     end
   end
 end
