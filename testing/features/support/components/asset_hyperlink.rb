a855f0416411947bf1366b2fdf81ad1973f19839
Luke Hill <lukehill_uk@hotmail.com>
2020-10-21 11:27:18 +0100

    Move helper into anonymous class for sectioning (Old style component)

--- testing/features/support/components/asset_hyperlink.rb
+++ testing/features/support/components/asset_hyperlink.rb
@@ -2,11 +2,11 @@
 
 module DesignSystem
   class AssetHyperlink < ::Base
-    include Helpers::Scripts
-
     set_url "/iframe.html?id=3-components--asset-hyperlink&viewMode=story"
 
     section :initial_form, "#a11yComponentToTest" do
+      include Helpers::Scripts
+
       element :download_link, "a"
       element :download_size, ".cads-asset-type"
 
