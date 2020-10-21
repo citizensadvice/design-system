14c0861707133e1325ea83f898ba3f6e97d4fb5c
Luke Hill <lukehill_uk@hotmail.com>
2020-10-21 15:43:49 +0100

    Add note about device availability and begin to split out appium restrictions

--- testing/features/support/drivers/browserstack/ios.rb
+++ testing/features/support/drivers/browserstack/ios.rb
@@ -4,14 +4,37 @@ module Drivers
   class Browserstack
     class Ios < Base
       def specific_browser_capabilities
+        # Rough availability list for active BS iOS Devices
+        # iOS 13: iPhone 11 Pro / iPhone 11 / iPhone XS / iPhone 8
+        # iOS 12: iPhone XS / iPhone 8
+        # iOS 11: iPhone X  / iPhone 8
+        # Use this availability list with caution!: LH - October 2020
         {
           "bstack:options" => {
             "osVersion" => "13",
             "deviceName" => "iPhone 8",
             "realMobile" => "true",
-            "appiumVersion" => "1.17.0"
+            "appiumVersion" => appium_version
           }
         }
+      end
+
+      private
+
+      def appium_version
+        if ios13? || ios12?
+          "1.17.0"
+        else
+          "1.16.0"
+        end
+      end
+
+      def ios13?
+        "TBC"
+      end
+
+      def ios12?
+        "TBC"
       end
     end
   end
