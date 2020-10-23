39071728a0afb19f0ef7223d4d88bdfba09c48b6
David Rapson <david.rapson@citizensadvice.org.uk>
2020-10-23 15:25:59 +0100

    Migrate sizing and borders doc

--- styleguide/foundations.stories.js
+++ styleguide/foundations.stories.js
@@ -1,6 +1,4 @@
 import Typography from './foundations/typography.md';
-import Spacing from './foundations/spacing.md';
-import Borders from './foundations/borders.md';
 import Visibility from './foundations/visibility.md';
 import IconFont from './foundations/icon_font.md';
 
@@ -13,7 +11,5 @@ export default {
 };
 
 export const typography = () => Typography;
-export const spacingAndSizing = () => Spacing;
-export const borders = () => Borders;
 export const visibility = () => Visibility;
 export const iconFont = () => IconFont;
