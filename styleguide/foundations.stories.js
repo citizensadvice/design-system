5e5d811f9a7d1f58945f28833311721378ce151f
David Rapson <david.rapson@citizensadvice.org.uk>
2020-10-23 15:44:27 +0100

    Remove typography docs (pending typography review)

--- styleguide/foundations.stories.js
+++ styleguide/foundations.stories.js
@@ -1,13 +1,7 @@
-import Typography from './foundations/typography.md';
 import IconFont from './foundations/icon_font.md';
 
 export default {
   title: '2: Design Foundations',
-  decorators: [
-    (storyFn) =>
-      `<div class="cads-styleguide-max-content-width">${storyFn()}</div>`,
-  ],
 };
 
-export const typography = () => Typography;
 export const iconFont = () => IconFont;
