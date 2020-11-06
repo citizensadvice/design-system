cdf84ad780a736e7c3166887c1eaf2ec68a09ad8
Dan Cseko <dan@baseonmars.co.uk>
2020-11-06 16:30:39 +0000

    feat: extract footer links [wip]

--- styleguide/haml_locals.rb
+++ styleguide/haml_locals.rb
@@ -182,6 +182,58 @@
     HTML
   },
 
+  "footer_links" => {
+    "column_one" => {
+      "title" => "Advice",
+      "links" => [
+        { "url" => "/benefits/", "label" => "Benefits" },
+        { "url" => "/work/", "label" => "Work" },
+        { "url" => "/debt-and-money/", "label" => "Debt and Money" },
+        { "url" => "/consumer/", "label" => "Consumer" },
+        { "url" => "/family/", "label" => "Family" },
+        { "url" => "/housing/", "label" => "Housing" },
+        { "url" => "/law-and-courts/", "label" => "Law and courts" },
+        { "url" => "/immigration/", "label" => "Immigration" },
+        { "url" => "/health/", "label" => "Health" }
+      ]
+    },
+    "column_two" => {
+      "title" => "Resources and tools",
+      "links" => [
+        { "url" => "/about-us/how-citizens-advice-works/products-and-services/adviser-resources/", "label" => "Advisor resources" },
+        { "url" => "/about-us/how-we-provide-advice/our-prevention-work/education/", "label" => "Education resources" },
+        { "url" => "/resources-and-tools/about-this-site/accessibility/", "label" => "Accessibility" },
+        { "url" => "/resources-and-tools/about-this-site/accessibility/", "label" => "Site search" },
+        { "url" => "/resources-and-tools/search-navigation-tools/a-to-z-of-advice/", "label" => "A-Z of advice" }
+      ]
+    },
+    "column_three" => {
+      "title" => "More from us",
+      "links" => [
+        { "url" => "/about-us/", "label" => "About us" },
+        { "url" => "/about-us/how-citizens-advice-works/who-we-are-and-what-we-do/annual-reports/", "label" => "Annual reports" },
+        { "url" => "/about-us/contact-us/contact-us/contact-us/", "label" => "Contact us" },
+        { "url" => "/about-us/contact-us/contact-us/make-a-complaint-about-us/", "label" => "Complaints" },
+        { "url" => "/about-us/how-citizens-advice-works/media/", "label" => "Media" },
+        { "url" => "/about-us/policy/", "label" => "Policy reseach" },
+        { "url" => "/about-us/support-us/", "label" => "Support us" },
+        { "url" => "/about-us/support-us/volunteering/", "label" => "Volunteering" },
+        { "url" => "/about-us/how-citizens-advice-works/job-and-voluntary-opportunities/", "label" => "Jobs" }
+      ]
+    },
+    "column_four" => {
+      "title" => "About Citizens Advice",
+      "links" => [
+        { "url" => "/about-us/how-we-provide-advice/", "label" => "How we provide advice" },
+        { "url" => "/about-us/difference-we-make/", "label" => "The difference we make" },
+        { "url" => "/about-us/support-us/", "label" => "Support us" },
+        { "url" => "/about-us/how-citizens-advice-works/", "label" => "How Citizens Advice works" },
+        { "url" => "/resources-and-tools/about-this-site/disclaimer-and-copyright/", "label" => "Disclaimer and Copyright" },
+        { "url" => "/about-us/citizens-advice-privacy-policy/", "label" => "Privacy and cookies" }
+      ]
+    }
+  },
+
   "root_path" => "root_path",
   "sign_in" => {
     "url" => "/sign-in/url",
@@ -223,7 +275,7 @@
 
   "contact_details" => <<~HTML,
     <p><strong>Bail for Immigration Detainees</strong></p>
-    <p>Telephone: 01234 567890</p>
+    <p>Telephone => 01234 567890</p>
     <p>Monday to Thursday 8am to 1pm</p>
     <p>Calls cost a lot of money</p>
     <p><a href='http =>//link.to.the.website'>Link to the website</a></p>
@@ -235,7 +287,7 @@
     "id" => "target-content-123",
     "title" => "If you are a citizen of a country outside the EU, EEA or Switzerland",
     "body" => <<~HTML
-      <p>You should apply to the EU Settlement Scheme if both:</p>
+      <p>You should apply to the EU Settlement Scheme if both =></p>
       <ul>
         <li>you’re in the UK by 31 December 2020</li>
         <li>you have family in the UK from the EU, EEA or Switzerland</li>
@@ -254,24 +306,25 @@
   "success_message" => "Thank you for your feedback",
 
   "related_content" => <<~HTML
-    <h3><a href="https://www.citizensadvice.org.uk">Search and navigation tools</a></h3>
+    <h3><a href="https =>//www.citizensadvice.org.uk">Search and navigation tools</a></h3>
     <ul>
-      <li><a href="https://www.citizensadvice.org.uk">Search</a></li>
-      <li><a href="https://www.citizensadvice.org.uk">A to Z of advice</a></li>
-      <li><a href="https://www.citizensadvice.org.uk">Finding information</a></li>
-      <li><a href="https://www.citizensadvice.org.uk">Search for your local Citizens Advice</a></li>
-      <li><a href="https://www.citizensadvice.org.uk">Browse our campaign pages</a></li>
-      <li><a href="https://www.citizensadvice.org.uk">Browse our research pages</a></li>
+      <li><a href="https =>//www.citizensadvice.org.uk">Search</a></li>
+      <li><a href="https =>//www.citizensadvice.org.uk">A to Z of advice</a></li>
+      <li><a href="https =>//www.citizensadvice.org.uk">Finding information</a></li>
+      <li><a href="https =>//www.citizensadvice.org.uk">Search for your local Citizens Advice</a></li>
+      <li><a href="https =>//www.citizensadvice.org.uk">Browse our campaign pages</a></li>
+      <li><a href="https =>//www.citizensadvice.org.uk">Browse our research pages</a></li>
     </ul>
     <div class="cads-related-content__advisernet">
       <ul>
-        <li><a href="https://www.citizensadvice.org.uk">Link 1</a></li>
-        <li><a href="https://www.citizensadvice.org.uk">Link 2</a></li>
-        <li><a href="https://www.citizensadvice.org.uk">Link 3</a></li>
-        <li><a href="https://www.citizensadvice.org.uk">Link 4</a></li>
-        <li><a href="https://www.citizensadvice.org.uk">Link 5</a></li>
-        <li><a href="https://www.citizensadvice.org.uk">Link 6</a></li>
+        <li><a href="https =>//www.citizensadvice.org.uk">Link 1</a></li>
+        <li><a href="https =>//www.citizensadvice.org.uk">Link 2</a></li>
+        <li><a href="https =>//www.citizensadvice.org.uk">Link 3</a></li>
+        <li><a href="https =>//www.citizensadvice.org.uk">Link 4</a></li>
+        <li><a href="https =>//www.citizensadvice.org.uk">Link 5</a></li>
+        <li><a href="https =>//www.citizensadvice.org.uk">Link 6</a></li>
       </ul>
     </div>
   HTML
+
 }
