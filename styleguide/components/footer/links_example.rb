f1dfc5425395832ca394ac365f2b2b1f9c92e7ef
Dan Cseko <dan@baseonmars.co.uk>
2020-11-09 11:15:19 +0000

    feat: updated variable structure and docs

---
+++ styleguide/components/footer/links_example.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+{ "column_one" => {
+  "title" => "Advice",
+  "links" => [
+    { "url" => "/benefits/", "label" => "Benefits" },
+    { "url" => "/work/", "label" => "Work" },
+    { "url" => "/debt-and-money/", "label" => "Debt and Money" },
+    { "url" => "/consumer/", "label" => "Consumer" },
+    { "url" => "/family/", "label" => "Family" },
+    { "url" => "/housing/", "label" => "Housing" },
+    { "url" => "/law-and-courts/", "label" => "Law and courts" },
+    { "url" => "/immigration/", "label" => "Immigration" },
+    { "url" => "/health/", "label" => "Health" }
+  ]
+},
+  "column_two" => {
+    "title" => "Resources and tools",
+    "links" => [
+      { "url" => "/about-us/how-citizens-advice-works/products-and-services/adviser-resources/", "label" => "Advisor resources" },
+      { "url" => "/about-us/how-we-provide-advice/our-prevention-work/education/", "label" => "Education resources" },
+      { "url" => "/resources-and-tools/about-this-site/accessibility/", "label" => "Accessibility" },
+      { "url" => "/resources-and-tools/about-this-site/accessibility/", "label" => "Site search" },
+      { "url" => "/resources-and-tools/search-navigation-tools/a-to-z-of-advice/", "label" => "A-Z of advice" }
+    ]
+  },
+  "column_three" => {
+    "title" => "More from us",
+    "links" => [
+      { "url" => "/about-us/", "label" => "About us" },
+      { "url" => "/about-us/how-citizens-advice-works/who-we-are-and-what-we-do/annual-reports/", "label" => "Annual reports" },
+      { "url" => "/about-us/contact-us/contact-us/contact-us/", "label" => "Contact us" },
+      { "url" => "/about-us/contact-us/contact-us/make-a-complaint-about-us/", "label" => "Complaints" },
+      { "url" => "/about-us/how-citizens-advice-works/media/", "label" => "Media" },
+      { "url" => "/about-us/policy/", "label" => "Policy reseach" },
+      { "url" => "/about-us/support-us/", "label" => "Support us" },
+      { "url" => "/about-us/support-us/volunteering/", "label" => "Volunteering" },
+      { "url" => "/about-us/how-citizens-advice-works/job-and-voluntary-opportunities/", "label" => "Jobs" }
+    ]
+  },
+  "column_four" => {
+    "title" => "About Citizens Advice",
+    "links" => [
+      { "url" => "/about-us/how-we-provide-advice/", "label" => "How we provide advice" },
+      { "url" => "/about-us/difference-we-make/", "label" => "The difference we make" },
+      { "url" => "/about-us/support-us/", "label" => "Support us" },
+      { "url" => "/about-us/how-citizens-advice-works/", "label" => "How Citizens Advice works" },
+      { "url" => "/resources-and-tools/about-this-site/disclaimer-and-copyright/", "label" => "Disclaimer and Copyright" },
+      { "url" => "/about-us/citizens-advice-privacy-policy/", "label" => "Privacy and cookies" }
+    ]
+  } }
