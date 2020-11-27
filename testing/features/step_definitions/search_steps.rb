24ee1cb6839c21e4986a3e1c7b4132de3aba90f8
Marcelo Nicolosi <celonicolosi@hotmail.com>
2020-11-27 03:02:43 +0000

    Fix some rubocop lint offenses

--- testing/features/step_definitions/search_steps.rb
+++ testing/features/step_definitions/search_steps.rb
@@ -1,20 +1,20 @@
 # frozen_string_literal: true
 
-Given('a Standard Search component is on the page') do
+Given("a Standard Search component is on the page") do
   @component = Search::Standard.new
   @component.load
 end
 
-Given('a Search With Value component is on the page') do
+Given("a Search With Value component is on the page") do
   @component = Search::WithValue.new
   @component.load
 end
 
-Then('the search field is clear') do
+Then("the search field is clear") do
   expect(@component.cads_search.search_field.value).to eq("")
 end
 
-Then('I can search for {string}') do |search_term|
+Then("I can search for {string}") do |search_term|
   @component.cads_search.search_field.send_keys(search_term)
 
   expect(@component.cads_search.search_field.value).to eq(search_term)
@@ -22,17 +22,17 @@ Then('I can search for {string}') do |search_term|
   expect(@component.cads_search).to have_search_button
 end
 
-Then('I can search for the pre-defined value {string}') do |pre_defined_value|
+Then("I can search for the pre-defined value {string}") do |pre_defined_value|
   expect(@component.cads_search.search_field.value).to eq(pre_defined_value)
 
   expect(@component.cads_search).to have_search_button
 end
 
-Then('the search field is filled with {string}') do |pre_defined_value|
+Then("the search field is filled with {string}") do |pre_defined_value|
   expect(@component.cads_search.search_field.value).to eq(pre_defined_value)
 end
 
-Then('I can delete the pre-defined value') do
+Then("I can delete the pre-defined value") do
   @component.clear_field
 
   expect(@component.cads_search.search_field.value).to eq("")
