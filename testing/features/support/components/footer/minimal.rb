12416c1d183eeaee6926295b9114a0f7ffef6f2d
Marcelo Nicolosi <celonicolosi@hotmail.com>
2020-11-30 22:04:03 +0000

    Change validate_initial_state! of Minimal to some element that is not asserted during the test, as base validate for root in this case Im validating the footer itself

--- testing/features/support/components/footer/minimal.rb
+++ testing/features/support/components/footer/minimal.rb
@@ -4,12 +4,13 @@ module Footer
   class Minimal < ::Base
     set_url "/iframe.html?id=components-footer--minimal&viewMode=story"
 
+    element :footer, "footer"
     element :logo, ".cads-logo"
     element :copyright, ".cads-footer__company-info div p:nth-of-type(1)"
     element :company_info, ".cads-footer__company-info div p:nth-of-type(2)"
 
     def validate_initial_state!
-      has_logo?(wait: 5)
+      has_footer?(wait: 5)
     end
   end
 end
