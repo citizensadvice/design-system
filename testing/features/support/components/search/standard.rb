d76debcd0db42a4325527b11247812fed962c187
Marcelo Nicolosi <celonicolosi@hotmail.com>
2020-12-01 17:47:09 +0000

    Use the form itself to validade_initial_state

--- testing/features/support/components/search/standard.rb
+++ testing/features/support/components/search/standard.rb
@@ -4,11 +4,12 @@ module Search
   class Standard < ::Base
     set_url "/iframe.html?id=components-search--search&viewMode=story"
 
+    element :search_form, ".cads-search"
     element :search_field, ".cads-search__input"
     element :search_button, ".cads-search__button"
 
     def validate_initial_state!
-      has_search_button?(wait: 5)
+      has_search_form?(wait: 5)
     end
 
     def clear_field
