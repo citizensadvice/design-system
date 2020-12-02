5c622b513c69a6bf05e51526283e6f088f679d9d
Luke Hill <lukehill_uk@hotmail.com>
2020-12-02 13:18:54 +0000

    Add ca_testing gem, use local driver logic

--- testing/features/support/drivers/local.rb
+++ testing/features/support/drivers/local.rb
@@ -5,32 +5,7 @@ module Drivers
     include Helpers::Drivers
 
     def register
-      Capybara.register_driver :selenium do |app|
-        Capybara::Selenium::Driver.new(
-          app,
-          browser: browser,
-          service: service,
-          capabilities: [desired_capabilities, options]
-        )
-      end
-    end
-
-    private
-
-    def service
-      return unless safari?
-
-      Selenium::WebDriver::Safari.technology_preview!
-      Selenium::WebDriver::Service.safari({ args: ["--diagnose"] })
-    end
-
-    def desired_capabilities
-      Selenium::WebDriver::Remote::Capabilities.new.tap do |capabilities|
-        if safari?
-          capabilities["browserName"] = "Safari Technology Preview"
-          AutomationLogger.debug("Altering Browser Name request to alleviate Capybara failure with STP.")
-        end
-      end
+      ::CaTesting::Drivers::Local.register
     end
   end
 end
