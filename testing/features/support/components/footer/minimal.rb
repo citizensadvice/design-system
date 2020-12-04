a8089aa12e5d3c3dd47fee12e92a897c956ebc8e
Marcelo Nicolosi <celonicolosi@hotmail.com>
2020-12-04 10:49:41 +0000

    Remove footer element and validation

--- testing/features/support/components/footer/minimal.rb
+++ testing/features/support/components/footer/minimal.rb
@@ -4,13 +4,8 @@ module Footer
   class Minimal < ::Base
     set_url "/iframe.html?id=components-footer--minimal&viewMode=story"
 
-    element :footer, "footer"
     element :logo, ".cads-logo"
     element :copyright, ".cads-footer__company-info div p:nth-of-type(1)"
     element :company_info, ".cads-footer__company-info div p:nth-of-type(2)"
-
-    def validate_initial_state!
-      has_footer?(wait: 5)
-    end
   end
 end
