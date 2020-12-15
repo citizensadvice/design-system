6f9573d0b3ff1e39074f78b4ef667f0a0c05c569
David Rapson <david.rapson@citizensadvice.org.uk>
2020-12-15 09:25:36 +0000

    remove redundant a11yComponentToTest check

--- testing/features/support/components/asset_hyperlink.rb
+++ testing/features/support/components/asset_hyperlink.rb
@@ -2,21 +2,19 @@
 
 module DesignSystem
   class AssetHyperlink < ::Base
+    include Helpers::Javascript
+
     set_url "/iframe.html?id=components-asset-hyperlink--example&viewMode=story"
 
-    section :initial_form, "#a11yComponentToTest" do
-      include Helpers::Javascript
+    element :download_link, "a"
+    element :download_size, ".cads-asset-type"
 
-      element :download_link, "a"
-      element :download_size, ".cads-asset-type"
-
-      def download_icon_content
-        before_content(".cads-icon_file").delete('\\"')
-      end
+    def download_icon_content
+      before_content(".cads-icon_file").delete('\\"')
     end
 
     def validate_initial_state!
-      has_initial_form?(wait: 5)
+      has_download_link?(wait: 5)
     end
   end
 end
