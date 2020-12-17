85cbb9dcb7038ac1f49c0c8a687837f56887a8e7
davidsauntson <david.sauntson@citizensadvice.org.uk>
2020-12-17 11:47:32 +0000

    feat: add extra header links and span

--- styleguide/haml_locals.rb
+++ styleguide/haml_locals.rb
@@ -118,7 +118,7 @@
 
   "header" => {
     "root_path" => "#",
-    "links" => [{ "title" => "AdviserNet", "url" => "?advisernet" }, { "title" => "Cymraeg", "url" => "?lang=cy" }],
+    "links" => [{"title" => "Public site"}, { "title" => "AdviserNet", "url" => "?advisernet" }, {"title" => "CABlink", "url" => "?CABlink"}, {"title" => "BMIS", "url" => "?BMIS"}, { "title" => "Cymraeg", "url" => "?lang=cy" }],
     "sign_in_out" => { "title" => "Sign out", "url" => "/sign-out/out" },
     "search_action_url" => "/search",
     "homepage_url" => "/"
