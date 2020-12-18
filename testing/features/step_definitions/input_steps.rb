695fd6cb6df19ebc623b5571282404842505bc44
Luke Hill <lukehill_uk@hotmail.com>
2020-12-18 10:10:43 +0000

    Optional label translation in input

--- testing/features/step_definitions/input_steps.rb
+++ testing/features/step_definitions/input_steps.rb
@@ -29,7 +29,7 @@ Then("there is no hint") do
 end
 
 Then("there is an optional label") do
-  expect(@component).to have_optional_label
+  expect(@component).to have_optional_label(text: I18n.t("cads.form.optional"))
 end
 
 Then("there is no optional label") do
