3da0c6175e337e2628410ffe59e3effa4ece0791
David Rapson <david@davidrapson.co.uk>
2020-12-22 09:49:51 +0000

    Extract badge styles from callout component (#675)

---
+++ styleguide/components/callouts/callouts.stories.js
@@ -0,0 +1,28 @@
+import { translate } from '../../story-helpers';
+
+import docs from './callouts-docs.mdx';
+import templateStandard from './_standard.html.haml';
+import templateExample from './_example.html.haml';
+import templateImportant from './_important.html.haml';
+import templateAdviser from './_adviser.html.haml';
+
+export default {
+  title: 'Components/Callouts',
+  parameters: { docs: { page: docs } },
+};
+
+export const Standard = (_, options) => translate(templateStandard, options);
+Standard.storyName = 'Standard callout';
+Standard.parameters = { docs: { source: { code: templateStandard.raw } } };
+
+export const Example = (_, options) => translate(templateExample, options);
+Example.storyName = 'Example callout';
+Example.parameters = { docs: { source: { code: templateExample.raw } } };
+
+export const Important = (_, options) => translate(templateImportant, options);
+Important.storyName = 'Important callout';
+Important.parameters = { docs: { source: { code: templateImportant.raw } } };
+
+export const Adviser = (_, options) => translate(templateAdviser, options);
+Adviser.storyName = 'Adviser callout';
+Adviser.parameters = { docs: { source: { code: templateAdviser.raw } } };
