cf7a54e76760b67c085b8bd2d9293f1f33f824ad
Luke Hill <lukehill_uk@hotmail.com>
2021-01-07 16:09:00 +0000

    CR Comments, use a better notation for test-id (And their values)

--- testing/features/support/components/footer/minimal.rb
+++ testing/features/support/components/footer/minimal.rb
@@ -5,7 +5,7 @@ module Footer
     set_url "/iframe.html?id=components-footer--minimal&viewMode=story"
 
     element :logo, ".cads-logo"
-    element :copyright, "[data-test-id='copyright']"
-    element :company_info, "[data-test-id='legal summary']"
+    element :copyright, "[data-testid='copyright']"
+    element :company_info, "[data-testid='legal-summary']"
   end
 end
