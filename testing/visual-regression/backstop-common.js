30f705ea42646fe39720ad8007e3f44aca2ed5fa
davidsauntson <david.sauntson@citizensadvice.org.uk>
2021-01-21 14:27:56 +0000

    test: add media screen sample pages

--- testing/visual-regression/backstop-common.js
+++ testing/visual-regression/backstop-common.js
@@ -32,6 +32,21 @@ module.exports = function backstopCommon(baseUrl) {
     ];
   }
 
+  function buildSamplePageScenarios(labelPrefix, componentId) {
+    return [
+      {
+        label: `${labelPrefix} (Print)`,
+        url: storyUrlFor(componentId),
+        readySelector: '.cads-greedy-nav-has-dropdown',
+        onReadyScript: 'onReadyEmulatePrint.js',
+      },
+      {
+        label: `${labelPrefix}`,
+        url: storyUrlFor(componentId),
+      },
+    ];
+  }
+
   return {
     id: 'backstop_default',
     viewports: [
@@ -284,37 +299,22 @@ module.exports = function backstopCommon(baseUrl) {
         url: storyUrlFor(`forms-radio-group--small`),
         viewports: [{ label: 'form-field', width: 800, height: 400 }],
       },
-      {
-        label: `Sample Page/Advice collection/Public (print)`,
-        url: storyUrlFor(
-          `sample-pages-advice-collection--advice-collection-public`
-        ),
-        readySelector: '.cads-greedy-nav-has-dropdown',
-        onReadyScript: 'onReadyEmulatePrint.js',
-      },
-      ,
-      {
-        label: `Sample Page/Advice collection/Adviser (print)`,
-        url: storyUrlFor(
-          `sample-pages-advice-collection--advice-collection-adviser`
-        ),
-        readySelector: '.cads-greedy-nav-has-dropdown',
-        onReadyScript: 'onReadyEmulatePrint.js',
-      },
-      ,
-      {
-        label: `Sample Page/Generic/Tables (print)`,
-        url: storyUrlFor(`sample-pages-generic--forms`),
-        readySelector: '.cads-greedy-nav-has-dropdown',
-        onReadyScript: 'onReadyEmulatePrint.js',
-      },
-      ,
-      {
-        label: `Sample Page/Generic/Forms (print)`,
-        url: storyUrlFor(`sample-pages-generic--tables`),
-        readySelector: '.cads-greedy-nav-has-dropdown',
-        onReadyScript: 'onReadyEmulatePrint.js',
-      },
+      ...buildSamplePageScenarios(
+        'Sample Page/Advice collection/Public',
+        'sample-pages-advice-collection--advice-collection-public'
+      ),
+      ...buildSamplePageScenarios(
+        'Sample Page/Advice collection/Adviser',
+        'sample-pages-advice-collection--advice-collection-adviser'
+      ),
+      ...buildSamplePageScenarios(
+        'Sample Page/Generic/Forms',
+        'sample-pages-generic--forms'
+      ),
+      ...buildSamplePageScenarios(
+        'Sample Page/Generic/Tables',
+        'sample-pages-generic--tables'
+      ),
     ],
     dockerCommandTemplate:
       'docker run --rm -i --mount type=bind,source="{cwd}",target=/src backstopjs/backstopjs:{version} {backstopCommand} {args}',
