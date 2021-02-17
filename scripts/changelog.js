737153c16fcbfbfaab047cf6d6e24e2951f47bdc
Dan Cseko <dan@baseonmars.co.uk>
2021-02-17 10:09:51 +0000

    feat: parse changelog, check version

--- scripts/changelog.js
+++ scripts/changelog.js
@@ -4,6 +4,10 @@ const assert = require('assert');
 const semver = require('semver');
 const fs = require('fs');
 const path = require('path');
+const marked = require('marked');
+
+const getPreviousVersion = (tokens) =>
+  tokens[0].tokens.find((t) => t.type === 'text').text;
 
 export const release = (version, changelogPath) => {
   // bail if version is not semver
@@ -19,4 +23,15 @@ export const release = (version, changelogPath) => {
   );
 
   assert.ok(fs.existsSync(changelogPath), 'failed to load CHANGELOG.md');
+
+  const file = fs.readFileSync(changelogPath).toString();
+  const tokens = marked.lexer(file);
+
+  const previousVersion = getPreviousVersion(tokens);
+  console.log(previousVersion);
+
+  assert.ok(
+    semver.gt(version, previousVersion),
+    `The release version must be greater than previous releases. version: ${version}  previous: ${previousVersion}`
+  );
 };
--- scripts/tests/changelog.test.ts
+++ scripts/tests/changelog.test.ts
@@ -1,10 +1,16 @@
 import { release } from '../changelog';
 import path from 'path';
 
+const withAlphaPath = path.join(
+  __dirname,
+  '__fixtures__',
+  'with-alpha-releases.md'
+);
+
 describe('pre-checks', () => {
   it('accepts a valid semver', () => {
     expect(() => {
-      release('v1.1.1-alpha.0');
+      release('v1.1.1-alpha.2', withAlphaPath);
     }).not.toThrow();
   });
 
@@ -17,17 +23,20 @@ describe('pre-checks', () => {
   it('looks for changelog in current dir', () => {
     // CHANGELOG.md in project root
     expect(() => {
-      release('v1.1.1');
+      release('v500.0.0'); // set high as we're reading the real changelog
     }).not.toThrow();
   });
 
   it('looks for changelog at path', () => {
     expect(() => {
-      release(
-        'v1.1.1',
-        path.join(__dirname, '__fixtures__', 'with-alpha-releases.md')
-      );
-    });
+      release('v1.1.1', withAlphaPath);
+    }).not.toThrow();
+  });
+
+  it('ensures version is greater than previous', () => {
+    expect(() => {
+      release('v1.0.0', withAlphaPath);
+    }).toThrow();
   });
 
   it('bails if changelog does not exist', () => {
