bc9f8d0619371cdea19179a722f82d54b998dc23
Luke Hill <20105237+luke-hill@users.noreply.github.com>
2021-02-23 10:01:32 +0000

    Add patches and remove duplicate code (#868)

--- testing/features/support/core_ext/capybara/node/element.rb
+++ testing/features/support/core_ext/capybara/node/element.rb
@@ -1,31 +1,23 @@
 # frozen_string_literal: true
 
-module CoreExtension
-  module Capybara
-    module Node
-      module Element
-        def text(type = nil, normalize_ws: ::Capybara.default_normalize_ws)
-          super(type, normalize_ws: normalize_ws)
-        end
+module Capybara
+  module Node
+    class Element
+      def color
+        native.css_value("color")
+      end
 
-        def color
-          native.css_value("color")
-        end
+      def background_color
+        native.css_value("background-color")
+      end
 
-        def background_color
-          native.css_value("background-color")
-        end
+      def font_weight
+        native.css_value("font-weight")
+      end
 
-        def font_weight
-          native.css_value("font-weight")
-        end
-
-        def vertical_position
-          native.rect[:y]
-        end
+      def vertical_position
+        native.rect[:y]
       end
     end
   end
 end
-
-Capybara::Node::Element.prepend CoreExtension::Capybara::Node::Element
