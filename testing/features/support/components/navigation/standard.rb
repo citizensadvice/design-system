15f0ba79921d106275995c62783e400c4419d32f
davidsauntson <david.sauntson@citizensadvice.org.uk>
2021-02-24 17:37:21 +0000

    test: move waits to element selectors

--- testing/features/support/components/navigation/standard.rb
+++ testing/features/support/components/navigation/standard.rb
@@ -6,15 +6,15 @@ module Navigation
 
     element :navigation, ".js-cads-greedy-nav"
     element :more_button, "button", { text: "More", wait: 2 }
-    element :close_button, "button", text: "Close"
+    element :close_button, "button", { text: "Close", wait: 2 }
     element :last_link, "a", text: "More from us"
 
     def open_dropdown_menu
-      more_button.click(wait: 2)
+      more_button.click
     end
 
     def close_dropdown_menu
-      close_button.click(wait: 2)
+      close_button.click
     end
 
     def tab_into_dropdown
