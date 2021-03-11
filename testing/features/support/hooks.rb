7cd0af36926a09e441feccdb6e417b697dc07d20
Luke Hill <lukehill_uk@hotmail.com>
2021-03-11 09:16:38 +0000

    We don't directly test iOS11 (So if stuff fails there we don't really care)

--- testing/features/support/hooks.rb
+++ testing/features/support/hooks.rb
@@ -8,7 +8,6 @@ Before do |test_case|
   I18n.locale = :en
   skip_this_scenario("This does not work on mobile") if mobile_phone? && test_case.source_tag_names.include?("@not_mobile")
   skip_this_scenario("This does not work on iOS12") if ios12? && test_case.source_tag_names.include?("@not_ios12")
-  skip_this_scenario("This does not work on iOS11") if ios11? && test_case.source_tag_names.include?("@not_ios11")
   resize_window unless mobile_phone?
   resize_window(320) if test_case.source_tag_names.include?("@small_screen") && !mobile_phone?
   AutomationLogger.info("Running Scenario: #{test_case.name}")
