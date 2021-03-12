89ca07b822e24270d32a673e2340817623e2b90f
David Rapson <david.rapson@citizensadvice.org.uk>
2021-03-12 16:58:23 +0000

    Upgrade Rails demo app to 6.1

--- demo/config/application.rb
+++ demo/config/application.rb
@@ -3,16 +3,27 @@
 require_relative "boot"
 
 require "rails"
+# Pick the frameworks you want:
 require "active_model/railtie"
+# require "active_job/railtie"
+# require "active_record/railtie"
+# require "active_storage/engine"
 require "action_controller/railtie"
+# require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
 require "action_view/railtie"
+# require "action_cable/engine"
+# require "sprockets/railtie"
+# require "rails/test_unit/railtie"
 
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Demo
   class Application < Rails::Application
-    config.load_defaults 6.0
-    config.generators.system_tests = nil
+    config.load_defaults 6.1
     config.view_component.preview_paths << DesignSystem::Engine.root.join("previews")
   end
 end
