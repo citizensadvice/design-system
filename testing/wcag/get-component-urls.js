0d72195ca10e05d5a3ceede0706c2451928ec711
Dan Cseko <dan@baseonmars.co.uk>
2021-03-12 11:26:58 +0000

    fix: type-o for exlcudeFromA11yTest->excludeFromA11yTest

--- testing/wcag/get-component-urls.js
+++ testing/wcag/get-component-urls.js
@@ -2,7 +2,7 @@ const backstopConfigCommon = require('../visual-regression/backstop-common');
 
 // Extracts urls from backstop testing scenarios, replaces host as directed
 module.exports = function getComponentUrls(baseUrl) {
-  const urls = backstopConfigCommon(baseUrl).scenarios.filter(scenario => !scenario.exlcudeFromA11yTest).map(
+  const urls = backstopConfigCommon(baseUrl).scenarios.filter(scenario => !scenario.excludeFromA11yTest).map(
     (scenario) => scenario.url
   );
 
