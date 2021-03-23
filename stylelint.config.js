cc0c3bcb2cc57c2ea0513cd56f4b98fc7a7a98ac
David Rapson <david.rapson@citizensadvice.org.uk>
2021-03-23 17:23:25 +0000

    Revert "Add implicitComponents config"
    This reverts commit 6b4506609746191eb5ba30a89f8c72fd19a4a974.

--- stylelint.config.js
+++ stylelint.config.js
@@ -52,7 +52,6 @@ module.exports = {
     'plugin/selector-bem-pattern': {
       preset: 'bem',
       presetOptions: { namespace: 'cads' },
-      implicitComponents: 'scss/6-components/*.scss',
     },
   },
 };
