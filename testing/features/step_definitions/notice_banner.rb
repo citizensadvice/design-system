080119b758f312aa75371ebd22fb66ac931dc707
Marcelo Nicolosi <celonicolosi@hotmail.com>
2021-04-12 12:34:27 +0100

    Add steps in step_definitions/notice_banner.rb

---
+++ testing/features/step_definitions/notice_banner.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+Given('a Standard Notice Banner component is on the page') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+
+Then('a notice banner title and message are present') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
