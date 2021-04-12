69e3c80e271d5800e9d1d7b90517f8de5ab2608a
Marcelo Nicolosi <celonicolosi@hotmail.com>
2021-04-12 21:40:53 +0100

    Better organizing the components

--- testing/features/step_definitions/notice_banner.rb
+++ testing/features/step_definitions/notice_banner.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Given("a Standard Notice Banner component is on the page") do
-  @component = DesignSystem::NoticeBanner.new.tap(&:load)
+  @component = NoticeBanner::Example.new.tap(&:load)
 end
 
 Then("a notice banner title and message are present") do
--- testing/features/support/components/notice_banner/notice_banner.rb
+++ testing/features/support/components/notice_banner/notice_banner.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-module DesignSystem
-  class NoticeBanner < ::Base
+module NoticeBanner
+  class Example < ::Base
     set_url "/iframe.html?id=components-notice-banner--example&args=&viewMode=story"
 
     element :notice_banner_title, ".cads-notice-banner__title"
