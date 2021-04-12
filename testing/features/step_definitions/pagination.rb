4391a1d3b62b5eae52404d05d4edbfb8fbbdc7f2
Marcelo Nicolosi <celonicolosi@hotmail.com>
2021-04-12 16:01:15 +0100

    Add steps in step_definitions/pagination.rb

---
+++ testing/features/step_definitions/pagination.rb
@@ -0,0 +1,33 @@
+Given('an Example Pagination component is on the page') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('a Paging Info Pagination component is on the page') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('the current page is highlighted') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('there are numerical buttons to skip to the {int} previous pages') do |int|
+  # Then('there are numerical buttons to skip to the {float} previous pages') do |float|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('there are numerical buttons to skip to the {int} next pages') do |int|
+  # Then('there are numerical buttons to skip to the {float} next pages') do |float|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('there are buttons to skip to the first and last page') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('there are buttons to skip to the next and previous page') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('the number of pages is displayed') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
