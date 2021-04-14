b222f24f4f24d316711ee6f3b8dcae8291bf2a32
Marcelo Nicolosi <celonicolosi@hotmail.com>
2021-04-14 15:33:09 +0100

    Change notice_banner_title to use element + text instead styling class

--- testing/features/support/components/notice_banner/notice_banner.rb
+++ testing/features/support/components/notice_banner/notice_banner.rb
@@ -4,7 +4,7 @@ module NoticeBanner
   class Example < ::Base
     set_url "/iframe.html?id=components-notice-banner--example&args=&viewMode=story"
 
-    element :notice_banner_title, ".cads-notice-banner__title"
+    element :notice_banner_title, "span", text: "Notice banner title"
     element :message, "p"
   end
 end
