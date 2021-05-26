87b5dcfcb98605bd741afb8da417e1a051de92ea
Luke Hill <lukehill_uk@hotmail.com>
2021-05-26 15:39:48 +0100

    bugfix: Some file names / inheritances were wrong

--- testing/features/support/components/navigation/default.rb
+++ testing/features/support/components/navigation/default.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Navigation
-  class Standard < ::Base
+  class Default < ::Base
     include Helpers::Page
 
     set_url "/iframe.html?id=components-navigation--default-story&viewMode=story"
--- testing/features/support/components/navigation/header.rb
+++ testing/features/support/components/navigation/header.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Navigation
-  class Header < Standard
+  class Header < Default
     set_url "/iframe.html?id=sample-pages-advice-collection--advice-collection-public&viewMode=story"
 
     element :sign_out, "button", text: "Sign out"
