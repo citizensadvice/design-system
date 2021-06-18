c22ad8b8e8d5fcff7a22a311f29af6b229a3c69a
David Rapson <david.rapson@citizensadvice.org.uk>
2021-06-18 16:44:10 +0100

    Migrate ContactDetails to a view component
    - Adds new component
    - Adds rspec tests
    - Updates documentation
    - Rewords example text to use a fake organisation instead of a real one
    - Updates visual regression tests

--- styleguide/components/contact-details/contact-details.stories.js
+++ styleguide/components/contact-details/contact-details.stories.js
@@ -1,13 +1,10 @@
 import docs from './contact-details-docs.mdx';
-import templateDefault from './_contact-details.html.haml';
+import templateExample from '../../examples/contact_details/example.html';
 
 export default {
   title: 'Components/Contact Details',
   parameters: { docs: { page: docs } },
 };
 
-export const ContactDetails = () => templateDefault.en;
-
-templateDefault.parameters = {
-  docs: { source: { code: templateDefault.toString() } },
-};
+export const Example = () => templateExample;
+Example.parameters = { docs: { source: { code: templateExample } } };
