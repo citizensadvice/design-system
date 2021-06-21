953c1af4e71aacf5a91de47e6be56e8d1455c4da
David Rapson <david.rapson@citizensadvice.org.uk>
2021-06-21 09:51:03 +0100

    Fix lint warning

--- engine/lib/citizens_advice_components/engine.rb
+++ engine/lib/citizens_advice_components/engine.rb
@@ -10,9 +10,9 @@ module CitizensAdviceComponents
 
     config.view_component.preview_paths << CitizensAdviceComponents::Engine.root.join("previews")
     config.autoload_paths = %W[
-      #{CitizensAdviceComponents::Engine.root.join("previews")}
-      #{CitizensAdviceComponents::Engine.root.join("app/components")}
-      #{CitizensAdviceComponents::Engine.root.join("app/lib")}
+      #{CitizensAdviceComponents::Engine.root.join('previews')}
+      #{CitizensAdviceComponents::Engine.root.join('app/components')}
+      #{CitizensAdviceComponents::Engine.root.join('app/lib')}
     ]
   end
 end
