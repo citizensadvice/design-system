7b01684539e0e13ce5dfddc69457fb06d597afe6
David Rapson <david.rapson@citizensadvice.org.uk>
2021-06-21 17:21:56 +0100

    Header: logo slot spec

--- engine/spec/components/header_spec.rb
+++ engine/spec/components/header_spec.rb
@@ -10,4 +10,20 @@ RSpec.describe CitizensAdviceComponents::Header, type: :component do
       expect(component.at("header")).not_to be_present
     end
   end
+
+  context "with logo" do
+    subject(:component) do
+      render_inline(described_class.new) do |header|
+        header.logo(title: "Logo title", url: "/homepage")
+      end
+    end
+
+    subject(:logo) { component.at(".cads-logo") }
+
+    it "renders a logo" do
+      expect(logo).to be_present
+      expect(logo.text.strip).to eq "Logo title"
+      expect(logo.attr("href")).to eq "/homepage"
+    end
+  end
 end
