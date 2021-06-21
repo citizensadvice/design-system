2134e43faf57dcf443afb20d60353f650a83bb7b
David Rapson <david.rapson@citizensadvice.org.uk>
2021-06-21 14:48:05 +0100

    Initial header component structure
    Rough structure for the header component. Makes heavy use of slots to
    keep things flexible. Allows things like:
    
    - Opting out of the search form
    - Providing a custom block for the account link, e.g. to replace the
      plain link with a form.
    - Skip links and secondary links can be provided by slots too to keep
      the interface consistent

---
+++ engine/app/components/citizens_advice_components/header.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+
+module CitizensAdviceComponents
+  class Header < Base
+    renders_many :skip_links, "HeaderLink"
+    renders_many :links, "HeaderLink"
+
+    # If custom_account_link then a block will be rendered
+    # allowing custom HTML to be provided, otherwise the
+    # component expects a account_link(title: "", url: "#") slot
+    renders_one :account_link, "HeaderLink"
+    renders_one :custom_account_link
+
+    renders_one :search_form, "HeaderSearch"
+
+    attr_reader :homepage_url
+
+    def initialize(homepage_url: nil)
+      super
+      @homepage_url = homepage_url || "/"
+    end
+
+    def render_account_link?
+      :custom_account_link.present? || :account_link.present?
+    end
+
+    def toggle_button_attributes
+      {
+        title: t("citizens_advice_components.header.open_search"),
+        "aria-expanded": "false",
+        "data-testid": "expand-button",
+        "data-descriptive-label-show": t("citizens_advice_components.header.open_search"),
+        "data-descriptive-label-hide": t("citizens_advice_components.header.close_search")
+      }
+    end
+
+    class HeaderLink < ViewComponent::Base
+      attr_reader :title, :url
+
+      def initialize(title:, url:, current_site: false)
+        super
+        @title = title
+        @url = url
+        @current_site = current_site
+      end
+
+      def current_site?
+        @current_site.present?
+      end
+    end
+
+    class HeaderSearch < Base
+      attr_reader :search_action_url, :search_param
+
+      def initialize(search_action_url:, search_param: :q)
+        super
+        @search_action_url = search_action_url
+        @search_param = search_param
+      end
+
+      def render?
+        search_action_url.present?
+      end
+    end
+  end
+end
---
+++ engine/previews/citizens_advice_components/header_preview.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+module CitizensAdviceComponents
+  class HeaderPreview < ViewComponent::Preview
+    include ActionView::Helpers::TagHelper
+
+    def with_basic_options
+      render(CitizensAdviceComponents::Header.new) do |component|
+        component.skip_links(skip_links)
+        component.links(links)
+        component.account_link(title: "Sign in", url: "#")
+        component.search_form(search_action_url: "/search")
+      end
+    end
+
+    def with_extra_links
+      render(CitizensAdviceComponents::Header.new) do |component|
+        component.skip_links(skip_links)
+        component.links([
+          { title: "Public site", url: "#", current_site: true },
+          { title: "Intranet", url: "#" },
+          { title: "Cymraeg", url: "?lang=cy" }
+        ])
+        component.account_link(title: "Sign out", url: "#")
+        component.search_form(search_action_url: "/search")
+      end
+    end
+
+    def with_custom_account_link
+      render(CitizensAdviceComponents::Header.new) do |component|
+        component.skip_links(skip_links)
+        component.links(links)
+        component.custom_account_link do
+          "Custom account link HTML"
+        end
+        component.search_form(search_action_url: "/search")
+      end
+    end
+
+    def with_no_search_form
+      render(CitizensAdviceComponents::Header.new) do |component|
+        component.skip_links(skip_links)
+        component.links(links)
+        component.account_link(title: "Sign in", url: "#")
+      end
+    end
+
+    private
+
+    def links
+      [{ title: "Cymraeg", url: "?lang=cy" }]
+    end
+
+    def skip_links
+      [{ title: "Skip to main content", url: "#content" }]
+    end
+  end
+end
