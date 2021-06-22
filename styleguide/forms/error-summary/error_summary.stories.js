9ad08ae28ea69f9b10059bca5cd575d88e9a011c
davidsauntson <david.sauntson@citizensadvice.org.uk>
2021-06-22 16:02:38 +0100

    feat: use html for error summary

---
+++ styleguide/forms/error-summary/error_summary.stories.js
@@ -0,0 +1,10 @@
+import docs from './error-summary-docs.mdx';
+import templateExample from '../../examples/error_summary/example.html';
+
+export default {
+  title: 'Components/Error Summary',
+  parameters: { docs: { page: docs } },
+};
+
+export const Example = () => templateExample;
+Example.parameters = { docs: { source: { code: templateExample } } };
