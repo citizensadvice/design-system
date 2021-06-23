074b0666aa7af0e879fa04a9aa6a664cd68f171d
davidsauntson <david.sauntson@citizensadvice.org.uk>
2021-06-23 09:11:18 +0100

    test: form field spec

--- engine/spec/components/form_field_spec.rb
+++ engine/spec/components/form_field_spec.rb
@@ -3,23 +3,23 @@
 RSpec.describe CitizensAdviceComponents::FormField, type: :component do
   let(:subject) do
     render_inline CitizensAdviceComponents::FormField.new(
-     error: error.presence
+      error: error.presence
     )
   end
 
   let(:error) { false }
 
   context "when there is no error" do
-    it "does not append the error class" do 
+    it "does not append the error class" do
       expect(subject.css(".cads-form-field").attribute("class").value).to_not include("cads-form-field--has-error")
     end
   end
 
   context "when there is an error" do
-
-    let(:error) { true 
-    }
-    it "appends the error class" do 
+    let(:error) do
+      true
+    end
+    it "appends the error class" do
       expect(subject.css(".cads-form-field").attribute("class").value).to include("cads-form-field--has-error")
     end
   end
