53ab6758590436339ff617d789ac43d0779b93d1
David Rapson <david.rapson@citizensadvice.org.uk>
2021-06-25 11:46:35 +0100

    Placeholder notice banner specs

---
+++ engine/spec/components/notice_banner_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe CitizensAdviceComponents::NoticeBanner, type: :component do
+  it "has a label"
+
+  it "renders content block"
+
+  context "when no content present" do
+    it "does not render"
+  end
+end
