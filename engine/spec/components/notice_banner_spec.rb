d7c7bf5a222c126f0e5c0f54eb021c14deb6d9bc
David Rapson <david.rapson@citizensadvice.org.uk>
2021-06-25 15:27:46 +0100

    Add component definition and specs

--- engine/spec/components/notice_banner_spec.rb
+++ engine/spec/components/notice_banner_spec.rb
@@ -1,11 +1,27 @@
 # frozen_string_literal: true
 
 RSpec.describe CitizensAdviceComponents::NoticeBanner, type: :component do
-  it "has a label"
+  subject(:component) do
+    render_inline(CitizensAdviceComponents::NoticeBanner.new(label: "Example label")) do
+      "Example content"
+    end
+  end
 
-  it "renders content block"
+  it "has a label" do
+    expect(component.at("span").text).to include "Example label"
+  end
+
+  it "renders content block" do
+    expect(component.text).to include "Example content"
+  end
 
   context "when no content present" do
-    it "does not render"
+    subject(:component) do
+      render_inline(CitizensAdviceComponents::NoticeBanner.new(label: "Example label"))
+    end
+
+    it "does not render" do
+      expect(component.at('.cads-notice-banner')).to_not be_present
+    end
   end
 end
