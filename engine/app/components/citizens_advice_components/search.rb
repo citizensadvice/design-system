9fda9c9b4c7ab2211578214816e956689ea5544d
David Rapson <david.rapson@citizensadvice.org.uk>
2021-06-25 17:43:53 +0100

    Add search view component

---
+++ engine/app/components/citizens_advice_components/search.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module CitizensAdviceComponents
+  class Search < Base
+    attr_reader :value, :param_name
+
+    def initialize(value: nil, param_name: nil)
+      super
+      @value = value
+      @param_name = param_name || :q
+    end
+  end
+end
