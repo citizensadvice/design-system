21761abb137cd607b7f1c254b2cdc3477c5e7d50
David Rapson <david.rapson@citizensadvice.org.uk>
2021-06-28 12:38:00 +0100

    Add page review component
    - Requires a date object for the review date
    - Supplies a default date format, can be overridden

---
+++ engine/app/components/citizens_advice_components/page_review.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module CitizensAdviceComponents
+  class PageReview < Base
+    def initialize(page_review_date:, date_format: nil)
+      super
+      @date_format = date_format || "%d %B %Y"
+      @page_review_date = page_review_date
+    end
+
+    def call
+      content_tag(
+        :p,
+        t("citizens_advice_components.page_review.body_html", date: formatted_date),
+        class: "cads-page-review"
+      )
+    end
+
+    def formatted_date
+      I18n.l(@page_review_date, format: @date_format)
+    end
+
+    def render?
+      @page_review_date.present?
+    end
+  end
+end
