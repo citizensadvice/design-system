63f8de613c9b2ffec91cde5588fc14d6407ef249
David Rapson <david.rapson@citizensadvice.org.uk>
2021-06-29 16:32:57 +0100

    Rubocop fix

--- testing/features/components/step_definitions/common_steps.rb
+++ testing/features/components/step_definitions/common_steps.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-When("I am able to skip to the {skip-link-area} part of the page labelled {string}" ) do |area, description|
+When("I am able to skip to the {skip-link-area} part of the page labelled {string}") do |area, description|
   @component.tab_to(area)
 
   expect(@component.active_element.text).to eq(description)
