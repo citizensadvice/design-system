b847222a74af29b25e50d9f6ed7078f147bdcf19
David Rapson <david.rapson@citizensadvice.org.uk>
2021-06-29 12:59:34 +0100

    Update footer docs with new examples

--- testing/features/support/components/footer/default.rb
+++ testing/features/support/components/footer/default.rb
@@ -2,7 +2,7 @@
 
 module Footer
   class Default < Minimal
-    set_url "/iframe.html?id=components-footer--default-story&viewMode=story"
+    set_url "/iframe.html?id=components-footer--default&viewMode=story"
 
     sections :category_titles, ".cads-grid-col-md-3 > h2" do
       elements :links, :xpath, "./following-sibling::ul[1]/li/a"
