cc6c95567bb924009948d58a2d57613e60de7f88
Marcelo Nicolosi <celonicolosi@hotmail.com>
2021-07-05 15:27:47 +0100

    Add .feature and _steps.rb for Success Message

---
+++ testing/features/forms/step_definitions/success_message_steps.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+Given('a Success Message form is on the page') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('a Success Message feedback is present') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
---
+++ testing/features/forms/success_message.feature
@@ -0,0 +1,10 @@
+Feature: Success Message
+
+  Success Message provides feedback about
+  users opinions regarding the advice
+
+  Background:
+    Given a Success Message form is on the page
+
+  Scenario: Success Message is present on the page
+    Then a Success Message feedback is present
