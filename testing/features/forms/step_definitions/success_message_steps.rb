a5aec168970f2338ea298ca427f39f207e324ee7
Marcelo Nicolosi <celonicolosi@hotmail.com>
2021-07-05 16:08:45 +0100

    Fix rubocop offenses

--- testing/features/forms/step_definitions/success_message_steps.rb
+++ testing/features/forms/step_definitions/success_message_steps.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
-Given('a Success Message form is on the page') do
+Given("a Success Message form is on the page") do
   @form = SuccessMessage::Example.new.tap(&:load)
 end
 
-Then('a Success Message feedback is present') do
+Then("a Success Message feedback is present") do
   expect(@form).to have_feedback
 end
-
