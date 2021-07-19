8ebddbc07c0ca6df19d3e5401210e66bbb98758a
David Rapson <david.rapson@citizensadvice.org.uk>
2021-07-19 14:30:38 +0100

    Update footer syntax

--- demo/app/components/app_footer_component.rb
+++ demo/app/components/app_footer_component.rb
@@ -15,51 +15,51 @@ class AppFooterComponent < ViewComponent::Base
     [{
       title: "Advice",
       links: [
-        { url: "/benefits/", label: "Benefits" },
-        { url: "/work/", label: "Work" },
-        { url: "/debt-and-money/", label: "Debt and Money" },
-        { url: "/consumer/", label: "Consumer" },
-        { url: "/family/", label: "Family" },
-        { url: "/housing/", label: "Housing" },
-        { url: "/law-and-courts/", label: "Law and courts" },
-        { url: "/immigration/", label: "Immigration" },
-        { url: "/health/", label: "Health" }
+        { url: "/benefits/", title: "Benefits" },
+        { url: "/work/", title: "Work" },
+        { url: "/debt-and-money/", title: "Debt and Money" },
+        { url: "/consumer/", title: "Consumer" },
+        { url: "/family/", title: "Family" },
+        { url: "/housing/", title: "Housing" },
+        { url: "/law-and-courts/", title: "Law and courts" },
+        { url: "/immigration/", title: "Immigration" },
+        { url: "/health/", title: "Health" }
       ]
     },
      {
        title: "Resources and tools",
        links: [
-         { url: "/about-us/how-citizens-advice-works/products-and-services/adviser-resources/", label: "Advisor resources" },
-         { url: "/about-us/how-we-provide-advice/our-prevention-work/education/", label: "Education resources" },
-         { url: "/resources-and-tools/about-this-site/accessibility/", label: "Accessibility" },
-         { url: "/resources-and-tools/about-this-site/accessibility/", label: "Site search" },
-         { url: "/resources-and-tools/search-navigation-tools/a-to-z-of-advice/", label: "A-Z of advice" }
+         { url: "/about-us/how-citizens-advice-works/products-and-services/adviser-resources/", title: "Advisor resources" },
+         { url: "/about-us/how-we-provide-advice/our-prevention-work/education/", title: "Education resources" },
+         { url: "/resources-and-tools/about-this-site/accessibility/", title: "Accessibility" },
+         { url: "/resources-and-tools/about-this-site/accessibility/", title: "Site search" },
+         { url: "/resources-and-tools/search-navigation-tools/a-to-z-of-advice/", title: "A-Z of advice" }
        ]
      },
      {
        title: "More from us",
        links: [
-         { url: "/about-us/", label: "About us" },
-         { url: "/about-us/how-citizens-advice-works/who-we-are-and-what-we-do/annual-reports/", label: "Annual reports" },
-         { url: "/about-us/contact-us/contact-us/contact-us/", label: "Contact us" },
-         { url: "/about-us/contact-us/contact-us/make-a-complaint-about-us/", label: "Complaints" },
-         { url: "/about-us/how-citizens-advice-works/media/", label: "Media" },
-         { url: "/about-us/policy/", label: "Policy research" },
-         { url: "/about-us/support-us/", label: "Support us" },
-         { url: "/about-us/support-us/volunteering/", label: "Volunteering" },
-         { url: "/about-us/how-citizens-advice-works/job-and-voluntary-opportunities/", label: "Jobs" }
+         { url: "/about-us/", title: "About us" },
+         { url: "/about-us/how-citizens-advice-works/who-we-are-and-what-we-do/annual-reports/", title: "Annual reports" },
+         { url: "/about-us/contact-us/contact-us/contact-us/", title: "Contact us" },
+         { url: "/about-us/contact-us/contact-us/make-a-complaint-about-us/", title: "Complaints" },
+         { url: "/about-us/how-citizens-advice-works/media/", title: "Media" },
+         { url: "/about-us/policy/", title: "Policy research" },
+         { url: "/about-us/support-us/", title: "Support us" },
+         { url: "/about-us/support-us/volunteering/", title: "Volunteering" },
+         { url: "/about-us/how-citizens-advice-works/job-and-voluntary-opportunities/", title: "Jobs" }
        ]
      },
      {
        title: "About Citizens Advice",
        links: [
-         { url: "/about-us/how-we-provide-advice/", label: "How we provide advice" },
-         { url: "/about-us/difference-we-make/", label: "The difference we make" },
-         { url: "/about-us/support-us/", label: "Support us" },
-         { url: "/about-us/how-citizens-advice-works/", label: "How Citizens Advice works" },
-         { url: "/resources-and-tools/about-this-site/disclaimer-and-copyright/", label: "Disclaimer and Copyright" },
-         { url: "/about-us/citizens-advice-privacy-policy/", label: "Privacy and cookies" },
-         { url: "http://example.com", label: "External link", icon: "external-link" }
+         { url: "/about-us/how-we-provide-advice/", title: "How we provide advice" },
+         { url: "/about-us/difference-we-make/", title: "The difference we make" },
+         { url: "/about-us/support-us/", title: "Support us" },
+         { url: "/about-us/how-citizens-advice-works/", title: "How Citizens Advice works" },
+         { url: "/resources-and-tools/about-this-site/disclaimer-and-copyright/", title: "Disclaimer and Copyright" },
+         { url: "/about-us/citizens-advice-privacy-policy/", title: "Privacy and cookies" },
+         { url: "http://example.com", title: "External link", icon: "external-link" }
        ]
      }]
   end
