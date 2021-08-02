80f17d3dc1aba3f3492c0e63031de8f48301b1df
David Rapson <david.rapson@citizensadvice.org.uk>
2021-08-02 17:16:30 +0100

    Autocorrect rubocop warning

--- testing/features/support/drivers/browserstack.rb
+++ testing/features/support/drivers/browserstack.rb
@@ -63,10 +63,8 @@ module Drivers
     end
 
     def cucumber_results_connection
-      @cucumber_results_connection ||= begin
-        ::Faraday.new(url: "https://api.browserstack.com/automate/sessions/#{id}.json").tap do |connection|
-          connection.basic_auth(browserstack_username, browserstack_api_key)
-        end
+      @cucumber_results_connection ||= ::Faraday.new(url: "https://api.browserstack.com/automate/sessions/#{id}.json").tap do |connection|
+        connection.basic_auth(browserstack_username, browserstack_api_key)
       end
     end
 
