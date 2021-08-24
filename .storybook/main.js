3a493eefd18aaf2cf0d41d3fb08ecd0603a83d5b
David Rapson <david.rapson@citizensadvice.org.uk>
2021-08-24 09:28:47 +0100

    Minimal storybook addons
    We were installing @storybook/addon-essentials only to disable nearly
    everything. Swap out with minimal alternatives.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,4 @@
 module.exports = {
   stories: ['../styleguide/**/*.stories.@(js|mdx)'],
-  addons: [
-    {
-      name: '@storybook/addon-essentials',
-      options: {
-        backgrounds: false,
-        controls: false,
-        actions: false,
-      },
-    },
-  ],
+  addons: ['@storybook/addon-docs', '@storybook/addon-postcss'],
 };
