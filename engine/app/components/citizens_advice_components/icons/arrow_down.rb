e3beae1b16feeba8a49d74741f995efa866d327c
davidsauntson <david.sauntson@citizensadvice.org.uk>
2021-09-02 09:08:04 +0100

    feat: rename icons correctly

---
+++ engine/app/components/citizens_advice_components/icons/arrow_down.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module CitizensAdviceComponents
+  module Icons
+    class ArrowDown < CitizensAdviceComponents::Icons::Base
+    end
+  end
+end
--- engine/app/components/citizens_advice_components/icons/base.rb
+++ engine/app/components/citizens_advice_components/icons/base.rb
@@ -3,15 +3,11 @@
 module CitizensAdviceComponents
   module Icons
     class Base < CitizensAdviceComponents::Base
+      attr_reader :size
+      
       def initialize(size: nil)
         super
         @size = fetch_or_fallback(allowed_values: [nil, :small, :large], given_value: size, fallback: nil)
-      end
-
-      def size
-        return if @size.blank?
-
-        size
       end
     end
   end
