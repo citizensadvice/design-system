f4cc97b6de781f299cc966ce93239efaa342cc5a
David Rapson <david.rapson@citizensadvice.org.uk>
2021-09-21 11:48:51 +0100

    Inline format_rows method in Textarea

--- engine/app/components/citizens_advice_components/textarea.rb
+++ engine/app/components/citizens_advice_components/textarea.rb
@@ -7,13 +7,9 @@ module CitizensAdviceComponents
     DEFAULT_ROWS = 8
 
     def initialize(rows: DEFAULT_ROWS, **args)
-      @rows = format_rows(rows)
+      @rows = rows.to_i.zero? ? DEFAULT_ROWS : rows
       @base_input_args = args.merge(type: nil)
       super(@base_input_args)
-    end
-
-    def format_rows(rows)
-      rows.to_i.zero? ? DEFAULT_ROWS : rows
     end
 
     def base_input_attributes
