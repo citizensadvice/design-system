e6d1a4d472ed33a3e5b335a530a924e5c943d947
David Rapson <david.rapson@citizensadvice.org.uk>
2021-09-22 09:21:16 +0100

    Update table specs with failing render checks

--- engine/spec/components/citizens_advice_components/table_spec.rb
+++ engine/spec/components/citizens_advice_components/table_spec.rb
@@ -1,55 +1,64 @@
 # frozen_string_literal: true
 
 RSpec.describe CitizensAdviceComponents::Table, type: :component do
-  subject(:component) do
-    render_inline(
+  context "when missing headers" do
+    subject(:component) do
       described_class.new(
-        header: header,
-        rows: rows,
-        caption: caption.presence
+        header: [],
+        rows: [["Cell 1", "Cell 2"]]
       )
-    )
+    end
+
+    it "does not render" do
+      render_inline component
+      expect(rendered_component).to have_no_table
+    end
   end
 
-  let(:header) do
-    [
-      "Your location",
-      "Post box collection times"
-    ]
+  context "when missing rows" do
+    subject(:component) do
+      described_class.new(
+        header: ["Header 1", "Header 2"],
+        rows: []
+      )
+    end
+
+    it "does not render" do
+      render_inline component
+      expect(rendered_component).to have_no_table
+    end
   end
 
-  let(:rows) do
-    [
-      [
-        "City or town",
-        "9am to 6.30pm"
-      ],
-      [
-        "Areas with lots of businesses - known as commercial",
-        "9am to 7.30pm"
-      ],
-      [
-        "Very rural areas - for example, where there aren't many people",
-        "9am to 4pm"
-      ],
-      [
-        "Rest of the UK",
-        "9am to 5.30pm"
-      ]
-    ]
+  context "when valid table" do
+    subject(:component) do
+      described_class.new(
+        header: ["Your location", "Post box collection times"],
+        rows: [
+          ["City or town", "9am to 6.30pm"],
+          ["Areas with lots of businesses - known as commercial", "9am to 7.30pm"],
+          ["Very rural areas - for example, where there aren't many people", "9am to 4pm"],
+          ["Rest of the UK", "9am to 5.30pm"]
+        ]
+      )
+    end
+
+    it "matches snapshot" do
+      expect(render_inline(component).to_html).to match_snapshot("table")
+    end
   end
 
-  let(:caption) { "I am a nice table" }
+  context "when a caption is present" do
+    subject(:component) do
+      described_class.new(
+        header: ["Header 1", "Header 1"],
+        rows: [["Cell 1", "Cell 2"]],
+        caption: "Example caption"
+      )
+    end
 
-  it "renders correctly" do
-    expect(component.to_html).to match_snapshot("table")
-  end
-
-  context "when there is no caption" do
-    let(:caption) { nil }
-
-    it "does not render a caption" do
-      expect(component.css("table caption")).to be_empty
+    it "renders a caption" do
+      render_inline component
+      expect(rendered_component).to have_css "caption", text: "Example caption"
     end
   end
 end
