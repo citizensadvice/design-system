fff69125b73614b0c4dcfaaacae697dc5fd06f65
davidsauntson <david.sauntson@citizensadvice.org.uk>
2021-09-28 17:01:11 +0100

    feat: use base class to render svg and attrs

--- engine/app/components/citizens_advice_components/icons/base.rb
+++ engine/app/components/citizens_advice_components/icons/base.rb
@@ -9,6 +9,14 @@ module CitizensAdviceComponents
         super
         @size = fetch_or_fallback(allowed_values: [nil, :small, :large], given_value: size, fallback: nil)
       end
+
+      def css_classes
+        ["cads-icon", "cads-icon--#{@size}", "cads-icon--#{human_class_name}"].join(" ")
+      end
+
+      def human_class_name
+        self.class.name.demodulize.underscore.dasherize
+      end
     end
   end
 end
