4e47051e91984465617a855c8a6d41d498279eef
David Rapson <david.rapson@citizensadvice.org.uk>
2021-09-29 16:25:17 +0100

    Improve SCSS tests
    - Structure SCSS tests around grouping (e.g. settings, grid)
    - Add usage tests for mixins to assert output and confirm they can be
      imported standalone
    
    This is also a good excuse to use `@use` to include any unresolved
    variables. We could stick with `@import` but as we require dart sass the
    `@use` syntax is preferred.

---
+++ scss/7-utilities/__tests__/utilities.test.js
@@ -0,0 +1,22 @@
+/* eslint-env jest */
+const glob = require('glob');
+const sass = require('sass');
+
+test.each(glob.sync('scss/7-utilities/_*.scss'))(
+  '%p can be imported standalone',
+  (file) => {
+    expect(() =>
+      sass.renderSync({
+        includePaths: ['scss/'],
+        file,
+      })
+    ).not.toThrow();
+  }
+);
+
+test('visibility styles match snapshot', () => {
+  const output = sass.renderSync({
+    file: 'scss/7-utilities/_visibility.scss',
+  });
+  expect(output.css.toString()).toMatchSnapshot();
+});
