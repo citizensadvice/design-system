396694d744e060a635ce441cd4eea9de15b6311d
davidsauntson <david.sauntson@citizensadvice.org.uk>
2021-09-29 09:06:03 +0100

    fix: linting

--- engine/app/components/citizens_advice_components/icons/base.rb
+++ engine/app/components/citizens_advice_components/icons/base.rb
@@ -4,7 +4,7 @@ module CitizensAdviceComponents
   module Icons
     class Base < CitizensAdviceComponents::Base
       attr_reader :size
-      
+
       def initialize(size: nil, icon_name: nil)
         super
         @size = fetch_or_fallback(allowed_values: [nil, :small, :large], given_value: size, fallback: nil)
--- engine/spec/components/citizens_advice_components/icons/base_spec.rb
+++ engine/spec/components/citizens_advice_components/icons/base_spec.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 RSpec.describe CitizensAdviceComponents::Icons::Base, type: :component do
   subject(:component) do
-    render_inline CitizensAdviceComponents::Icons::Base.new(
+    render_inline described_class.new(
       size: size.presence,
       icon_name: "CitizensAdviceComponents::Icons::MyGreatIcon"
     )
@@ -16,7 +18,7 @@ RSpec.describe CitizensAdviceComponents::Icons::Base, type: :component do
     expect(component.css(".cads-icon--small")).to be_present
   end
 
-  it "renders the correct icon class" do 
+  it "renders the correct icon class" do
     expect(component.css(".cads-icon--my-great-icon")).to be_present
   end
 end
