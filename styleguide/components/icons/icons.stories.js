72dd7adb456d3eeebff7cadb4cdcb3371ce5eefd
davidsauntson <david.sauntson@citizensadvice.org.uk>
2021-10-05 16:46:46 +0100

    docs: add combined icon docs and examples

--- styleguide/components/icons/icons.stories.js
+++ styleguide/components/icons/icons.stories.js
@@ -1,5 +1,7 @@
 import docs from './icons-docs.mdx';
 import icons from '../../examples/icons/icons.html';
+import upDown from '../../examples/icons/arrow_up_down.html';
+import plusMinus from '../../examples/icons/plus_minus.html';
 
 export default {
   title: 'Components/SVG Icons',
@@ -8,3 +10,8 @@ export default {
 
 export const Default = () => icons;
 Default.parameters = { docs: { source: { code: icons } } };
+
+export const UpDown = () => upDown;
+UpDown.parameters = { docs: { source: { code: upDown } } };
+export const PlusMinus = () => plusMinus;
+PlusMinus.parameters = { docs: { source: { code: plusMinus } } };
