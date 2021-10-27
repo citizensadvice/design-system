4b61ea3cd86b05136c9cb1118c0302427331d8e6
davidsauntson <david.sauntson@citizensadvice.org.uk>
2021-10-27 14:24:54 +0100

    feat: remove redundant initialise

--- engine/app/components/citizens_advice_components/checkable/radio.rb
+++ engine/app/components/citizens_advice_components/checkable/radio.rb
@@ -1,10 +1,6 @@
 module CitizensAdviceComponents
   module Checkable 
     class Radio < Base
-      def initialize(label:, value:, checked: false, **additional_attributes)
-        super(label: label, value: value, checked: checked, **additional_attributes)
-      end
-
       def input_type
         "radio"
       end
