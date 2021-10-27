a5aae935d61f20645c35cdddbf4ce3c2da1cd085
davidsauntson <david.sauntson@citizensadvice.org.uk>
2021-10-27 16:07:57 +0100

    feat: add more checkbox preview examples

--- engine/previews/citizens_advice_components/checkbox_group_preview.rb
+++ engine/previews/citizens_advice_components/checkbox_group_preview.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 module CitizensAdviceComponents
   class CheckboxGroupPreview < ViewComponent::Preview
-    def default
+    def basic
       render CheckboxGroup.new(
         legend: "This is the group legend",
         name: "my-radio-group"
@@ -10,13 +10,84 @@ module CitizensAdviceComponents
       end
     end
 
+    def error
+      render CheckboxGroup.new(
+        legend: "Example radio group",
+        name: "radio-buttons-error",
+        options: {
+          error_message: "Select an option"
+        }
+      ) do |c|
+        c.checkboxes(checkboxes)
+      end
+    end
+
+    def optional
+      render CheckboxGroup.new(
+        legend: "Example radio group",
+        name: "radio-buttons-optional",
+        options: {
+          optional: true
+        }
+      ) do |c|
+        c.checkboxes(checkboxes)
+      end
+    end
+
+    def hint
+      render CheckboxGroup.new(
+        legend: "Example radio group",
+        name: "radio-buttons-hint",
+        options: {
+          hint: "This is the hint text"
+        }
+      ) do |c|
+        c.checkboxes(checkboxes)
+      end
+    end
+
+   
+
+    def long
+      render CheckboxGroup.new(
+        legend: "Example radio group",
+        name: "radio-buttons-long"
+      ) do |c|
+        c.checkboxes(long_checkboxes)
+      end
+    end
+
+    def additional_attributes
+      render CheckboxGroup.new(
+        legend: "Example radio group",
+        name: "radio-buttons-attrs"
+      ) do |c|
+        c.checkboxes(additional_attribute_checkboxes)
+      end
+    end
+
+
     private
     
     def checkboxes 
       [
-        { label: "Option 1", value: 1},
-        { label: "Option 2", value: 2},
-        { label: "Option 3", value: 3}
+        { label: "Option 1", value: 1 },
+        { label: "Option 2", value: 2 },
+        { label: "Option 3", value: 3 }
+      ]
+    end
+
+    def long_checkboxes
+      [
+        { label: "This option is pretty long and will probably wrap over multiple lines", value: "1" },
+        { label: "This option is also pretty long, too long even, and will almost definitely wrap over multiple lines", value: "2" }
+      ]
+    end
+
+    def additional_attribute_checkboxes
+      [
+        { label: "Option 1", value: "1", "data-testid": "first-input" },
+        { label: "Option 2", value: "2", "data-testid": "second-input" }
       ]
     end
   end
