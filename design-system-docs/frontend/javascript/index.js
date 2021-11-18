e08acfbb5b85ee08e0ef3534cfe4eef70b973b40
David Sauntson <davidsauntson@gmail.com>
2021-11-18 11:13:23 +0000

    feat: initial bridgetown installation

---
+++ design-system-docs/frontend/javascript/index.js
@@ -0,0 +1,7 @@
+import "index.scss"
+
+// Import all javascript files from src/_components
+const componentsContext = require.context("bridgetownComponents", true, /.js$/)
+componentsContext.keys().forEach(componentsContext)
+
+console.info("Bridgetown is loaded!")
