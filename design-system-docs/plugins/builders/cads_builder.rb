7d1ad18ba2ce530a71430f423f2a422ad457cafd
David Sauntson <david.sauntson@citizensadvice.org.uk>
2021-11-24 10:51:52 +0000

    Design system docs (#1659)
    * feat: initial bridgetown installation
    
    * feat: add design system
    
    * feat: add support for view components
    
    * feat: remove oob templates
    
    * feat: add cads_components collection
    
    * feat: add first cads component
    
    * feat: webpack config
    
    * chore: update linters
    
    * Re-add scss import
    
    Co-authored-by: David Saunston <davidsaunston@CITA4652-MAC-L.local>
    Co-authored-by: David Rapson <david.rapson@citizensadvice.org.uk>

---
+++ design-system-docs/plugins/builders/cads_builder.rb
@@ -0,0 +1,26 @@
+require "action_dispatch"
+require "rails/engine"
+require "citizens_advice_components"
+require "haml/plugin"
+
+ActionView::Template.register_template_handler(:haml, Haml::Plugin)
+
+module Builders
+    class CadsBuilder < SiteBuilder
+        def build
+            load_cads
+        end
+
+        def load_cads
+            site.config.loaded_cads ||= begin
+                cads_loader = Zeitwerk::Loader.new
+                CitizensAdviceComponents::Engine.config.autoload_paths.each do |path|
+                    cads_loader.push_dir path
+                end
+                cads_loader.setup
+                cads_loader.eager_load
+                true
+            end     
+        end
+    end
+end
---
+++ design-system-docs/src/_cads_components/targeted_content.md
@@ -0,0 +1,5 @@
+---
+layout: component
+---
+
+# Targeted Content
