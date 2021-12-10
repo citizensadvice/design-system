c1b978a0d3144de3d86c79fedbc7aa7989fdf9b5
marianayovcheva <mariana.yovcheva@citizensadvice.org.uk>
2021-12-10 11:30:53 +0000

    fix: changing the error summary docs and the preview file to use plural (errors)

--- engine/previews/citizens_advice_components/error_summary_preview.rb
+++ engine/previews/citizens_advice_components/error_summary_preview.rb
@@ -4,14 +4,10 @@ module CitizensAdviceComponents
   class ErrorSummaryPreview < ViewComponent::Preview
     def example
       render CitizensAdviceComponents::ErrorSummary.new do |c|
-        c.error(
-          href: "#your-name-input",
-          message: "Enter your full name"
-        )
-        c.error(
-          href: "#your-email-input",
-          message: "Enter an email address in the correct format, like name@example.com"
-        )
+        c.errors([
+          { href: "#your-name-input", message: "Enter your full name" },
+          { href: "#your-email-input", message: "Enter an email address in the correct format, like name@example.com" }
+        ])
       end
     end
   end
