2b779dcb1a7122f1c4d1f84a93fbbba193359714
marianayovcheva <mariana.yovcheva@citizensadvice.org.uk>
2021-12-17 10:54:58 +0000

    feat: on this page docs working with ts

--- styleguide/components/on-this-page/on-this-page.stories.js
+++ styleguide/components/on-this-page/on-this-page.stories.js
@@ -1,8 +1,8 @@
 import initOnThisPage from '../../../src/ts/on-this-page/on-this-page';
 
 import docs from './on-this-page.mdx';
-import templateDefault from '../../examples/on_this_page/example.html';
-import templateMinimal from '../../examples/on_this_page/with_nested_links.html';
+import templateExample from '../../examples/on_this_page/example.html';
+import templateWithNestedLinks from '../../examples/on_this_page/with_nested_links.html';
 
 function decorateWithBehaviour(Story) {
   setTimeout(() => initOnThisPage());
@@ -12,11 +12,11 @@ function decorateWithBehaviour(Story) {
 export default {
   title: 'Components/On this page',
   parameters: { docs: { page: docs } },
-  decorators: [decorateWithBehaviour],
 };
 
-export const Example = () => templateDefault;
-Example.parameters = { docs: { source: { code: templateDefault } } };
+export const Example = () => templateExample;
+Example.parameters = { docs: { source: { code: templateExample } } };
 
-export const WithNestedLinks = () => templateMinimal;
-WithNestedLinks.parameters = { docs: { source: { code: templateMinimal } } };
+export const WithNestedLinks = () => templateWithNestedLinks;
+WithNestedLinks.parameters = { docs: { source: { code: templateWithNestedLinks } } };
+WithNestedLinks.decorators = [decorateWithBehaviour];
