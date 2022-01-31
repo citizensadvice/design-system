a4905f68ccba9b23f54c6402e3b6bb8bc3bff77b
Marcelo Nicolosi <celonicolosi@hotmail.com>
2022-01-31 17:59:56 +0000

    Run tests on Saucelabs and Browserstack

--- testing/features/support/drivers/all.rb
+++ testing/features/support/drivers/all.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
+require_relative "browserstack"
+require_relative "browserstack/payload_values_generator"
 require_relative "saucelabs"
 require_relative "saucelabs/payload_values_generator"
