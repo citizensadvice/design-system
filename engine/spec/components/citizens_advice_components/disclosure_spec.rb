4f45ea6b374ec0cd9166241579c746bd85de1b63
marianayovcheva <mariana.yovcheva@citizensadvice.org.uk>
2022-02-14 17:16:27 +0000

    feat: adjusting disclosure specs

--- engine/spec/components/citizens_advice_components/disclosure_spec.rb
+++ engine/spec/components/citizens_advice_components/disclosure_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe CitizensAdviceComponents::Disclosure, type: :component do
-  let(:subject) do
+  subject(:component) do
     render_inline described_class.new(
       closed_summary: closed_summary.presence,
       open_summary: open_summary.presence
@@ -13,30 +13,30 @@ RSpec.describe CitizensAdviceComponents::Disclosure, type: :component do
   let(:content) { "The disclosure content" }
 
   it "renders the closed summary text" do
-    expect(subject.css("[data-label-when-hiding='View template letter']")).to be_present
+    expect(component.css("[data-label-when-hiding='View template letter']")).to be_present
   end
 
   it "renders the open summary text" do
-    expect(subject.css("[data-label-when-showing='Hide template letter']")).to be_present
+    expect(component.css("[data-label-when-showing='Hide template letter']")).to be_present
   end
 
   it "renders the disclosure content" do
-    expect(subject.text.strip).to include "The disclosure content"
+    expect(component.text.strip).to include "The disclosure content"
   end
 
   it "has aria-expand set to false on initial render" do
-    expect(subject.css("[aria-expanded=false]")).to be_present
+    expect(component.css("[aria-expanded=false]")).to be_present
   end
 
   it "has aria-controls set to the id of the disclosure content" do
-    expect(subject.css("[aria-controls=view-template-letter-disclosure-details]")).to be_present
+    expect(component.css("[aria-controls=view-template-letter-disclosure-details]")).to be_present
   end
 
   context "when there is no open summary text" do
     let(:open_summary) { nil }
 
     it "shows the closed summary text when open" do
-      expect(subject.css("[data-label-when-showing='View template letter']")).to be_present
+      expect(component.css("[data-label-when-showing='View template letter']")).to be_present
     end
   end
 
@@ -44,15 +44,20 @@ RSpec.describe CitizensAdviceComponents::Disclosure, type: :component do
     let(:closed_summary) { nil }
 
     it "does not render" do
-      expect(subject.children).to be_empty
+      expect(component.children).to be_empty
     end
   end
 
   context "when there is no disclosure content" do
-    let(:content) { nil }
+    subject(:component) do
+      render_inline described_class.new(
+        closed_summary: closed_summary.presence,
+        open_summary: open_summary.presence
+      )
+    end
 
     it "does not render" do
-      expect(subject.at(".cads-disclosure")).not_to be_present
+      expect(component.at(".cads-disclosure")).not_to be_present
     end
   end
 end
