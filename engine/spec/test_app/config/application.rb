0aea46a1251bec540bbe223ab75cb453d38aca09
David Rapson <david.rapson@citizensadvice.org.uk>
2022-03-14 11:53:54 +0000

    Test against multiple versions of Rails
    Test engine against Rails 6, 6.1, and 7 in CI.

--- engine/spec/test_app/config/application.rb
+++ engine/spec/test_app/config/application.rb
@@ -12,6 +12,6 @@ Bundler.require(*Rails.groups)
 
 module Demo
   class Application < Rails::Application
-    config.load_defaults 6.1
+    config.load_defaults (ENV["RAILS_VERSION"] ? ENV["RAILS_VERSION"][0..2] : "6.1").to_s
   end
 end
