523f65b6c63f12d6d21b98bc9282367e70d720a7
David Rapson <david.rapson@citizensadvice.org.uk>
2022-03-16 09:32:18 +0000

    Add initial Cypress tests to demo app
    - Add axe test for all component previews (replaces pa11y)
    - Migration disclosure, navigation, targeted-content, and skip links
      tests
    - Add new pagination test to demonstrate benefits of running against the
      demo app

---
+++ demo/cypress/integration/pagination.spec.js
@@ -0,0 +1,26 @@
+describe('Pagination', () => {
+  it('should allow paginating', () => {
+    /**
+     * Uses the with_dynamic_query_parameters preview which is a
+     * working pagination example to test interaction states.
+     */
+    cy.visitComponentUrl('pagination/with_dynamic_query_parameters');
+
+    assertCurrentPage(1);
+    cy.findByText('Next').click();
+    assertCurrentPage(2);
+
+    cy.findByText('Last').click();
+    assertCurrentPage(100);
+
+    cy.findByText('Previous').click();
+    assertCurrentPage(99);
+
+    cy.findByText('First').click();
+    assertCurrentPage(1);
+  });
+
+  function assertCurrentPage(pageNum) {
+    cy.findByRole('link', { current: 'page' }).should('have.text', pageNum);
+  }
+});
