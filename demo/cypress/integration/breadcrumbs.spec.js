d73e6e946d87c831f88ca11532219df15a02539f
David Rapson <david.rapson@citizensadvice.org.uk>
2022-03-24 17:37:04 +0000

    Migrate viewport based breadcrumb test to Cypress
    Migrate a basic breadcrumb scenario to cover collapsing behaviour that
    can't be well covered by view component specs and only partially covered
    by visual regression tests.

---
+++ demo/cypress/integration/breadcrumbs.spec.js
@@ -0,0 +1,18 @@
+describe('Breadcrumbs', () => {
+  it('renders a breadcrumb trail', () => {
+    cy.visitComponentUrl('breadcrumbs/collapsing');
+
+    cy.findByRole('link', { name: 'Home' }).should('be.visible');
+    cy.findByRole('link', { name: 'Immigration' }).should('be.visible');
+    cy.findByText('Staying in the UK').should('be.visible');
+  });
+
+  it('only renders a single item on smalls screens', () => {
+    cy.visitComponentUrl('breadcrumbs/collapsing');
+    cy.viewport('iphone-6');
+
+    cy.findByText('Home').should('not.be.visible');
+    cy.findByRole('link', { name: /Immigration/ }).should('be.visible');
+    cy.findByText('Staying in the UK').should('not.be.visible');
+  });
+});
