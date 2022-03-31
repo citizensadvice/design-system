c64ea420ba48ddd03e7150efd22efb22e760cbee
David Rapson <david.rapson@citizensadvice.org.uk>
2022-03-31 14:10:46 +0100

    Add support for component examples
    Adds a basic component template and index page along with a
    ComponentExample view component which allows rendering a component
    example along with source code and generated HTML.

--- design-system-docs/frontend/javascript/index.js
+++ design-system-docs/frontend/javascript/index.js
@@ -1,1 +1,7 @@
 import '../styles/index.scss';
+
+import initTargetedContent from '@citizensadvice/design-system/lib/targeted-content';
+import initDisclosure from '@citizensadvice/design-system/lib/disclosure/disclosure';
+
+initTargetedContent();
+initDisclosure();
