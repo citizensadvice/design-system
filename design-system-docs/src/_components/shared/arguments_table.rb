b6348d77ba5cd93fd4b972e158f3519899ac68b0
David Rapson <david.rapson@citizensadvice.org.uk>
2022-05-04 17:35:08 +0100

    Add support for component arguments table
    Extracts out a revised approach to component arguments tables from
    https://github.com/citizensadvice/design-system/pull/1874
    
    Longer term we'd like to pull this data from yard or something similar,
    so using an approach that pulls from data files makes sense for now.
    
    Makes a simple _data/component_arguments.yml file containing argument
    data and then extracts that within a shared component based on a key.

---
+++ design-system-docs/src/_components/shared/arguments_table.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module Shared
+  class ArgumentsTable < ViewComponent::Base
+    Bridgetown::ViewComponentHelpers.allow_rails_helpers :tag
+
+    include Bridgetown::ViewComponentHelpers
+
+    def initialize(component_name)
+      super
+
+      @component_name = component_name
+      @site = Bridgetown::Current.site
+    end
+
+    def call
+      render CitizensAdviceComponents::Table.new(
+        header: %w[Argument Description],
+        rows: data.map do |item|
+          [tag.code(item[:argument]), item[:description]]
+        end
+      )
+    end
+
+    def render?
+      data.any?
+    end
+
+    def data
+      # Maps to keys in _data/component_arguments.yml
+      @site.data.component_arguments[@component_name].to_a
+    end
+  end
+end
