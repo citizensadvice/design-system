17e99ca59ec6a135cd7a56e44a3423b844d3d81f
David Rapson <david.rapson@citizensadvice.org.uk>
2022-05-13 16:33:44 +0100

    Add fonts loader on button active script

--- demo/visual-regression/engine_scripts/onReadyButtonActive.js
+++ demo/visual-regression/engine_scripts/onReadyButtonActive.js
@@ -6,5 +6,16 @@ module.exports = async (page) => {
     { name: 'prefers-reduced-motion', value: 'reduce' },
   ]);
 
+  /**
+   * Wait for fonts to be loaded.
+   * Ensure that icon fonts are ready to avoid display inconsistencies
+   */
+  await page.waitFor(() =>
+    document.fonts.ready.then(() => {
+      console.log('Fonts loaded');
+      return true;
+    })
+  );
+
   await forcePseudoState({ page, selector: 'button', pseudoClass: 'active' });
 };
