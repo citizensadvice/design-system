b4c8be16ab2aaf5bd53196e14b3cc24b4e7c8207
davidsauntson <david.sauntson@citizensadvice.org.uk>
2022-06-28 17:28:32 +0100

    feat: add copy functionality to copy code button

--- design-system-docs/frontend/javascript/code-copy/index.js
+++ design-system-docs/frontend/javascript/code-copy/index.js
@@ -1,20 +1,31 @@
+import copy from 'clipboard-copy';
+
 export default function initCodeCopy() {
   const highlights = document.querySelectorAll('pre.highlight');
   if (highlights) {
-    highlights.forEach(h => addCopyButton((h)))
+    highlights.forEach((h) => addCopyButton(h));
   }
 }
 
 function addCopyButton(highlight) {
-  const buttonDiv = document.createElement("div")
-  buttonDiv.classList.add("highlight__buttons")
+  const buttonDiv = document.createElement('div');
+  buttonDiv.classList.add('highlight__buttons');
 
-  const button = document.createElement("button")
-  button.classList.add('cads-button')
-  button.classList.add('cads-button__tertiary')
-  button.textContent = "Copy code"
-  highlight.classList.add('highlight--with-button')
+  const button = document.createElement('button');
+  button.classList.add('cads-button');
+  button.classList.add('cads-button__tertiary');
+  button.textContent = 'Copy code';
 
-  buttonDiv.append(button)
-  highlight.prepend(buttonDiv)
+  button.addEventListener('click', function () {
+    copyCode(highlight);
+  });
+
+  highlight.classList.add('highlight--with-button');
+
+  buttonDiv.append(button);
+  highlight.prepend(buttonDiv);
+}
+
+function copyCode(highlight) {
+  copy(highlight.querySelector('code').innerText);
 }
--- design-system-docs/frontend/javascript/index.js
+++ design-system-docs/frontend/javascript/index.js
@@ -2,7 +2,7 @@ import '../styles/index.scss';
 
 import initTargetedContent from '@citizensadvice/design-system/lib/targeted-content';
 import initDisclosure from '@citizensadvice/design-system/lib/disclosure/disclosure';
-import initCodeCopy from "./code-copy";
+import initCodeCopy from './code-copy';
 
 initTargetedContent();
 initDisclosure();
