92e3b82aeef6f6dd788b8983ec7afcde8db59513
David Rapson <david.rapson@citizensadvice.org.uk>
2022-08-31 09:45:10 +0100

    Split docs assets into core and theme
    When rendering the header examples in the design system docs the theme
    styles were overriding the example header styles.
    
    Split the assets into two so we have:
    
    1. A core set of assets that only load the design system, used on
       isolated component sample pages (and via iframe)
    2. A second set of theme assets which is what we actually load on the
       docs pages

--- design-system-docs/frontend/javascript/theme.js
+++ design-system-docs/frontend/javascript/theme.js
@@ -1,18 +1,23 @@
-// This file loads just the design system components for use in isolated iframes
-// We use this when we need a clean base with no theme overrides
+// This file loads the docs theme
+// We use this for the main docs pages where we want to customise the design system theme
+// For isolated component pages we use the based index.scss/js
 
-import '../styles/index.scss';
+import '../styles/theme.scss';
 
 import initHeader from '../../../lib/header';
 import greedyNav from '../../../lib/greedy-nav/index';
 import initTargetedContent from '../../../lib/targeted-content';
 import initDisclosure from '../../../lib/disclosure/disclosure';
+import initCodeCopy from './code-copy';
+import initExamples from './examples';
 
 try {
   initHeader();
   greedyNav.init();
   initTargetedContent();
   initDisclosure();
+  initCodeCopy();
+  initExamples();
 } catch (error) {
   document.querySelector('html').classList.add('no-js');
   throw error;
