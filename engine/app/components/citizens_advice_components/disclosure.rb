7e6894703f84e5f3753fff07da97eb1c88fff5d5
David Rapson <david.rapson@citizensadvice.org.uk>
2022-09-05 17:10:47 +0100

    Convert disclosure to ERB
    Converts disclosure component to use an ERB template. Tidies up the
    component class in the process, mostly to flatten the button attributes.

--- engine/app/components/citizens_advice_components/disclosure.rb
+++ engine/app/components/citizens_advice_components/disclosure.rb
@@ -2,7 +2,7 @@
 
 module CitizensAdviceComponents
   class Disclosure < Base
-    attr_reader :closed_summary
+    attr_reader :closed_summary, :open_summary
 
     def initialize(closed_summary:, open_summary: nil, additional_attributes: nil, id: nil)
       super
@@ -13,38 +13,11 @@ module CitizensAdviceComponents
     end
 
     def render?
-      @closed_summary.present? && content.present?
+      closed_summary.present? && content.present?
     end
 
-    def disclosure_details_id
-      @id.presence || "#{@closed_summary.parameterize}-disclosure-details"
-    end
-
-    def base_button_attrs
-      {
-        type: "button",
-        class: %w[cads-disclosure__toggle cads-icon_plus cads-linkbutton js-disclosure-toggle],
-        aria: { expanded: "false", controls: disclosure_details_id },
-        data: button_data_attrs
-      }
-    end
-
-    def button_data_attrs
-      {
-        toggle_target_id: disclosure_details_id,
-        label_when_hiding: generate_label("Show this section", @closed_summary),
-        label_when_showing: generate_label("Hide this section", @open_summary),
-        open_summary: @open_summary,
-        closed_summary: @closed_summary
-      }
-    end
-
-    def generate_label(prepend_text, summary)
-      if @open_summary.eql? @closed_summary
-        "#{prepend_text}, #{summary}"
-      else
-        summary
-      end
+    def id
+      @id.presence || "#{closed_summary.parameterize}-disclosure-details"
     end
 
     def button_attrs
@@ -52,6 +25,28 @@ module CitizensAdviceComponents
         base_button_attrs.merge @additional_attributes
       else
         base_button_attrs
+      end
+    end
+
+    def base_button_attrs
+      {
+        type: "button",
+        class: %w[cads-disclosure__toggle cads-icon_plus cads-linkbutton js-disclosure-toggle],
+        "aria-expanded": "false",
+        "aria-controls": id,
+        "data-toggle-target-id": id,
+        "data-label-when-hiding": generate_label("Show this section", closed_summary),
+        "data-label-when-showing": generate_label("Hide this section", open_summary),
+        "data-open-summary": open_summary,
+        "data-closed-summary": closed_summary
+      }
+    end
+
+    def generate_label(prepend_text, summary)
+      if open_summary.eql? closed_summary
+        "#{prepend_text}, #{summary}"
+      else
+        summary
       end
     end
   end
