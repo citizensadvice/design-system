c905e768c0d913f19da2a6209f1e22a4e8db17bf
David Rapson <david.rapson@citizensadvice.org.uk>
2022-10-06 10:57:48 +0100

    Fix ruby 3 syntax issues in docs and demo app
    See https://www.ruby-lang.org/en/news/2019/12/12/separation-of-positional-and-keyword-arguments-in-ruby-3-0/
    
    Ruby 3 separates how positional and keyword arguments are handled.
    Passing in arguments to the single checkbox as a hash relied on a quirk
    of ruby 2 where hashes were converted to positional arguments.
    
    Now we either need to pass them explicitly or use the ** double splat
    operator. It's simplest to use the named parameters directly here.

--- demo/spec/components/previews/checkbox_single_preview.rb
+++ demo/spec/components/previews/checkbox_single_preview.rb
@@ -3,19 +3,15 @@
 class CheckboxSinglePreview < ViewComponent::Preview
   def basic
     render CitizensAdviceComponents::CheckboxSingle.new(name: "a-single-checkbox") do |c|
-      c.checkbox(checkbox)
+      c.checkbox(label: "I agree to the great terms and conditions", value: "agreed-to-tcs")
     end
   end
 
   def error
     render CitizensAdviceComponents::CheckboxSingle.new(name: "a-single-checkbox", error_message: "Agree to the terms and conditions") do |c|
-      c.checkbox(checkbox)
+      c.checkbox(label: "I agree to the great terms and conditions", value: "agreed-to-tcs")
     end
   end
 
   private
-
-  def checkbox
-    { label: "I agree to the great terms and conditions", value: "agreed-to-tcs" }
-  end
 end
