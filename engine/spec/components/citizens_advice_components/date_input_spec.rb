e811fe7af410b518a22fc00ab9cf7c851394a061
marianayovcheva <mariana.yovcheva@citizensadvice.org.uk>
2023-09-28 13:10:00 +0100

    feat: date field tests to be in line with our general test styles, using Capybara selectors

--- engine/spec/components/citizens_advice_components/date_input_spec.rb
+++ engine/spec/components/citizens_advice_components/date_input_spec.rb
@@ -1,95 +1,70 @@
 # frozen_string_literal: true
 
 RSpec.describe CitizensAdviceComponents::DateInput, type: :component do
-  subject(:component) do
-    render_inline described_class.new(
-      name: name.presence,
-      label: label.presence,
-      errors: errors.presence,
-      values: values.presence
-    )
-  end
+  subject { page }
 
-  let(:name) { "example-date-input" }
-  let(:label) { "Example date input" }
-  let(:errors) { nil }
-  let(:values) { nil }
-
-  it "renders a day label" do
-    expect(subject.text.strip).to include "Day"
-  end
-
-  it "renders a month label" do
-    expect(subject.text.strip).to include "Month"
-  end
-
-  it "renders a year label" do
-    expect(subject.text.strip).to include "Year"
-  end
-
-  it "renders a day input" do
-    expect(subject.css("[data-testid=day-input]")).to be_present
-  end
-
-  it "renders a month input" do
-    expect(subject.css("[data-testid=month-input]")).to be_present
-  end
-
-  it "renders a year input" do
-    expect(subject.css("[data-testid=year-input]")).to be_present
-  end
-
-  context "in Cymraeg" do
-    around { |example| I18n.with_locale(:cy) { example.run } }
-
-    it "renders a day label" do
-      expect(subject.text.strip).to include "Dydd"
+  context "with default arguments" do
+    before do
+      render_inline described_class.new(
+        name: "example-date-input",
+        label: "Example date input",
+        errors: nil,
+        values: nil
+      )
     end
 
-    it "renders a month label" do
-      expect(subject.text.strip).to include "Mis"
-    end
+    it { is_expected.to have_field "Day" }
+    it { is_expected.to have_field "Month" }
+    it { is_expected.to have_field "Year" }
 
-    it "renders a year label" do
-      expect(subject.text.strip).to include "Blwyddyn"
+    context "when in Cymraeg" do
+      around { |example| I18n.with_locale(:cy) { example.run } }
+
+      it { is_expected.to have_field "Dydd" }
+      it { is_expected.to have_field "Mis" }
+      it { is_expected.to have_field "Blwyddyn" }
     end
   end
 
   context "with errors" do
-    let(:errors) { %i[day month year] }
+    before do
+      render_inline described_class.new(
+        name: "example-date-input",
+        label: "Example date input",
+        errors: %i[day month year],
+        values: nil
+      )
+    end
 
     it "renders the day input as error" do
-      expect(subject.css("[data-testid=day-input]").attribute("aria-invalid")).to be_present
+      expect(page).to have_selector "[data-testid=day-input][aria-invalid=true]"
     end
 
     it "renders the month input as error" do
-      expect(subject.css("[data-testid=month-input]").attribute("aria-invalid")).to be_present
+      expect(page).to have_selector "[data-testid=month-input][aria-invalid=true]"
     end
 
     it "renders the year input as error" do
-      expect(subject.css("[data-testid=year-input]").attribute("aria-invalid")).to be_present
+      expect(page).to have_selector "[data-testid=year-input][aria-invalid=true]"
     end
   end
 
   context "with values" do
-    let(:values) do
-      {
-        day: 1,
-        month: 2,
-        year: 1990
-      }
+    before do
+      render_inline described_class.new(
+        name: "example-date-input",
+        label: "Example date input",
+        errors: %i[day month year],
+        values: {
+          day: 1,
+          month: 2,
+          year: 1990
+        }
+      )
     end
 
-    it "renders the day input with the correct value" do
-      expect(subject.css("[data-testid=day-input]").attribute("value").value).to eq "1"
-    end
-
-    it "renders the month input with the correct value" do
-      expect(subject.css("[data-testid=month-input]").attribute("value").value).to eq "2"
-    end
-
-    it "renders the year input with the correct value" do
-      expect(subject.css("[data-testid=year-input]").attribute("value").value).to eq "1990"
-    end
+    it { is_expected.to have_field "Day", with: "1" }
+    it { is_expected.to have_field "Month", with: "2" }
+    it { is_expected.to have_field "Year", with: "1990" }
   end
 end
