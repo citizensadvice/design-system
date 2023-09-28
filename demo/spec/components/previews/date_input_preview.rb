1a1b0a9c88aeef2abcd7469e3be2d89f7c1cfa60
marianayovcheva <mariana.yovcheva@citizensadvice.org.uk>
2023-09-28 13:33:35 +0100

    feat: tidying up the date input preview

--- demo/spec/components/previews/date_input_preview.rb
+++ demo/spec/components/previews/date_input_preview.rb
@@ -2,9 +2,7 @@
 
 class DateInputPreview < ViewComponent::Preview
   def example
-    subject = CitizensAdviceComponents::DateInput.new(name: "dob", label: "Date of birth")
-
-    render subject
+    render CitizensAdviceComponents::DateInput.new(name: "dob", label: "Date of birth")
   end
 
   def hint
@@ -20,76 +18,76 @@ class DateInputPreview < ViewComponent::Preview
   end
 
   def optional
-    subject = CitizensAdviceComponents::DateInput.new(
-      name: "bday",
-      label: "Date of birth",
-      options: {
-        optional: true
-      }
+    render(
+      CitizensAdviceComponents::DateInput.new(
+        name: "bday",
+        label: "Date of birth",
+        options: {
+          optional: true
+        }
+      )
     )
-
-    render subject
   end
 
   def error
-    subject = CitizensAdviceComponents::DateInput.new(
-      name: "bday",
-      label: "Date of birth",
-      errors: %i[day month year],
-      options: {
-        error_message: "Enter your date of birth, like 01 02 1990"
-      }
+    render(
+      CitizensAdviceComponents::DateInput.new(
+        name: "bday",
+        label: "Date of birth",
+        errors: %i[day month year],
+        options: {
+          error_message: "Enter your date of birth, like 01 02 1990"
+        }
+      )
     )
-
-    render subject
   end
 
   def incomplete_error
-    subject = CitizensAdviceComponents::DateInput.new(
-      name: "bday",
-      label: "Date of birth",
-      errors: %i[year],
-      values: {
-        day: 1,
-        month: 1
-      },
-      options: {
-        error_message: "Date of birth must include a year"
-      }
+    render(
+      CitizensAdviceComponents::DateInput.new(
+        name: "bday",
+        label: "Date of birth",
+        errors: %i[year],
+        values: {
+          day: 1,
+          month: 1
+        },
+        options: {
+          error_message: "Date of birth must include a year"
+        }
+      )
     )
-
-    render subject
   end
 
   def validation_error
-    subject = CitizensAdviceComponents::DateInput.new(
-      name: "bday",
-      label: "Date of birth",
-      errors: %i[day],
-      values: {
-        day: 99,
-        month: 1,
-        year: 1990
-      },
-      options: {
-        error_message: "Date of birth must be a real date"
-      }
+    render(
+      CitizensAdviceComponents::DateInput.new(
+        name: "bday",
+        label: "Date of birth",
+        errors: %i[day],
+        values: {
+          day: 99,
+          month: 1,
+          year: 1990
+        },
+        options: {
+          error_message: "Date of birth must be a real date"
+        }
+      )
     )
-
-    render subject
   end
 
   def values
-    subject = CitizensAdviceComponents::DateInput.new(
-      name: "bday",
-      label: "Date of birth",
-      values: {
-        day: 1,
-        month: 1,
-        year: 1990
-      }
+    render(
+      CitizensAdviceComponents::DateInput.new(
+        name: "bday",
+        label: "Date of birth",
+        values: {
+          day: 1,
+          month: 1,
+          year: 1990
+        }
+      )
     )
-
-    render subject
   end
 end
