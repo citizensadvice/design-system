583b68ee4c516153c2ce7f2654add46006bad3aa
David Rapson <david.rapson@citizensadvice.org.uk>
2024-03-25 15:42:45 +0000

    Add option to disable icon font styles
    Adds a sass config option to disable icon font styles. Intended to be
    used to help us flush out any remaining uses of icon fonts both
    internally and within applications.

---
+++ scss/3-generics/__fonts__/fonts.tests.js
@@ -0,0 +1,21 @@
+/* eslint-env jest */
+const sass = require('sass');
+
+test('icon font styles are included by default', () => {
+  const output = sass
+    .renderSync({
+      data: `@import 'scss/lib';`,
+    })
+    .css.toString();
+  expect(output).toContain('font-family: cads');
+});
+
+test('icon font styles are included by default', () => {
+  const output = sass
+    .renderSync({
+      data: `$cads-enable-icon-font: false;
+        @import 'scss/lib';`,
+    })
+    .css.toString();
+  expect(output).not.toContain('font-family: cads');
+});
