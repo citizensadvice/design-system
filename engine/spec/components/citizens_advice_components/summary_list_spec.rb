99c07cbe05a16c3593b505bf32ce4ee217973ae1
David Rapson <david.rapson@citizensadvice.org.uk>
2025-01-22 09:31:00 +0000

    Summary list can remove rows with blank values
    Adds a new `hide_empty_rows` option to remove rows with blank values

--- engine/spec/components/citizens_advice_components/summary_list_spec.rb
+++ engine/spec/components/citizens_advice_components/summary_list_spec.rb
@@ -1,23 +1,43 @@
 # frozen_string_literal: true
 
 RSpec.describe CitizensAdviceComponents::SummaryList, type: :component do
-  subject(:component) { described_class.new(items: items) }
+  subject { page }
 
   context "with items" do
-    let(:items) { [{ name: "Salary", value: "£26,000" }] }
-
-    it "renders the dl" do
-      render_inline component
-      expect(page).to have_css "dl.cads-summary-list"
+    before do
+      render_inline described_class.new(items: [
+        { name: "Example name A", value: "Example value A" },
+        { name: "Example name B", value: nil }
+      ])
     end
+
+    it { is_expected.to have_css "dl.cads-summary-list" }
+    it { is_expected.to have_css "dt", count: 2 }
+    it { is_expected.to have_css "dt", text: "Example name A" }
+    it { is_expected.to have_css "dd", text: "Example value A" }
+    it { is_expected.to have_css "dt", text: "Example name B" }
+  end
+
+  context "with hide_empty_rows" do
+    before do
+      render_inline described_class.new(items: [
+        { name: "Example name A", value: "Example value A" },
+        { name: "Example name B", value: nil }
+      ], hide_empty_rows: true)
+    end
+
+    it { is_expected.to have_css "dl.cads-summary-list" }
+    it { is_expected.to have_css "dt", count: 1 }
+    it { is_expected.to have_css "dt", text: "Example name A" }
+    it { is_expected.to have_css "dd", text: "Example value A" }
+    it { is_expected.to have_no_css "dt", text: "Example name B" }
   end
 
   context "without items" do
-    let(:items) { [] }
-
-    it "does not render the dl" do
-      render_inline component
-      expect(page).to have_no_css "dl.cads-summary-list"
+    before do
+      render_inline described_class.new(items: [])
     end
+
+    it { is_expected.to have_no_css "dl.cads-summary-list" }
   end
 end
