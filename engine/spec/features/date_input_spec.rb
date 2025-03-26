45724de758d807c0b3635c3f54e2cabc88c028d7
johnny-m-young <94846816+johnny-m-young@users.noreply.github.com>
2025-03-26 10:11:03 +0000

    Pass options as params
    This allows us to define options within contexts, so we can render and test each
    version of the field in isolation.

--- engine/spec/features/date_input_spec.rb
+++ engine/spec/features/date_input_spec.rb
@@ -2,84 +2,54 @@
 
 RSpec.describe "date inputs" do
   before do
-    visit element_path("date_inputs")
+    visit element_path("date_inputs", **options)
   end
 
-  it "renders the 'day' input field" do
-    within "#default_date_input" do
+  context "default date input" do
+    let(:options) { {} }
+
+    it "renders the 'day' input field" do
       expect(page).to have_css("label[for=person_date_of_birth-input]", text: "Day")
       expect(page).to have_field("person[date_of_birth(3i)]")
     end
-  end
 
-  it "renders the 'month' input field" do
-    within "#default_date_input" do
+    it "renders the 'month' input field" do
       expect(page).to have_css("label[for=person_date_of_birth_2i]", text: "Month")
       expect(page).to have_field("person[date_of_birth(2i)]")
     end
-  end
 
-  it "renders the 'year' input field" do
-    within "#default_date_input" do
+    it "renders the 'year' input field" do
       expect(page).to have_css("label[for=person_date_of_birth_1i]", text: "Year")
       expect(page).to have_field("person[date_of_birth(1i)]")
     end
-  end
 
-  it "indicates the fields are optional" do
-    within "#default_date_input" do
+    it "indicates the fields are optional" do
       expect(page).to have_css(".cads-form-field__optional", text: "(optional)")
     end
-  end
 
-  it "doesn't include a hint" do
-    within "#default_date_input" do
+    it "doesn't include a hint" do
       expect(page).to have_no_css("p.cads-form-field__hint")
     end
-  end
 
-  it "renders a hint" do
-    within "#date_input_with_hint" do
-      expect(page).to have_css("p.cads-form-field__hint", text: "Hint text")
-    end
-  end
-
-  it "stores and retrieves the day, month and year values in a Rails-compatible way" do
-    within "#default_date_input" do
+    it "stores and retrieves the day, month and year values in a Rails-compatible way" do
       fill_in "person[date_of_birth(3i)]", with: 13
       fill_in "person[date_of_birth(2i)]", with: 12
       fill_in "person[date_of_birth(1i)]", with: 2005
+
+      click_button
+
+      expect(page).to have_field("person[date_of_birth(3i)]", with: "13")
+      expect(page).to have_field("person[date_of_birth(2i)]", with: "12")
+      expect(page).to have_field("person[date_of_birth(1i)]", with: "2005")
     end
 
-    click_button
-
-    expect(page).to have_field("person[date_of_birth(3i)]", with: "13")
-    expect(page).to have_field("person[date_of_birth(2i)]", with: "12")
-    expect(page).to have_field("person[date_of_birth(1i)]", with: "2005")
-  end
-
-  it "doesn't add the 'optional' label text" do
-    within "#required_date_input" do
-      expect(page).to have_no_css(".cads-form-field__optional")
-    end
-  end
-
-  it "renders a page heading" do
-    within "#page_heading_date_input" do
-      expect(page).to have_css("h1.cads-page-title", text: "Purchase date")
-    end
-  end
-
-  it "renders the error message" do
-    within "#default_date_input" do
+    it "renders the error message" do
       fill_in "person[date_of_birth(3i)]", with: 25
       fill_in "person[date_of_birth(2i)]", with: 12
       fill_in "person[date_of_birth(1i)]", with: 3000
-    end
 
-    click_button
+      click_button
 
-    within "#default_date_input" do
       expect(page).to have_css("div.cads-form-field.cads-form-field--has-error")
       expect(page).to have_css("div.cads-form-field__error-marker")
       expect(page).to have_css("p.cads-form-field__error-message")
@@ -87,6 +57,30 @@ RSpec.describe "date inputs" do
       expect(page).to have_css("#person_date_of_birth-input.cads-input--error")
       expect(page).to have_css("#person_date_of_birth_2i.cads-input--error")
       expect(page).to have_css("#person_date_of_birth_1i.cads-input--error")
+    end
+  end
+
+  context "date input with hint" do
+    let(:options) { { hint: "Hint text"} }
+
+    it "renders a hint" do
+      expect(page).to have_css("p.cads-form-field__hint", text: "Hint text")
+    end
+  end
+
+  context "required date input" do
+    let(:options) { { required: true } }
+
+    it "doesn't add the 'optional' label text" do
+      expect(page).to have_no_css(".cads-form-field__optional")
+    end
+  end
+
+  context "page heading date input" do
+    let(:options) { { page_heading: true, required: true } }
+
+    it "renders a page heading" do
+      expect(page).to have_css("h1.cads-page-title", text: "Date of birth")
     end
   end
 end
