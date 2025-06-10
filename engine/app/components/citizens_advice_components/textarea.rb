47588073c7c07da13de5ad27ed86eebe71c53fbf
David Rapson <david.rapson@citizensadvice.org.uk>
2025-06-10 09:39:28 +0100

    Add scaffolding for character counter
    Background
    ==========
    
    We currently have a basic character counter in use on the referrals
    application. It's general enough and we have an open issue (#1691) that
    suggests it's worth making this available through the design system
    itself.
    
    Changes
    =======
    
    Adds the scaffolding for a character count component supported by
    textareas. Takes care to follow the same accessibility patterns as the
    GOV.UK equivalent which has recently undergone some major accessibility
    improvements[^1].
    
    This version also differs from the current version in our products in that it
    doesn't use `maxlength` as the limit. This is deliberate to avoid
    data-loss when pasting text[^2].
    
    As a result this version has the following additional states:
    
    1. A no-js fallback state
    2. A error state when exceeding the character limit
    3. A screen-reader only version of the character count which updates at
       a slower rate.
    
    Caveats
    =======
    
    The current implementation doesn't have any tests (neither rspec updates
    or Cypress tests) nor documentation.
    
    Using this as a starting point to validate against the current implementation.
    
    [^1]: https://dav-idc.com/making-a-character-count-component-more-accessible/
    [^2]: https://adamsilver.io/blog/dont-use-the-maxlength-attribute-to-stop-users-from-exceeding-the-limit/

--- engine/app/components/citizens_advice_components/textarea.rb
+++ engine/app/components/citizens_advice_components/textarea.rb
@@ -2,20 +2,30 @@
 
 module CitizensAdviceComponents
   class Textarea < Input
-    attr_reader :base_input_args
+    attr_reader :base_input_args, :character_count
 
     DEFAULT_ROWS = 8
 
     def initialize(rows: DEFAULT_ROWS, **args)
       @rows = format_rows(rows)
       @base_input_args = args.merge(type: nil)
+      @character_count = args.dig(:options, :character_count)
       super(**@base_input_args)
     end
 
-    def call
-      render CitizensAdviceComponents::Input.new(**base_input_args) do
-        content_tag(:textarea, value, class: "cads-textarea", **input_attributes)
-      end
+    private
+
+    def character_count?
+      character_count.present?
+    end
+
+    def described_by_ids
+      super << character_count_id if character_count?
+      super
+    end
+
+    def character_count_id
+      "#{general_id}-info"
     end
 
     def format_rows(rows)
@@ -23,7 +33,7 @@ module CitizensAdviceComponents
     end
 
     def base_input_attributes
-      super.merge(rows: @rows, value: nil)
+      super.merge(rows: @rows, value: nil, "data-character-count": character_count.presence)
     end
   end
 end
