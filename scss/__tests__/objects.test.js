566f49d2ba841606c7da43efea615e9a7843c8e5
David Rapson <david.rapson@citizensadvice.org.uk>
2025-06-17 10:22:31 +0100

    Migrate from jest to vitest
    In lieu of upgrading to Jest 30. Includes an ADR for the proposed
    change.

--- scss/__tests__/objects.test.js
+++ scss/__tests__/objects.test.js
@@ -2,5 +2,5 @@ const sass = require('sass');
 
 test('grid styles match snapshot', () => {
   const output = sass.compile(`scss/5-objects/_grid.scss`);
-  expect(output.css.toString()).toMatchSnapshot();
+  expect(output.css.toString({ logger: sass.Logger.silent })).toMatchSnapshot();
 });
--- scss/__tests__/settings.test.js
+++ scss/__tests__/settings.test.js
@@ -2,13 +2,17 @@ const { globSync } = require('glob');
 const sass = require('sass');
 
 test('importing settings does not output css', () => {
-  const output = sass.compile('scss/1-settings/settings-imports.scss');
+  const output = sass.compile('scss/1-settings/settings-imports.scss', {
+    logger: sass.Logger.silent,
+  });
   expect(output.css).toEqual('');
 });
 
 test.each(globSync('scss/1-settings/_*.scss'))(
   '%p can be imported standalone',
   (file) => {
-    expect(() => sass.compile(file)).not.toThrow();
+    expect(() =>
+      sass.compile(file, { logger: sass.Logger.silent }),
+    ).not.toThrow();
   },
 );
