25e33a70d3182b66210598a1a6269968fcdaf3de
David Rapson <david.rapson@citizensadvice.org.uk>
2025-08-06 09:29:27 +0100

    Rails-based static site generation for docs website
    Builds on a recent prototype to use
    https://github.com/benpickles/parklife along with
    https://github.com/benpickles/decant to build a static site using Rails.
    
    Attempts to bypass the trade-offs we get integrating Rails-components in
    a non-Rails environment with the current Bridgetown docs.
    
    The prototype was successful enough (everything ported over) that I
    thought it worth testing a full integration with the design system repo.
    
    Parklife does the static site generation (works with any Rack app) and
    Decant handles directories of static content and front matter.
    
    Configures things to use a similar setup to the bridgetown site for ease
    of migration.

---
+++ website/app/assets/javascript/theme.js
@@ -0,0 +1,26 @@
+import {
+  initHeader,
+  initNavigation,
+  initTargetedContent,
+  initDisclosure,
+  initOnThisPage,
+} from "@citizensadvice/design-system";
+
+import initCodeCopy from "./components/code-copy";
+import initSampleCode from "./components/sample-code";
+import initComponentExamples from "./components/component-examples";
+
+try {
+  initHeader();
+  initNavigation();
+  initTargetedContent();
+  initDisclosure();
+  initOnThisPage();
+  initCodeCopy();
+  initSampleCode();
+  initComponentExamples();
+} catch (error) {
+  document.querySelector("html").classList.add("no-js");
+  throw error;
+}
+// Entry point for the build script in your package.json
---
+++ website/app/components/spacing_table_component.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class SpacingTableComponent < ViewComponent::Base
+  def call
+    render(
+      CitizensAdviceComponents::Table.new(
+        header: %w[Variables Example Size],
+        rows: %w[4px 8px 12px 16px 24px 32px 40px].each_with_index.map do |size, index|
+          [
+            "$cads-spacing-#{index + 1}",
+            tag.div(class: "spacing-block-#{index + 1}"),
+            size
+          ]
+        end
+      )
+    )
+  end
+end
