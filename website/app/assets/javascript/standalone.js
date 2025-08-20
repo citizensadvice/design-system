0417a033cf960d72a245e7022e49eae4193f9642
David Rapson <david.rapson@citizensadvice.org.uk>
2025-08-20 16:35:46 +0100

    Use open-iframe-resizer (#3787)
    The previous library we were using switched to dual commercial/GPLv3
    license with version 5.x rather than the original MIT license.
    
    Switches to https://lemick.github.io/open-iframe-resizer/ which is a MIT
    licensed alternative.

--- website/app/assets/javascript/standalone.js
+++ website/app/assets/javascript/standalone.js
@@ -1,10 +1,6 @@
 // This file loads just the design system components for use in isolated iframes
 // We use this when we need a clean base with no theme overrides
 
-// iframeResizer.contentWindow must be loaded by the content iframe
-// see components/iframes for the companion host code
-import "iframe-resizer/js/iframeResizer.contentWindow";
-
 import {
   initHeader,
   initNavigation,
