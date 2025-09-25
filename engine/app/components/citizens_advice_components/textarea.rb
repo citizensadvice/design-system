484f1219c7b916f4c2ac8338d135eac11abfc3f0
David Rapson <david.rapson@citizensadvice.org.uk>
2025-09-25 09:42:03 +0100

    Revert "Simplify textarea interface"
    This reverts commit 48144085c962852611ac3fa6851ecc0a2456f4f2.

--- engine/app/components/citizens_advice_components/textarea.rb
+++ engine/app/components/citizens_advice_components/textarea.rb
@@ -1,105 +1,29 @@
 # frozen_string_literal: true
 
 module CitizensAdviceComponents
-  class Textarea < Base
+  class Textarea < Input
+    attr_reader :base_input_args
+
     DEFAULT_ROWS = 8
 
-    attr_reader :name, :label, :error_message, :hint, :value, :id
-
-    def initialize(name:, label:, rows: DEFAULT_ROWS, id: nil, options: nil)
-      @name = name
-      @id = id
-      @label = label
-      @rows = rows.to_i.zero? ? DEFAULT_ROWS : rows
-
-      set_options(options)
+    def initialize(rows: DEFAULT_ROWS, **args)
+      @rows = format_rows(rows)
+      @base_input_args = args.merge(type: nil)
+      super(**@base_input_args)
     end
 
-    private
-
-    def set_options(options)
-      return if options.blank?
-
-      @error_message = options[:error_message]
-      @hint = options[:hint]
-      @optional = fetch_or_fallback_boolean(options[:optional], fallback: false)
-      @page_heading = fetch_or_fallback_boolean(options[:page_heading], fallback: false)
-
-      @value = options[:value]
-      @additional_attributes = options[:additional_attributes]
+    def call
+      render CitizensAdviceComponents::Input.new(**base_input_args) do
+        content_tag(:textarea, value, class: "cads-textarea", **input_attributes)
+      end
     end
 
     def format_rows(rows)
       rows.to_i.zero? ? DEFAULT_ROWS : rows
     end
 
-    def page_heading?
-      @page_heading
-    end
-
-    def optional?
-      @optional
-    end
-
-    def required?
-      !optional?
-    end
-
-    def error?
-      @error_message.present?
-    end
-
-    def hint?
-      @hint.present?
-    end
-
-    def general_id
-      return id if @id.present?
-
-      name
-    end
-
-    def label_id
-      "#{general_id}-label"
-    end
-
-    def input_id
-      "#{general_id}-input"
-    end
-
-    def error_id
-      "#{general_id}-error"
-    end
-
-    def hint_id
-      "#{general_id}-hint"
-    end
-
-    def described_by
-      ids = []
-      ids << error_id if error?
-      ids << hint_id if hint?
-      ids.present? ? ids.join(" ") : nil
-    end
-
     def base_input_attributes
-      {
-        type: (@type.to_s.dasherize if @type.present?),
-        id: input_id,
-        name: name,
-        rows: @rows,
-        "aria-required": required?,
-        "aria-invalid": error?,
-        "aria-describedby": described_by
-      }
-    end
-
-    def input_attributes
-      if @additional_attributes.present?
-        base_input_attributes.merge @additional_attributes
-      else
-        base_input_attributes
-      end
+      super.merge(rows: @rows, value: nil)
     end
   end
 end
