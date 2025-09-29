29dccb88c406abcde0676850f5326259a4358cac
David Rapson <david.rapson@citizensadvice.org.uk>
2025-09-29 12:02:39 +0100

    Await initialize for iframe resizer

--- website/app/assets/javascript/components/component-examples.js
+++ website/app/assets/javascript/components/component-examples.js
@@ -3,7 +3,7 @@ import { initialize } from "@open-iframe-resizer/core";
 export default function initComponentExamples() {
   // Resize component iFrames using https://www.npmjs.com/package/@open-iframe-resizer/core
   const els = document.querySelectorAll(".js-component-example-iframe");
-  Array.from(els).forEach((iframe) => {
-    initialize({}, iframe);
+  Array.from(els).forEach(async (iframe) => {
+    await initialize({}, iframe);
   });
 }
