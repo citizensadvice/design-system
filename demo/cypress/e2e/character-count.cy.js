f997cdf6527804d9e22b52c77dc2762bd1b289f7
David Rapson <david.rapson@citizensadvice.org.uk>
2025-11-19 09:21:43 +0000

    Experimental: Add character count to textareas
    Background
    ==========
    
    We currently have a basic character counter in use on the referrals
    application. It's general enough and we have an open issue (#1691) that
    suggests it's worth making this available through the design system
    itself.
    
    Changes
    =======
    
    Adds the scaffolding for a character count component supported by
    textareas. Takes care to follow the same accessibility patterns as the
    GOV.UK equivalent which has recently undergone some major accessibility
    improvements[^1].
    
    This version also differs from the current version in our products in that it
    doesn't use `maxlength` as the limit. This is deliberate to avoid
    data-loss when pasting text[^2].
    
    As a result this version has the following additional states:
    
    1. A no-js fallback state
    2. A error state when exceeding the character limit
    3. A screen-reader only version of the character count which updates at
       a slower rate.
    
    [^1]: https://dav-idc.com/making-a-character-count-component-more-accessible/
    [^2]: https://adamsilver.io/blog/dont-use-the-maxlength-attribute-to-stop-users-from-exceeding-the-limit/

---
+++ demo/cypress/e2e/character-count.cy.js
@@ -0,0 +1,88 @@
+describe("Character count", () => {
+  describe("english language", () => {
+    it("has a character counter", () => {
+      cy.visitComponentUrl("textarea/with_character_count");
+      cy.findByTestId("character-count").should(
+        "have.text",
+        "You have 1000 characters remaining",
+      );
+    });
+
+    it("counts characters as the user types", () => {
+      cy.visitComponentUrl("textarea/with_character_count");
+      cy.findByLabelText("Example input").type("Some initial text.");
+
+      cy.findByTestId("character-count").should(
+        "have.text",
+        "You have 982 characters remaining",
+      );
+    });
+
+    it("displays the amount over if exceeding the limit", () => {
+      cy.visitComponentUrl("textarea/with_character_count");
+
+      cy.findByLabelText("Example input").type(tooMuchText().trim(), {
+        delay: 0,
+      });
+
+      cy.findByTestId("character-count").should(
+        "have.text",
+        "You have 412 characters too many",
+      );
+    });
+  });
+
+  describe("welsh language", () => {
+    it("has a character counter", () => {
+      cy.visitComponentUrl("textarea/with_character_count?locale=cy");
+      cy.findByTestId("character-count").should(
+        "have.text",
+        "You have 1000 characters remaining (cy)",
+      );
+    });
+
+    it("counts characters as the user types", () => {
+      cy.visitComponentUrl("textarea/with_character_count?locale=cy");
+      cy.findByLabelText("Example input").type("Some initial text.");
+
+      cy.findByTestId("character-count").should(
+        "have.text",
+        "You have 982 characters remaining (cy)",
+      );
+    });
+
+    it("displays the amount over if exceeding the limit", () => {
+      cy.visitComponentUrl("textarea/with_character_count?locale=cy");
+
+      cy.findByLabelText("Example input").type(tooMuchText().trim(), {
+        delay: 0,
+      });
+
+      cy.findByTestId("character-count").should(
+        "have.text",
+        "You have 412 characters too many (cy)",
+      );
+    });
+  });
+
+  function tooMuchText() {
+    return `Alice was beginning to get very tired of sitting by her sister on the bank,
+      and of having nothing to do: once or twice she had peeped into the book her sister was reading,
+      but it had no pictures or conversations in it, “and what is the use of a book,” thought Alice
+      “without pictures or conversations?”
+
+      So she was considering in her own mind (as well as she could, for the hot day made her feel very
+      sleepy and stupid), whether the pleasure of making a daisy-chain would be worth the trouble of
+      getting up and picking the daisies, when suddenly a White Rabbit with pink eyes ran close by her.
+
+      There was nothing so very remarkable in that; nor did Alice think it so very much out of the way
+      to hear the Rabbit say to itself, “Oh dear! Oh dear! I shall be late!” (when she thought it over
+      afterwards, it occurred to her that she ought to have wondered at this, but at the time it all
+      seemed quite natural); but when the Rabbit actually took a watch out of its waistcoat-pocket,
+      and looked at it, and then hurried on, Alice started to her feet, for it flashed across her mind
+      that she had never before seen a rabbit with either a waistcoat-pocket, or a watch to take out of it,
+      and burning with curiosity, she ran across the field after it, and fortunately was just in time to see
+      it pop down a large rabbit-hole under the hedge.
+    `;
+  }
+});
