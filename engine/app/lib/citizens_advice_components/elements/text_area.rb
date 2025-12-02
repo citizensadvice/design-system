a908df888961e65e1201318552f99f059705d5c7
David Rapson <david.rapson@citizensadvice.org.uk>
2025-12-02 10:31:48 +0000

    Decouple cads_textarea from view component interface
    Now that #3849 is in we can extract another step from #3837, this time
    to decouple the rendering of cads_textarea from the view component
    interface. Addresses a couple of pending tests in the process.

--- engine/app/lib/citizens_advice_components/elements/text_area.rb
+++ engine/app/lib/citizens_advice_components/elements/text_area.rb
@@ -2,24 +2,52 @@
 
 module CitizensAdviceComponents
   module Elements
-    class TextArea < Field
-      def render
-        component = CitizensAdviceComponents::Textarea.new(
-          name: field_name,
-          id: field_id,
-          label: label,
-          rows: options[:rows],
-          options: {
-            hint: hint,
-            optional: optional,
-            value: current_value,
-            error_message: error_message,
-            additional_attributes: options[:additional_attributes],
-            page_heading: options[:page_heading].present?
-          }
-        )
+    class TextArea < Base
+      include Traits::Field
 
-        component.render_in(@template)
+      def initialize(builder, template, object, attribute, options)
+        super(builder, template, object)
+
+        @attribute = attribute
+        @options = options
+        additional_attributes_deprecation
+      end
+
+      private
+
+      def render_field
+        builder.text_area(
+          attribute,
+          class: "cads-textarea",
+          id: input_id,
+          rows: rows,
+          required: false, # use aria-required over required attribute
+          "aria-required": required?,
+          "aria-invalid": error?,
+          "aria-describedby": described_by,
+          **text_area_options
+        )
+      end
+
+      def text_area_options
+        # The default behaviour of text_field is to pass options on to the HTML element
+        # Extract any custom options that we don't want passing on and add expected defaults.
+        # For backwards compatability merge in additional_options hash
+        options_for_html
+          .without(:rows)
+          .merge(options[:additional_attributes] || {})
+      end
+
+      def additional_attributes_deprecation
+        return if options[:additional_attributes].blank?
+
+        CitizensAdviceComponents.deprecator.warn(
+          "additional_attributes hash is deprecated, pass directly via options hash"
+        )
+      end
+
+      def rows
+        options[:rows].to_i.zero? ? 8 : options[:rows]
       end
     end
   end
