4666303ba2978fdd9d2f9fbda8abf57b3a030e2b
David Rapson <david.rapson@citizensadvice.org.uk>
2026-02-13 12:30:45 +0000

    Set i18n_customize_full_message by default (#3935)
    The Rails format is `"%{attribute} %{message}"` e.g. Name can't be blank.
    We prefer a more plain english format, e.g. Enter your full name. To allow
    customising this fully we require `i18n_customize_full_message` to be enabled.
    This then allows you to override the format in your locale files to something like this:
    
    ```
    en:
      activemodel:
        errors:
          models:
            person:
              format: "%{message}"
              attributes:
                first_name:
                  blank: Enter your first name
    ```
    
    See https://guides.rubyonrails.org/configuring.html#config-active-model-i18n-customize-full-message

--- engine/lib/citizens_advice_components/engine.rb
+++ engine/lib/citizens_advice_components/engine.rb
@@ -24,6 +24,25 @@ module CitizensAdviceComponents
       }
     end
 
+    # Our form builder uses the full_message method for displaying error messages.
+    # The Rails format is "%{attribute} %{message}" e.g. Name can't be blank
+    #
+    # We prefer a more plain english format, e.g. Enter your full name
+    #
+    # To allow customising this fully we require i18n_customize_full_message to be enabled.
+    # This then allows you to override the format in your locale files to something like this:
+    # en:
+    #   activemodel:
+    #     errors:
+    #       models:
+    #         person:
+    #           format: "%{message}"
+    #           attributes:
+    #             first_name:
+    #               blank: Enter your first name
+    # https://guides.rubyonrails.org/configuring.html#config-active-model-i18n-customize-full-message
+    config.active_model.i18n_customize_full_message = true
+
     # Ensure that application level configuration settings apply to
     # library specific deprecation warnings.
     # https://api.rubyonrails.org/classes/ActiveSupport/Deprecation.html
