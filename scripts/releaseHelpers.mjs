758f3f8b59739650ac2186d52a47b3e0cc257126
David Rapson <david.rapson@citizensadvice.org.uk>
2026-03-03 11:51:20 +0000

    Simplify npm-run scripts (#3998)
    Motivation of this change is to remove the number of development only dependencies in the npm package.
    
    Primarily does this by simplifying our release and npm-run scripts. Notably to upgrade inquirer, avoid taking a dependency on two separate git libraries, and to remove single use dependencies.
    
    The migration to the latest version of inquirer requires the use of ESM, that combined with using async/await significantly flattens the release scripts.

---
+++ scripts/releaseHelpers.mjs
@@ -0,0 +1,44 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+import simpleGit from "simple-git";
+
+import pkg from "../package.json" with { type: "json" };
+
+export const packageName = pkg.name;
+
+export const packageVersion = pkg.version;
+
+export const packageRoot = path.join(fileURLToPath(import.meta.url), "../..");
+
+export function exitWithError(err) {
+  console.log(err);
+  process.exit(1);
+}
+
+export function exitWithoutPublishing() {
+  console.log("Exiting without publishing");
+  process.exit(1);
+}
+
+export async function checkRepoStatus() {
+  const git = simpleGit(packageRoot);
+
+  try {
+    const status = await git.status();
+
+    if (status.current !== "main") {
+      exitWithError(`You must be in the main branch to release`);
+    }
+
+    if (status.ahead !== 0) {
+      exitWithError(`Your repo has changes not pushed to remote.`);
+    }
+
+    if (!status.isClean()) {
+      exitWithError(`Your have uncommitted changes in your repo.`);
+    }
+  } catch (gitErr) {
+    exitWithError(gitErr);
+  }
+}
