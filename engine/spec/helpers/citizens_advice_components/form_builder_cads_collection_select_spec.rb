7ccd0a6e47a6a10acbf43c1573e8eddf4bfae9e6
David Rapson <david.rapson@citizensadvice.org.uk>
2026-03-10 17:32:50 +0000

    Fix deprecation warning in cads_collection_select spec
    Make sure we're testing both the current and deprecated method of
    passing additional attributes

--- engine/spec/helpers/citizens_advice_components/form_builder_cads_collection_select_spec.rb
+++ engine/spec/helpers/citizens_advice_components/form_builder_cads_collection_select_spec.rb
@@ -109,8 +109,13 @@ RSpec.describe CitizensAdviceComponents::FormBuilder do
     context "when additional attributes are provided" do
       let(:field) do
         builder.cads_collection_select(
-          :currency, example_options, :id, :name,
-          additional_attributes: { autocomplete: "name", "data-additional": "example" }
+          :currency,
+          example_options,
+          :id,
+          :name,
+          {},
+          autocomplete: "name",
+          "data-additional": "example"
         )
       end
 
@@ -118,6 +123,29 @@ RSpec.describe CitizensAdviceComponents::FormBuilder do
         render_inline field
         expect(page).to have_css "select[autocomplete=name]"
         expect(page).to have_css "select[data-additional=example]"
+      end
+    end
+
+    context "when deprecated additional_attributes has is provided" do
+      let(:field) do
+        builder.cads_collection_select(
+          :currency, example_options, :id, :name,
+          additional_attributes: { autocomplete: "name", "data-additional": "example" }
+        )
+      end
+
+      before { allow(CitizensAdviceComponents.deprecator).to receive(:warn) }
+
+      it "passes additional attributes through to element" do
+        render_inline field
+        expect(page).to have_css "select[autocomplete=name]"
+        expect(page).to have_css "select[data-additional=example]"
+      end
+
+      it "logs deprecation warning" do
+        render_inline field
+        expect(CitizensAdviceComponents.deprecator).to have_received(:warn)
+          .with(/additional_attributes hash is deprecated/)
       end
     end
 
