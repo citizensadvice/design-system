928bf9b1bfc768ed7e1c87055d98f1dd761274cb
David Rapson <david.rapson@citizensadvice.org.uk>
2026-03-12 17:22:51 +0000

    Fix deprecation warnings in demo forms (#4016)
    Some examples were still using the previous format for collections

--- demo/app/models/example_form_builder/radio_group.rb
+++ demo/app/models/example_form_builder/radio_group.rb
@@ -2,6 +2,18 @@
 
 module ExampleFormBuilder
   class RadioGroup < WizardSteps::Step
+    Option = Data.define(:name, :id)
+
     attribute :favourite_drink
+
+    validates :favourite_drink, presence: true, inclusion: { in: ->(step) { step.drinks.map(&:id) } }
+
+    def drinks
+      [
+        Option.new(id: "tea", name: "Tea"),
+        Option.new(id: "coffee", name: "Coffee"),
+        Option.new(id: "water", name: "Water")
+      ]
+    end
   end
 end
