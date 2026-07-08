8692763b8b8832abb354d02a5a023cb5961109c5
David Rapson <david.rapson@citizensadvice.org.uk>
2026-07-08 10:10:18 +0100

    Remove additional_attributes named parameter from field methods

--- engine/app/lib/citizens_advice_components/elements/check_box.rb
+++ engine/app/lib/citizens_advice_components/elements/check_box.rb
@@ -16,7 +16,6 @@ module CitizensAdviceComponents
 
         @attribute = attribute
         @options = kwargs
-        additional_attributes_deprecation
       end
 
       protected
@@ -50,7 +49,7 @@ module CitizensAdviceComponents
           "aria-required": required?,
           "aria-invalid": error?,
           "aria-describedby": described_by,
-          **check_box_options
+          **options_for_html
         )
       end
 
@@ -65,21 +64,6 @@ module CitizensAdviceComponents
           "cads-form-group",
           "cads-form-group--checkbox",
           "cads-checkbox-single"
-        )
-      end
-
-      def check_box_options
-        # The default behaviour of text_field is to pass options on to the HTML element
-        # Extract any custom options that we don't want passing on and add expected defaults.
-        # For backwards compatability merge in additional_options hash
-        options_for_html.merge(options[:additional_attributes] || {})
-      end
-
-      def additional_attributes_deprecation
-        return if options[:additional_attributes].blank?
-
-        CitizensAdviceComponents.deprecator.warn(
-          "additional_attributes hash is deprecated, pass directly via options hash"
         )
       end
     end
