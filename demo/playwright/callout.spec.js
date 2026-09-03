ef8e1566263822f0f3d85d3940de469bbbd51aea
David Rapson <david.rapson@citizensadvice.org.uk>
2026-09-03 12:01:27 +0100

    Refactor callout spec to isolate scenarios

--- demo/playwright/callout.spec.js
+++ demo/playwright/callout.spec.js
@@ -1,66 +1,98 @@
-import { test, expect } from "@playwright/test";
+import { describe, expect, test } from "@playwright/test";
 import {
   componentUrl,
   defaultViewports,
   expectNoAxeViolations,
 } from "./playwright-helpers";
 
-test("Callout (standard)", async ({ page }) => {
-  await page.goto(componentUrl("callout/standard"));
+describe("Callout (standard)", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(componentUrl("callout/standard"));
+  })
 
-  await expectNoAxeViolations(page);
+  test("accessibility check", async ({ page }) => {
+    await expectNoAxeViolations(page);
+  })
 
   for (const viewport of defaultViewports) {
-    await page.setViewportSize(viewport);
-    await expect(page).toHaveScreenshot(
-      `callout-standard-${viewport.label}.png`,
-    );
+    test(`visual regression check ${viewport.label}`, async ({ page }) => {
+      await page.setViewportSize(viewport);
+      await expect(page).toHaveScreenshot(
+        `callout-standard-${viewport.label}.png`
+      );
+    })
   }
 });
 
-test("Callout (nested)", async ({ page }) => {
-  await page.goto(componentUrl("callout/nested"));
+describe("Callout (nested)", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(componentUrl("callout/nested"));
+  })
 
-  await expectNoAxeViolations(page);
+  test("accessibility check", async ({ page }) => {
+    await expectNoAxeViolations(page);
+  })
 
-  await expect(page).toHaveScreenshot(`callout-nested.png`, { fullPage: true });
+  test(`visual regression check`, async ({ page }) => {
+    await expect(page).toHaveScreenshot(
+      "callout-nested.png"
+    );
+  })
 });
 
-test("Callout (example)", async ({ page }) => {
-  await page.goto(componentUrl("callout/example"));
+describe("Callout (example)", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(componentUrl("callout/example"));
+  })
 
-  await expectNoAxeViolations(page);
+  test("accessibility check", async ({ page }) => {
+    await expectNoAxeViolations(page);
+  })
 
   for (const viewport of defaultViewports) {
-    await page.setViewportSize(viewport);
-    await expect(page).toHaveScreenshot(
-      `callout-example-${viewport.label}.png`,
-    );
+    test(`visual regression check ${viewport.label}`, async ({ page }) => {
+      await page.setViewportSize(viewport);
+      await expect(page).toHaveScreenshot(
+        `callout-example-${viewport.label}.png`
+      );
+    })
   }
 });
 
-test("Callout (important)", async ({ page }) => {
-  await page.goto(componentUrl("callout/important"));
+describe("Callout (important)", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(componentUrl("callout/important"));
+  })
 
-  await expectNoAxeViolations(page);
+  test("accessibility check", async ({ page }) => {
+    await expectNoAxeViolations(page);
+  })
 
   for (const viewport of defaultViewports) {
-    await page.setViewportSize(viewport);
-    await expect(page).toHaveScreenshot(
-      `callout-important-${viewport.label}.png`,
-    );
+    test(`visual regression check ${viewport.label}`, async ({ page }) => {
+      await page.setViewportSize(viewport);
+      await expect(page).toHaveScreenshot(
+        `callout-important-${viewport.label}.png`
+      );
+    })
   }
 });
 
-test("Callout (adviser)", async ({ page }) => {
-  await page.goto(componentUrl("callout/adviser"));
+describe("Callout (adviser)", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(componentUrl("callout/adviser"));
+  })
 
-  await expectNoAxeViolations(page);
+  test("accessibility check", async ({ page }) => {
+    await expectNoAxeViolations(page);
+  })
 
   for (const viewport of defaultViewports) {
-    await page.setViewportSize(viewport);
-    await expect(page).toHaveScreenshot(
-      `callout-adviser-${viewport.label}.png`,
-    );
+    test(`visual regression check ${viewport.label}`, async ({ page }) => {
+      await page.setViewportSize(viewport);
+      await expect(page).toHaveScreenshot(
+        `callout-adviser-${viewport.label}.png`
+      );
+    })
   }
 });
