0cf777c33642af4c78ba20dcfd94823ce902cdca
A Haque <40199643+zaid3@users.noreply.github.com>
2026-09-15 20:58:43 +0100

    Test prose heading utilities

--- scss/__tests__/objects.test.js
+++ scss/__tests__/objects.test.js
@@ -4,3 +4,13 @@ test("grid styles match snapshot", () => {
   const output = sass.compile(`scss/5-objects/_grid.scss`);
   expect(output.css.toString({ logger: sass.Logger.silent })).toMatchSnapshot();
 });
+
+test("prose supports heading utility classes", () => {
+  const output = sass.compile("scss/lib.scss", {
+    logger: sass.Logger.silent,
+  });
+
+  for (const level of [1, 2, 3, 4]) {
+    expect(output.css.toString()).toContain(`.cads-prose .cads-h${level}`);
+  }
+});
