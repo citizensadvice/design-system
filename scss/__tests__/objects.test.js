f3da80be3b526d13fe2f77945c9b253fd19653bd
David Rapson <david.rapson@citizensadvice.org.uk>
2026-09-16 15:43:47 +0100

    Rewrite remaining Vitest tests using node:test
    Because the only remaining unit tests after migrating to Playwright are
    SCSS conformance tests we can simplify the test setup by using the
    inbuilt `node:test` methods.
    
    Since Node 22+ this includes snapshot testing and all the conveniences
    you'd expect of a testing tool.
    
    Simplifies the SCSS and stylelint checks into ~200 lines of node test
    code. Removes the now unused dependencies on Vitest, JSDOM, and Testing
    Library.

--- scss/__tests__/objects.test.js
+++
@@ -1,6 +0,0 @@
-const sass = require("sass");
-
-test("grid styles match snapshot", () => {
-  const output = sass.compile(`scss/5-objects/_grid.scss`);
-  expect(output.css.toString({ logger: sass.Logger.silent })).toMatchSnapshot();
-});
